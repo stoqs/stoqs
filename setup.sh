6cf45c1344cc9b730dd5cc5c0837292f7d653fad
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-02-01 12:24:25 -0800

    Refined for making sure GDAL shared libaries are installed.  Added wsgi and log folder permission settings.

--- setup.sh
+++ setup.sh
@@ -4,31 +4,46 @@
 
 REQ="requirements.txt"
 EGG_CACHE="wsgi/egg-cache"
+VENV_NAME="venv-stoqs"
 
 which virtualenv &>/dev/null
 if [ $? -ne 0 ]; then
-  echo "virtualenv command not found"
-  echo "sudo easy_install virtualenv"
+ echo "virtualenv command not found"
+ echo "sudo easy_install virtualenv"
 fi
-if [ -d venv-stoqs ]; then
-  echo "virtualenv has already been created"
+
+if [ -d $VENV_NAME ]; then
+ echo "virtualenv has already been created"
 else
-  virtualenv venv-stoqs
-
-  # Setup gdal-config so we can install GDAL (if needed) in venv
-  CONFIG=$(which gdal-config)
-  if [ $? -ne 0 ]; then
-    echo "gdal-config is not in PATH"
-
-    ln -s $CONFIG venv-stoqs/bin/
-  fi
+ virtualenv $VENV_NAME
+ # Install GDAL in venv
+ CONFIG=$(which gdal-config)
+ if [ $? -ne 0 ]; then
+   echo "gdal-config is not in PATH"
+   rm -rf $VENV_NAME
+   exit 1
+ fi
+ ln -s $CONFIG $VENV_NAME/bin/
 fi
-source venv-stoqs/bin/activate
+source $VENV_NAME/bin/activate
 easy_install pip
+
 if [ -f "$REQ" ]; then
-  pip install -r $REQ
+ pip install -r $REQ
 fi
-pip freeze > $REQ
+
+
+pip freeze|grep -v pysqlite > $REQ
+if [ ! -d $EGG_CACHE ]; then
+ echo "Creating the egg cache"
+ mkdir $EGG_CACHE
+fi
+sudo chown www-data $EGG_CACHE
+
+sudo chgrp apache log
+sudo chmod g+s log
+touch log/django.log
+chmod g+w log/django.log
 
 # echo running a quick test to make sure things aren't broke
 export PYTHONPATH=$(pwd)
