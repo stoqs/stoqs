50457a38c23df1f37ecfeec990aae698647127c6
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-02-01 15:50:16 -0800

    Factored out methods so as not to repeat myself.

--- loaders/loadMarMenor2011Data.py
+++ loaders/loadMarMenor2011Data.py
@@ -30,117 +30,92 @@ import DAPloaders
 from datetime import datetime
 from stoqs import models as mod
 
-# ------------------------- Mar Menor In Situ (AUV and Castaway) data loads -------------------------
-# E.g.:
-#   http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/UniversityOfGirona/exp4_5Nov2011_data.nc.html
-# This could be smarter with a directory scan...
-baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
-files =      [  'UniversityOfGirona/exp1_3Nov2011_data.nc',
-		'UniversityOfGirona/exp2_3Nov2011_data.nc',
-		'UniversityOfGirona/exp3_5Nov2011_data.nc',
-		'UniversityOfGirona/exp4_5Nov2011_data.nc',
-		]
-for (aName, file) in zip([ a + ' (stride=1)' for a in files], files):
-##while False:
-	print "Instantiating Auvctd_Loader for url = %s" % baseUrl + file
-	loader = DAPloaders.Auvctd_Loader(aName,
-				url = baseUrl + file,
-				platformName = 'sparus',
-				platformTypeName = 'auv',
-				activitytypeName = 'AUV Mission',
-				stride = 1)
+def runLoader(url, cName, aName, pName, pTypeName, aTypeName, parmList, dbName, stride):
+	'''Run the DAPloader for AUVCTD trajectory data and update the Activity with attributes resulting from the load into dbName'''
 
-	loader.include_names = ['temperature', 'conductivity']
+	print "Instantiating Auvctd_Loader for url = %s" % url
+	loader = DAPloaders.Auvctd_Loader(
+			url = url,
+			campaignName = cName,
+			dbName = dbName,
+			activityName = aName,
+			activitytypeName = aTypeName,
+			platformName = pName,
+			platformTypeName = pTypeName,
+			stride = stride)
+
+	print "runLoader(): Setting include_names to %s" % parmList
+	loader.include_names = parmList
 	(nMP, path) = loader.process_data()
+	print "runLoader(): Loaded Activity with name = %s" % aName
 
 	# Careful with the structure of this comment.  It is parsed in views.py to give some useful links in showActivities()
-	newComment = "%d MeasuredParameters loaded for Parameters: %s. Loaded on %sZ" % (nMP, ' '.join(loader.varsLoaded), datetime.utcnow())
-	print "Updating comment with newComment = %s" % newComment
-	mod.Activity.objects.filter(name = aName).update(comment = newComment,
-								maptrack = path,
-								num_measuredparameters = nMP,
-								loaded_date = datetime.utcnow())
+	newComment = "%d MeasuredParameters loaded: %s. Loaded on %sZ" % (nMP, ' '.join(loader.varsLoaded), datetime.utcnow())
+	print "runLoader(): Updating its comment with newComment = %s" % newComment
+
+	num_updated = mod.Activity.objects.using(dbName).filter(name = aName).update(comment = newComment,
+						maptrack = path,
+						num_measuredparameters = nMP,
+						loaded_date = datetime.utcnow())
+
+	print "runLoader(): %d activities updated with new attributes." % num_updated
+	raw_input('paused')
 
 
-# http://odss.mbari.org:8080/thredds/dodsC/agg/Castaway.html
-baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/agg/Castaway'
-aName = 'All Castaway CTD casts done from the Sorell on 5 November 2011'
-file = ''
-if True:	# Do just once
-##if False:
-	print "Instantiating Auvctd_Loader for url = %s" % baseUrl + file
-	loader = DAPloaders.Auvctd_Loader(aName,
-				url = baseUrl + file,
-				platformName = 'Castaway',
-				platformTypeName = 'ship',
-				activitytypeName = 'CTD Casts',
-				stride = 1)
+def loadMissions(baseUrl, fileList, activityName, campaignName, pName, pTypeName, aTypeName, parmList, dbName, stride = 1):
+	'''Load missions from OPeNDAP url from either a list of files from a base or a single URL with a given activityName '''
 
-	loader.include_names = ['temperature', 'conductivity', 'salinity']
-	(nMP, path) = loader.process_data()
-
-	# Careful with the structure of this comment.  It is parsed in views.py to give some useful links in showActivities()
-	newComment = "%d MeasuredParameters loaded for Parameters: %s. Loaded on %sZ" % (nMP, ' '.join(loader.varsLoaded), datetime.utcnow())
-	print "Updating comment with newComment = %s" % newComment
-	mod.Activity.objects.filter(name = aName).update(comment = newComment,
-								maptrack = path,
-								num_measuredparameters = nMP,
-								loaded_date = datetime.utcnow())
+	if fileList: 
+		for (aName, file) in zip([ a + ' (stride=%d)' % stride for a in files], fileList):
+			url = baseUrl + file
+			print "loadMissions(): Calling runLoader() with parmList = %s" % parmList
+			runLoader(url, campaignName, aName, pName, pTypeName, aTypeName, parmList, dbName, stride)
+	elif activityName:
+		url = baseUrl
+		runLoader(url, campaignName, activityName, pName, pTypeName, aTypeName, parmList, dbName, stride)
+	else:
+		print "loadMissions(): Must specify either a fileList or an activityName"
 
 
-# University of Villanova
-baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
-files =      [  'UniversityOfVillanova/GuanayII_2011-11-3_Salinity_v2.nc',
-		'UniversityOfVillanova/GuanayII-4nov.nc',
-		]
-for (aName, file) in zip([ a + ' (stride=1)' for a in files], files):
-##while False:
-	print "Instantiating Auvctd_Loader for url = %s" % baseUrl + file
-	loader = DAPloaders.Auvctd_Loader(aName,
-				url = baseUrl + file,
-				platformName = 'guanayii',
-				platformTypeName = 'auv',
-				activitytypeName = 'AUV Mission',
-				stride = 1)
+# Specific locations of data to be loaded - ideally the only thing that needs to be changed for another campaign
+if __name__ == '__main__':
+	'''Mar Menor In Situ (AUV and Castaway) data loads '''
 
-	loader.include_names = ['temperature', 'conductivity', 'salinity']
-	(nMP, path) = loader.process_data()
+	dbName = 'stoqs_nov2011'
+	campaignName = 'Mar Menor - November 2011'
 
-	# Careful with the structure of this comment.  It is parsed in views.py to give some useful links in showActivities()
-	newComment = "%d MeasuredParameters loaded for Parameters: %s. Loaded on %sZ" % (nMP, ' '.join(loader.varsLoaded), datetime.utcnow())
-	print "Updating comment with newComment = %s" % newComment
-	mod.Activity.objects.filter(name = aName).update(comment = newComment,
-								maptrack = path,
-								num_measuredparameters = nMP,
-								loaded_date = datetime.utcnow())
+	# Sparus, e.g.: http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/UniversityOfGirona/exp4_5Nov2011_data.nc.html
+	baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
+	files =      [  'UniversityOfGirona/exp1_3Nov2011_data.nc',
+			'UniversityOfGirona/exp2_3Nov2011_data.nc',
+			'UniversityOfGirona/exp3_5Nov2011_data.nc',
+			'UniversityOfGirona/exp4_5Nov2011_data.nc',
+			]
+	parms = ['temperature', 'conductivity']
+	loadMissions(baseUrl, files, '', campaignName, 'sparus', 'auv', 'AUV Mission', parms, dbName)
 
+	# University of Villanova
+	baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
+	files =      [  'UniversityOfVillanova/GuanayII_2011-11-3_Salinity_v2.nc',
+			'UniversityOfVillanova/GuanayII-4nov.nc',
+			]
+	parms = ['temperature', 'conductivity', 'salinity']
+	loadMissions(baseUrl, files, '', campaignName, 'guanayii', 'auv', 'AUV Mission', parms, dbName)
 
-# University of Porto
-baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
-files =      [  'UniversityOfPorto/092152_quad_1m.nc',
-		'UniversityOfPorto/085825_rect_6_8.nc',
-		'UniversityOfPorto/085422_quad_surface.nc',
-		'UniversityOfPorto/083743_rect_1m.nc',
-		'UniversityOfPorto/081952_quad_100m_superficie.nc',
-		]
-for (aName, file) in zip([ a + ' (stride=1)' for a in files], files):
-##while False:
-	print "Instantiating Auvctd_Loader for url = %s" % baseUrl + file
-	loader = DAPloaders.Auvctd_Loader(aName,
-				url = baseUrl + file,
-				platformName = 'seacon-2',
-				platformTypeName = 'auv',
-				activitytypeName = 'AUV Mission',
-				stride = 1)
+	# University of Porto
+	baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/marmenor/insitu/'
+	files =      [  'UniversityOfPorto/092152_quad_1m.nc',
+			'UniversityOfPorto/085825_rect_6_8.nc',
+			'UniversityOfPorto/085422_quad_surface.nc',
+			'UniversityOfPorto/083743_rect_1m.nc',
+			'UniversityOfPorto/081952_quad_100m_superficie.nc',
+			]
+	parms = ['temperature', 'conductivity', 'salinity']
+	loadMissions(baseUrl, files, '', campaignName, 'seacon-2', 'auv', 'AUV Mission', parms, dbName)
 
-	loader.include_names = ['temperature', 'conductivity', 'salinity']
-	(nMP, path) = loader.process_data()
-
-	# Careful with the structure of this comment.  It is parsed in views.py to give some useful links in showActivities()
-	newComment = "%d MeasuredParameters loaded for Parameters: %s. Loaded on %sZ" % (nMP, ' '.join(loader.varsLoaded), datetime.utcnow())
-	print "Updating comment with newComment = %s" % newComment
-	mod.Activity.objects.filter(name = aName).update(comment = newComment,
-								maptrack = path,
-								num_measuredparameters = nMP,
-								loaded_date = datetime.utcnow())
+	# Castaway: http://odss.mbari.org:8080/thredds/dodsC/agg/Castaway.html
+	baseUrl = 'http://odss.mbari.org:8080/thredds/dodsC/agg/Castaway'
+	aName = 'All Castaway CTD casts done from the Sorell on 5 November 2011'
+	parms = ['temperature', 'conductivity', 'salinity']
+	loadMissions(baseUrl, '', aName, campaignName, 'Castaway', 'ship', 'CTD Casts', parms, dbName)
 
