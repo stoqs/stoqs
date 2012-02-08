bae79e78ac7f1a154b13720dcc0b9d3e62ec2736
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-02-07 22:21:50 -0800

    Revamped \w+ to [^/]+ to allow for special characters in names, etc.  Making use of formatPat.

--- urls.py
+++ urls.py
@@ -41,7 +41,9 @@ pre = r'^(?P<dbName>[^/]+)/'    # The database name will prefix all of our reque
 btwnCl = r'(?P<pName>[^/]+)/between/(?P<startDate>\w+)/(?P<endDate>\w+)/depth/(?P<dmin>[0-9\.]+)/(?P<dmax>[0-9\.]+)'
 
 # type is one of: 'data', 'perf'; format is one of: 'html', 'csv', 'kml'
-formatCl = r'/(?P<type>\w{4,5})\.(?P<format>\w{0,4})$'
+typePat = r'/(?P<type>[^/]{4,5})'
+formatPat = r'(?P<format>[^/]{0,4})$'
+formatCl = typePat + r'\.' + formatPat
 
 
 urlpatterns = patterns('',
@@ -50,58 +52,56 @@ urlpatterns = patterns('',
     url(pre + r'admin/', include(admin.site.urls)),
 
     # Type and name queries
-    url(pre + r'platformTypes.?(\w{0,4})$', 'stoqs.views.showPlatformTypes', {}, name='show-platform-types'),
-    url(pre + r'platformNames.?(\w{0,4})$', 'stoqs.views.showPlatformNames', {}, name='show-platform-names'),
-    url(pre + r'platformNamesOfType/(\w+).?(\w{0,4})$', 'stoqs.views.showPlatformNamesOfType', {}, name='show-platform-names-of-type'),
-    url(pre + r'platformAssociations.?(\w{0,4})$', 'stoqs.views.showPlatformAssociations', {}, name='show-platform-associations'),
+    url(pre + r'platformTypes.?' + formatPat, 'stoqs.views.showPlatformTypes', {}, name='show-platform-types'),
+    url(pre + r'platformNames.?' + formatPat, 'stoqs.views.showPlatformNames', {}, name='show-platform-names'),
+    url(pre + r'platformNamesOfType/(?P<ptn>[^/]+).?' + formatPat, 'stoqs.views.showPlatformNamesOfType', {}, name='show-platform-names-of-type'),
+    url(pre + r'platformAssociations.?' + formatPat, 'stoqs.views.showPlatformAssociations', {}, name='show-platform-associations'),
 
     # Parameters
-    url(pre + r'parameters.?(?P<format>\w{0,4})$', 'stoqs.views.showParameters', {}, name='show-parameters'),
+    url(pre + r'parameters.?' + formatPat, 'stoqs.views.showParameters', {}, name='show-parameters'),
 
     # Position queries (last, since, between)
-    url(pre + r'position/(?P<name>\w+)/last/(?P<number>\d{1,10})(?P<unit>[smhdw]?)/data.?(?P<format>\w{0,4})$', 'stoqs.views.showLastPositions', {}, name='show-last-positions'),
-    url(pre + r'position/(\w+)/last/(\d{1,10})([smhdw]?)/stride/(\d+)/data.(\w{0,4})$', 'stoqs.views.showLastPositions', {}, name='show-last-positions'),
-    url(pre + r'position/(\w+)/last/(\d{1,10})([smhdw]?)/count$', 'stoqs.views.showLastPositions', {'countFlag': True}, name='show-last-positions'),
+    url(pre + r'position/(?P<name>[^/]+)/last/(?P<number>\d{1,10})(?P<unit>[smhdw]?)/data.' + formatPat, 'stoqs.views.showLastPositions', {}, name='show-last-positions'),
+    url(pre + r'position/([^/]+)/last/(\d{1,10})([smhdw]?)/stride/(\d+)/data.' + formatPat, 'stoqs.views.showLastPositions', {}, name='show-last-positions'),
+    url(pre + r'position/([^/]+)/last/(\d{1,10})([smhdw]?)/count$', 'stoqs.views.showLastPositions', {'countFlag': True}, name='show-last-positions'),
 
-    url(pre + r'position/(?P<name>\w+)/since/(?P<startDate>\w+)/data.(?P<format>\w{0,4})$', 'stoqs.views.showSincePositions', {}, name='show-since-positions'),
-    url(pre + r'position/(\w+)/since/(\w+)/stride/(\d+)/data.(\w{0,4})$', 'stoqs.views.showSincePositions', {}, name='show-since-positions'),
-    url(pre + r'position/(\w+)/since/(\w+)/count$', 'stoqs.views.showSincePositions', {'countFlag': True}, name='show-since-positions'),
+    url(pre + r'position/(?P<name>\w+)/since/(?P<startDate>\w+)/data.' + formatPat, 'stoqs.views.showSincePositions', {}, name='show-since-positions'),
+    url(pre + r'position/([^/]+)/since/([^/]+)/stride/(\d+)/data.' + formatPat, 'stoqs.views.showSincePositions', {}, name='show-since-positions'),
+    url(pre + r'position/([^/]+)/since/([^/]+)/count$', 'stoqs.views.showSincePositions', {'countFlag': True}, name='show-since-positions'),
 
-    url(pre + r'position/(?P<name>\w+)/between/(?P<startDate>\w+)/(?P<endDate>\w+)/data.(?P<format>\w{0,4})$', 'stoqs.views.showBetweenPositions', {}, name='show-between-positions'),
-    url(pre + r'position/(\w+)/between/(\w+)/(\w+)/stride/(\d+)/data.(\w{0,4})$', 'stoqs.views.showBetweenPositions', {}, name='show-between-positions'),
-    url(pre + r'position/(\w+)/between/(\w+)/(\w+)/count$', 'stoqs.views.showBetweenPositions', {'countFlag': True}, name='show-between-positions'),
+    url(pre + r'position/(?P<name>\w+)/between/(?P<startDate>[^/]+)/(?P<endDate>[^/]+)/data.' + formatPat, 'stoqs.views.showBetweenPositions', {}, name='show-between-positions'),
+    url(pre + r'position/([^/]+)/between/([^/]+)/([^/]+)/stride/(\d+)/data.' + formatPat, 'stoqs.views.showBetweenPositions', {}, name='show-between-positions'),
+    url(pre + r'position/([^/]+)/between/([^/]+)/([^/]+)/count$', 'stoqs.views.showBetweenPositions', {'countFlag': True}, name='show-between-positions'),
 
     # Repeat last position queries, but for 'OfType'
-    url(pre + r'positionOfType/(?P<name>\w+)/last/(?P<number>\d{1,10})(?P<unit>[smhdw]?)/data.?(?P<format>\w{0,4})$', 
+    url(pre + r'positionOfType/(?P<name>\w+)/last/(?P<number>\d{1,10})(?P<unit>[smhdw]?)/data.' + formatPat, 
 	   'stoqs.views.showLastPositions', {'ofType': True}, name='show-last-positions'),
-    url(pre + r'positionOfType/(\w+)/last/(\d{1,10})([smhdw]?)/stride/(\d+)/data.(\w{0,4})$', 
+    url(pre + r'positionOfType/(\w+)/last/(\d{1,10})([smhdw]?)/stride/(\d+)/data.' + formatPat, 
 	   'stoqs.views.showLastPositions', {'ofType': True}, name='show-last-positions'),
-    url(pre + r'positionOfType/(\w+)/last/(\d{1,10})([smhdw]?)/count$', 
+    url(pre + r'positionOfType/([^/]+)/last/(\d{1,10})([smhdw]?)/count$', 
        'stoqs.views.showLastPositions', {'ofType': True, 'countFlag': True}, name='show-last-positions'),
 
-    url(pre + r'positionOfType/(?P<name>\w+)/since/(?P<startDate>\w+)/data.(?P<format>\w{0,4})$', 
+    url(pre + r'positionOfType/(?P<name>[^/]+)/since/(?P<startDate>\w+)/data.' + formatPat, 
 	   'stoqs.views.showSincePositions', {'ofType': True}, name='show-since-positions'),
-    url(pre + r'positionOfType/(\w+)/since/(\w+)/stride/(\d+)/data.(\w{0,4})$', 
+    url(pre + r'positionOfType/(\w+)/since/([^/]+)/stride/(\d+)/data.' + formatPat, 
 	   'stoqs.views.showSincePositions', {'ofType': True}, name='show-since-positions'),
-    url(pre + r'positionOfType/(\w+)/since/(\w+)/count$', 
+    url(pre + r'positionOfType/([^/]+)/since/(\w+)/count$', 
        'stoqs.views.showSincePositions', {'ofType': True, 'countFlag': True}, name='show-since-positions'),
 
-    url(pre + r'positionOfType/(?P<name>\w+)/between/(?P<startDate>\w+)/(?P<endDate>\w+)/data.(?P<format>\w{0,4})$', 
+    url(pre + r'positionOfType/(?P<name>[^/]+)/between/(?P<startDate>\w+)/(?P<endDate>\w+)/data.' + formatPat, 
 	   'stoqs.views.showBetweenPositions', {'ofType': True}, name='show-between-positions'),
-    url(pre + r'positionOfType/(\w+)/between/(\w+)/(\w+)/stride/(\d+)/data.(\w{0,4})$', 
+    url(pre + r'positionOfType/([^/]+)/between/([^/]+)/([^/]+)/stride/(\d+)/data.' + formatPat, 
 	    'stoqs.views.showBetweenPositions', {'ofType': True}, name='show-between-positions'),
-    url(pre + r'positionOfType/(\w+)/between/(\w+)/(\w+)/count$', 
+    url(pre + r'positionOfType/([^/]+)/between/([^/]+)/([^/]+)/count$', 
         'stoqs.views.showBetweenPositions', {'ofType': True, 'countFlag': True}, name='show-between-positions'),
 
     # Position 'OfActivity' queries
-    url(pre + r'positionOfActivity/name/(?P<aName>\w+)/type/(?P<aType>\w+)/data.?(?P<format>\w{0,4})$', 
+    url(pre + r'positionOfActivity/name/(?P<aName>[^/]+)/type/(?P<aType>[^/]+)/data.' + formatPat, 
 	   'stoqs.views.showPositionsOfActivity', {}, name='show-positions-of-activity'),
-    url(pre + r'positionOfActivity/name/(\w+)/type/(\w+)/stride/(\d+)/data.(\w{0,4})$', 
+    url(pre + r'positionOfActivity/name/([^/]+)/type/([^/]+)/stride/(\d+)/data.' + formatPat, 
 	   'stoqs.views.showPositionsOfActivity', {}, name='show-positions-of-activity'),
 
     # Measurements  
-    url(pre + r'parameterNames.?(\w{0,4})$', 'stoqs.views.measurement.showParameterNames', {}, name='show-parameter-names'),
-    
     url(pre + 'measurement/' + btwnCl + formatCl, 
         'stoqs.views.measurement.showBetweenMeasurements', {}, name='show-between-meas'),
     url(pre + r'measurement/' + btwnCl + '/stride/(?P<stride>\d+)' + formatCl, 
