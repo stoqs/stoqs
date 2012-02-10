3bb5bd5cde21e6192a2d78a9fa830ce17195f827
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-02-09 16:07:42 -0800

    Added .select_related() to query that needs to traverse the stoqs data model.

--- stoqs/views/measurement.py
+++ stoqs/views/measurement.py
@@ -32,8 +32,6 @@ import time
 import sys
 import csv
 import stoqs.models as mod
-##from stoqs import query_wrapper
-##from django.db.backends import util
 import logging
 
 from KML import makeKML
@@ -42,14 +40,13 @@ logger = logging.getLogger(__name__)
 
 # Control util.CursorDebugWrapper - setting to True will cause all db queries to have their stack trace logged
 ##connection.use_debug_cursor = False
-##util.CursorDebugWrapper = query_wrapper.PrintQueryWrapper
 
 def showMeasurementsOfActivity(request, aName, aType, stride = '1', format = 'html', ofType = False, countFlag = False):
     '''Return measurements in specified format of specified Activity Name and Activity Type
     '''
     
     measList = []
-    qs = mod.MeasuredParameter.objects.filter(measurement__instantpoint__activity__name = aName,
+    qs = mod.MeasuredParameter.objects.select_related().filter(measurement__instantpoint__activity__name = aName,
                                                measurement__instantpoint__activity__activitytype__name = aType)
     if countFlag:
         count = qs.count()
@@ -122,16 +119,17 @@ def showBetweenMeasurements(request, pName, startDate, endDate, dmin, dmax, stri
     else:
         start = time.clock()
         if snFlag:
-            qs = mod.MeasuredParameter.objects.filter(parameter__standard_name = pName, measurement__instantpoint__timevalue__gte = sDate, 
+            qs = mod.MeasuredParameter.objects.select_related().filter(parameter__standard_name = pName, measurement__instantpoint__timevalue__gte = sDate, 
                         measurement__instantpoint__timevalue__lte = eDate, measurement__depth__gte = dmin, 
                         measurement__depth__lte = dmax).order_by('measurement__instantpoint__timevalue')
             logger.debug(qs.query)
             mpList = qs[::int(stride)]
             
         else:
-            qs = mod.MeasuredParameter.objects.filter(parameter__name = pName, measurement__instantpoint__timevalue__gte = sDate, 
+            qs = mod.MeasuredParameter.objects.select_related().filter(parameter__name = pName, measurement__instantpoint__timevalue__gte = sDate, 
                         measurement__instantpoint__timevalue__lte = eDate, measurement__depth__gte = dmin, 
                         measurement__depth__lte = dmax).order_by('measurement__instantpoint__timevalue')
+
             logger.debug(qs.query)
             mpList = qs[::int(stride)]
             
@@ -164,11 +162,13 @@ def showBetweenMeasurements(request, pName, startDate, endDate, dmin, dmax, stri
         else:
             start = time.clock()
             # This statement causes many SQL queries in order to look up all of the attributes
+
+
             measList.extend([ (mp.parameter.name, mp.measurement.instantpoint.activity.platform.name,
                         time.mktime(mp.measurement.instantpoint.timevalue.timetuple()), 
                         '%.6f' % mp.measurement.geom.x, '%.6f' %  mp.measurement.geom.y, '%.1f' %  mp.measurement.depth,
                         '%f' % mp.datavalue,
-                        mp.measurement.instantpoint.timevalue.strftime('%Y-%m-%dT%H:%M:%SZ')) for mp in list(mpList[::]) ])
+                        mp.measurement.instantpoint.timevalue.strftime('%Y-%m-%dT%H:%M:%SZ')) for mp in mpList[::] ])
             perfDict['measListBuildTime'] = time.clock() - start
 
     if countFlag:
