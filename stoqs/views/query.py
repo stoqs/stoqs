af0ccbefd41e18f9baf06b8cce3d8206e54cb526
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-03-24 21:44:35 -0700

    Put spaces around =, testing data output processing

--- stoqs/views/query.py
+++ stoqs/views/query.py
@@ -27,36 +27,42 @@ logger = logging.getLogger(__name__)
 
 
 def queryData(request, format=None):
-    response=HttpResponse()
-    query_parms={'parameters': 'parameters', # This should be specified once in the query string for each parameter.
-                 'time': ('start_time','end_time'), # Single values
-                 'depth': ('min_depth', 'max_depth'), # Single values
-                 'platforms': 'platforms', # Specified once in the query string for each platform.
-                 }
-    params={}
+    response = HttpResponse()
+    query_parms = {'parameters': 'parameters', # This should be specified once in the query string for each parameter.
+                   'time': ('start_time','end_time'), # Single values
+                   'depth': ('min_depth', 'max_depth'), # Single values
+                   'platforms': 'platforms', # Specified once in the query string for each platform.
+                   }
+    params = {}
     for key, value in query_parms.iteritems():
         if type(value) in (list, tuple):
-            params[key]=[request.GET.get(p, None) for p in value]
+            params[key] = [request.GET.get(p, None) for p in value]
         else:
-            params[key]=request.GET.getlist(key)
+            params[key] = request.GET.getlist(key)
     
-    qm=STOQSQManager(request, response, request.META['dbAlias'])
+    qm = STOQSQManager(request, response, request.META['dbAlias'])
     qm.buildQuerySet(**params)
     
     if not format: # here we export in a given format, or just provide summary data if no format is given.
-        response['Content-Type']='text/json'
+        response['Content-Type'] = 'text/json'
         response.write(simplejson.dumps(qm.generateOptions(),
                                         cls=encoders.STOQSJSONEncoder))
                                         # use_decimal=True) # After json v2.1.0 this can be used instead of the custom encoder class.
     elif format == 'json':
-        response['Content-Type']='text/json'
+        response['Content-Type'] = 'text/json'
         response.write(serializers.serialize('json', qm.qs))
+    elif format == 'csv':
+        logger.info('csv output')
+    elif format == 'dap':
+        logger.info('dap output')
+    elif format == 'kml':
+        logger.info('kml output')
     return response
     
 def queryUI(request):
     formats={'csv': 'Comma-Separated Values (CSV)',
-             'DAP': 'OPeNDAP Format',
-             'KML': 'KML (Google Earth)'}
+             'dap': 'OPeNDAP Format',
+             'kml': 'KML (Google Earth)'}
     return render_to_response('stoqsquery.html', {'site_uri': request.build_absolute_uri('/')[:-1],
                                                   'formats': formats,}, 
                             context_instance=RequestContext(request))
