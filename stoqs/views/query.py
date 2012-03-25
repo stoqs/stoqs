4efb7a34ba552813128164226d1de3d7acd2a407
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-03-24 21:15:09 -0700

    Factored query methods out of views/__init__.py.  Removed moqua refs from activitiesWMS.html.

---
+++ stoqs/views/query.py
@@ -0,0 +1,63 @@
+__author__    = 'Mike McCann'
+__copyright__ = '2011'
+__license__   = 'GPL v3'
+__contact__   = 'mccann at mbari.org'
+
+__doc__ = '''
+
+View functions to supoprt the main query web page
+
+@undocumented: __doc__ parser
+@status: production
+@license: GPL
+'''
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.utils import simplejson
+from django.core import serializers
+from utils.STOQSQManager import STOQSQManager
+from utils import encoders
+
+import logging 
+import wms
+
+logger = logging.getLogger(__name__)
+
+
+def queryData(request, format=None):
+    response=HttpResponse()
+    query_parms={'parameters': 'parameters', # This should be specified once in the query string for each parameter.
+                 'time': ('start_time','end_time'), # Single values
+                 'depth': ('min_depth', 'max_depth'), # Single values
+                 'platforms': 'platforms', # Specified once in the query string for each platform.
+                 }
+    params={}
+    for key, value in query_parms.iteritems():
+        if type(value) in (list, tuple):
+            params[key]=[request.GET.get(p, None) for p in value]
+        else:
+            params[key]=request.GET.getlist(key)
+    
+    qm=STOQSQManager(request, response, request.META['dbAlias'])
+    qm.buildQuerySet(**params)
+    
+    if not format: # here we export in a given format, or just provide summary data if no format is given.
+        response['Content-Type']='text/json'
+        response.write(simplejson.dumps(qm.generateOptions(),
+                                        cls=encoders.STOQSJSONEncoder))
+                                        # use_decimal=True) # After json v2.1.0 this can be used instead of the custom encoder class.
+    elif format == 'json':
+        response['Content-Type']='text/json'
+        response.write(serializers.serialize('json', qm.qs))
+    return response
+    
+def queryUI(request):
+    formats={'csv': 'Comma-Separated Values (CSV)',
+             'DAP': 'OPeNDAP Format',
+             'KML': 'KML (Google Earth)'}
+    return render_to_response('stoqsquery.html', {'site_uri': request.build_absolute_uri('/')[:-1],
+                                                  'formats': formats,}, 
+                            context_instance=RequestContext(request))
+
