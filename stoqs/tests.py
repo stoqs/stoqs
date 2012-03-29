1828ae3b742d8dc3edc5e477068e454d9c3bb967
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-03-28 21:23:59 -0700

    No need to test for JSON response in queryUI().  All tests pass now.

--- stoqs/tests.py
+++ stoqs/tests.py
@@ -137,7 +137,6 @@ class BaseAndMeasurementViewsTestCase(TestCase):
     def test_query_ui(self):
         req = reverse('stoqs-query-ui', kwargs={'dbAlias': 'default'})
         response = self.client.get(req)
-        json.loads(response.content) # Verify we don't get an exception when open the main query page
         self.assertEqual(response.status_code, 200, 'Status code should be 200 for %s' % req)
    
     # Measurement view tests 
