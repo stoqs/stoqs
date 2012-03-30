e97567f1f6ea01f0868c56cb39a28fea54fcf75d
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-03-30 16:31:03 -0700

    Add STATIC_ROOT value.  Adjust for new location of the static dir - inside the stoqs project.

--- settings.py
+++ settings.py
@@ -139,11 +139,11 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/html/stoqs/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/stoqs/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -155,7 +155,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(project_dir, 'static'),
+    os.path.join(project_dir, 'stoqs/static'),
 )
 
 # List of finder classes that know how to find static files in
