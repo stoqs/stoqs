e963c3be4395ec1125e46e29492394b0f7badc8c
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-04-16 21:01:59 -0700

    SCHEMA CHANGES: Added SampleType, connected SimpleDepthTime to InstantPoint and CampaignLog to Resource as well as Sample to Resrouce.  Added fields to CampaignLog to match ObsLog fields.

--- stoqs/models.py
+++ stoqs/models.py
@@ -37,7 +37,6 @@ class UUIDField(models.CharField) :
     '''
     Major classes in the model have been given a uuid field, which may prove helpful as web accessible resource identifiers.
     '''
-    
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = kwargs.get('max_length', 32 )
         models.CharField.__init__(self, *args, **kwargs)
@@ -52,13 +51,43 @@ class UUIDField(models.CharField) :
         else:
             return super(models.CharField, self).pre_save(model_instance, add)
 
+class ResourceType(models.Model):
+    '''
+    Type of Resource. Example names: nc_global, quick-look-plot.
+    '''
+    uuid = UUIDField(editable=False)
+    name = models.CharField(max_length=128, db_index=True, unique=True)
+    description = models.CharField(max_length=256, blank=True, null=True)
+    objects = models.GeoManager()
+    class Meta:
+        app_label = 'stoqs'
+        def __str__(self):
+            return "%s" % (self.name,)
+
+class Resource(models.Model):
+    '''
+    A catchall class for saving any bit of information that may be associated with an Activity, or other STOQS model class.
+    This is useful for collecting web resources that may be shown in a popup window for an activity.  Examples include: NC_GLOBAL data set
+    attributes or quick-look plots.  The ResoureType class may be used to help categorize the display of resources.
+    '''
+    uuid = UUIDField(editable=False)
+    name = models.CharField(max_length=128, null=True)
+    value = models.TextField(null=True)
+    resourcetype = models.ForeignKey(ResourceType, blank=True, null=True, default=None)
+    uristring = models.CharField(max_length=256, null=True)
+    objects = models.GeoManager()
+    class Meta:
+        verbose_name = 'Resource'
+        verbose_name_plural = 'Resources'
+        app_label = 'stoqs'
+        def __str__(self):
+            return "%s" % (self.name,)
 
 class Campaign(models.Model):
     '''
     A Campaign holds a collection of Activities and can have a name, description and start and end time.  
     An example name is "CANON October 2010".
     '''
-
     uuid = UUIDField(editable=False)
     name = models.CharField(max_length=128, db_index=True, unique_for_date='startdate')
     description = models.CharField(max_length=4096, blank=True, null=True)
@@ -70,7 +99,7 @@ class Campaign(models.Model):
         verbose_name='Campaign'
         verbose_name_plural='Campaigns'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
         
 class CampaignLog(models.Model):
     '''
@@ -81,7 +110,11 @@ class CampaignLog(models.Model):
     uuid = UUIDField(editable=False)
     campaign = models.ForeignKey(Campaign)
     timevalue = models.DateTimeField(db_index=True)
-    message = models.CharField(max_length=2048)
+    geom = models.PointField(srid=4326, spatial_index=True, dim=2, blank=True, null=True)
+    depth = models.FloatField(blank=True, null=True)
+    username = models.CharField(max_length=128, blank=True, null=True)
+    description = models.CharField(max_length=2048)
+    resource = models.ForeignKey(Resource, blank=True, null=True)
     objects = models.GeoManager()
     class Meta:
         app_label = 'stoqs'
@@ -92,7 +125,6 @@ class ActivityType(models.Model):
     '''
     Type of Activity.  Example names: AUV Survey, Mooring Deployment, Ship Cruse, GLider Mission.
     '''
-
     uuid = UUIDField(editable=False)
     name = models.CharField(max_length=128, db_index=True, unique=True)
     objects = models.GeoManager()
@@ -101,26 +133,24 @@ class ActivityType(models.Model):
         verbose_name_plural='Activity Types'
         app_label = 'stoqs'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
 
 class PlatformType(models.Model):
     '''
     Type of platform. Example names: auv, mooring, drifter, ship.
     '''
-
     uuid = UUIDField(editable=False)
     name = models.CharField(max_length=128, db_index=True, unique=True)
     objects = models.GeoManager()
     class Meta:
         app_label = 'stoqs'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
 
 class Platform(models.Model):
     '''
     Platform.  Example names (use lower case): dorado, tethys, martin.
     '''
-
     uuid = UUIDField(editable=False)
     name = models.CharField(max_length=128)
     platformtype = models.ForeignKey(PlatformType) 
@@ -130,7 +160,7 @@ class Platform(models.Model):
         verbose_name_plural = 'Platforms'
         app_label = 'stoqs'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
 
 class Activity(models.Model):
     '''
@@ -138,7 +168,6 @@ class Activity(models.Model):
     Dorado389_2011_117_01_117_01_decim.nc (stride=10), 
     20110415_20110418/20110418T192351/slate.nc (stride=10), 27710_jhmudas_v1.nc (stride=1).
     '''
-
     uuid = UUIDField(editable=False)
     campaign = models.ForeignKey(Campaign, blank=True, null=True, default=None) 
     platform = models.ForeignKey(Platform) 
@@ -160,19 +189,6 @@ class Activity(models.Model):
     def __str__(self):
         return "%s" % (self.name,)
 
-class SimpleDepthTime(models.Model):
-    '''
-    A simplified time series of depth values for an Activity
-    '''
-    activity = models.ForeignKey(Activity) 
-    timevalue = models.DateTimeField(db_index=True)
-    depth= models.FloatField()
-    objects = models.GeoManager()
-    class Meta:
-        verbose_name='Simple depth time series'
-        verbose_name_plural='Simple depth time series'
-        app_label = 'stoqs'
-
 class InstantPoint(models.Model):
     '''
     An instance in time for an Activity.  This InstantPoint may have a measurement or sample associated with it.
@@ -183,12 +199,25 @@ class InstantPoint(models.Model):
     class Meta:
         app_label = 'stoqs'
 
+class SimpleDepthTime(models.Model):
+    '''
+    A simplified time series of depth values for an Activity useful for plotting in the UI
+    '''
+    activity = models.ForeignKey(Activity) 
+    instantpoint = models.ForeignKey(InstantPoint)
+    epochmilliseconds = models.FloatField()
+    depth= models.FloatField()
+    objects = models.GeoManager()
+    class Meta:
+        verbose_name='Simple depth time series'
+        verbose_name_plural='Simple depth time series'
+        app_label = 'stoqs'
+
 class Parameter(models.Model):
     '''
     A Parameter is something that can be measured producing a numeric value.  Example names include: 
     temperature, salinity, fluoresence.
     '''
-
     uuid = UUIDField(editable=False)
     name = models.CharField(max_length=128, unique=True)
     type = models.CharField(max_length=128, blank=True, null=True)
@@ -203,7 +232,7 @@ class Parameter(models.Model):
         verbose_name_plural = 'Parameters'
         app_label = 'stoqs'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
 
 class ParameterGroup(models.Model):
     '''
@@ -219,7 +248,7 @@ class ParameterGroup(models.Model):
         verbose_name_plural = 'ParameterGroups'
         app_label = 'stoqs'
         def __str__(self):
-                return "%s" % (self.name,)
+            return "%s" % (self.name,)
 
 class ParameterGroupParameter(models.Model):
     '''
@@ -234,42 +263,9 @@ class ParameterGroupParameter(models.Model):
         app_label = 'stoqs'
         unique_together = ['parametergroup', 'parameter']
 
-class ResourceType(models.Model):
-    '''
-    Type of Resource. Example names: nc_global, quick-look-plot.
-    '''
-
-    uuid = UUIDField(editable=False)
-    name = models.CharField(max_length=128, db_index=True, unique=True)
-    description = models.CharField(max_length=256, blank=True, null=True)
-    objects = models.GeoManager()
-    class Meta:
-        app_label = 'stoqs'
-        def __str__(self):
-                return "%s" % (self.name,)
-
-class Resource(models.Model):
-    '''
-    A catchall class for saving any bit of information that may be associated with an Activity, or other STOQS model class.
-    This is useful for collecting web resources that may be shown in a popup window for an activity.  Examples include: NC_GLOBAL data set
-    attributes or quick-look plots.  The ResoureType class may be used to help categorize the display of resources.
-    '''
-    uuid = UUIDField(editable=False)
-    name = models.CharField(max_length=128, null=True)
-    value = models.TextField(null=True)
-    resourcetype = models.ForeignKey(ResourceType, blank=True, null=True, default=None)
-    uristring = models.CharField(max_length=256, null=True)
-    objects = models.GeoManager()
-    class Meta:
-        verbose_name = 'Resource'
-        verbose_name_plural = 'Resources'
-        app_label = 'stoqs'
-        def __str__(self):
-                return "%s" % (self.name,)
-
 class ActivityResource(models.Model):
     '''
-    Association class pairing Activities and Resources.
+    Association class pairing Activities and Resources.  Must use explicit many-to-many and GeoManager does not support .add().
     '''
     uuid = UUIDField(editable=False)
     activity = models.ForeignKey(Activity)
@@ -285,7 +281,6 @@ class Measurement(models.Model):
     A Measurement may have a depth value (this is an Oceanographic Query System) and a location (represented by the geom field), 
     be associated with an InstantPoint and and a MeasuredParameter (where the measured datavalue is stored).
     '''
-
     instantpoint = models.ForeignKey(InstantPoint)
     depth= models.DecimalField(max_digits=100, db_index=True, decimal_places=30)
     geom = models.PointField(srid=4326, spatial_index=True, dim=2)
@@ -295,7 +290,21 @@ class Measurement(models.Model):
         verbose_name_plural = 'Measurements'
         app_label = 'stoqs'
         def __str__(self):
-                return "Measurement at %s" % (self.geom,)
+            return "Measurement at %s" % (self.geom,)
+
+class SampleType(models.Model):
+    '''
+    Type of Sample.  Example names: Gulper, Niskin, Bucket
+    '''
+    uuid = UUIDField(editable=False)
+    name = models.CharField(max_length=128, db_index=True, unique=True)
+    objects = models.GeoManager()
+    class Meta:
+        verbose_name='Sample Type'
+        verbose_name_plural='Sample Types'
+        app_label = 'stoqs'
+        def __str__(self):
+            return "%s" % (self.name,)
 
 class Sample(models.Model):
     '''
@@ -304,18 +313,31 @@ class Sample(models.Model):
     differs from a Measurement in that it represents an actual physical sample from which analyses may be made producing
     digital values which may be stored in the SampleParameter table.
     '''
-
     instantpoint = models.ForeignKey(InstantPoint)
     depth= models.DecimalField(max_digits=100, db_index=True, decimal_places=30)
     geom = models.PointField(srid=4326, spatial_index=True, dim=2)
     name = models.CharField(max_length=128, db_index=True)
+    sampletype = models.ForeignKey(SampleType, blank=True, null=True, default=None) 
     objects = models.GeoManager()
     class Meta:
         verbose_name = 'Sample'
         verbose_name_plural = 'Samples'
         app_label = 'stoqs'
         def __str__(self):
-                return "Sample at %s" % (self.geom,)
+            return "Sample at %s" % (self.geom,)
+
+class SampleResource(models.Model):
+    '''
+    Association class pairing Samples and Resources.  Must use explicit many-to-many and GeoManager does not support .add().
+    '''
+    uuid = UUIDField(editable=False)
+    sample = models.ForeignKey(Sample)
+    resource = models.ForeignKey(Resource)
+    class Meta:
+        verbose_name = 'Sample Resource'
+        verbose_name_plural = 'Sample Resource'
+        app_label = 'stoqs'
+        unique_together = ['sample', 'resource']
 
 class ActivityParameter(models.Model):
     '''
