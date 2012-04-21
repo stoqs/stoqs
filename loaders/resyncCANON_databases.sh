6d0f4226e907aa92680ed21d0e91c55dd6c5383e
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-04-20 17:35:49 -0700

    Master scripts to recreate and resync the databases for the CANON campaigns.  USE WITH CAUTION.  But they save a lot of command entry when you just need to reload everything.

---
+++ loaders/resyncCANON_databases.sh
@@ -0,0 +1,7 @@
+#python manage.py syncdb --noinput --database=stoqs 
+python manage.py syncdb --noinput --database=stoqs_sept2010
+python manage.py syncdb --noinput --database=stoqs_oct2010
+python manage.py syncdb --noinput --database=stoqs_april2011
+python manage.py syncdb --noinput --database=stoqs_june2011
+python manage.py syncdb --noinput --database=stoqs_September2011
+python manage.py syncdb --noinput --database=stoqs_October2011
