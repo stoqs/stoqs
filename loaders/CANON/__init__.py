f76f2ff8face44d3e8cc62fbc2f4e12cbe042a88
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-04-22 21:53:21 -0700

    An O-O replacement for the separate loadCANON_*Data.py scripts one directory up.  All the netCDF files are defined in loadCANON_all.py and the CANONLoader class is in __init__.py.  Now using full month names for the databases.

---
+++ loaders/CANON/__init__.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+__author__    = 'Mike McCann'
+__copyright__ = '2011'
+__license__   = 'GPL v3'
+__contact__   = 'mccann at mbari.org'
+
+__doc__ = '''
+
+Contains class for common routines for loading all CANON data
+
+Mike McCann
+MBARI 22 April 2012
+
+@undocumented: __doc__ parser
+@status: production
+@license: GPL
+'''
+
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+project_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # settings.py is one dir up
+
+
+import DAPloaders
+import GulperLoader
+
+
+class CANONLoader(object):
+    '''
+    Common routines for loading all CANON data
+    '''
+    stride = 1
+    def __init__(self, dbAlias, campaignName):
+        self.dbAlias = dbAlias
+        self.campaignName = campaignName
+
+
+    def loadDorado(self):
+        '''
+        Dorado specific load functions
+        '''
+        for (aName, file) in zip([ a + ' (stride=%d)' % self.stride for a in self.dorado_files], self.dorado_files):
+            url = self.dorado_base + file
+            print url
+            DAPloaders.runDoradoLoader(url, self.campaignName, aName, 'dorado', 'auv', 'AUV mission', self.dbAlias, self.stride)
+            GulperLoader.load_gulps(aName, file, self.dbAlias)
+
+
+    def loadTethys(self):
+        '''
+        Tethys specific load functions
+        '''
+        for (aName, file) in zip([ a + ' (stride=%d)' % self.stride for a in self.tethys_files], self.tethys_files):
+            url = self.tethys_base + file
+            DAPloaders.runLrauvLoader(url, self.campaignName, aName, 'tethys', 'auv', 'AUV mission', self.tethys_parms, self.dbAlias, self.stride)
+
+    def loadMartin(self):
+        '''
+        Martin specific load functions
+        '''
+        for (aName, file) in zip([ a + ' (stride=%d)' % self.stride for a in self.martin_files], self.martin_files):
+            url = self.martin_base + file
+            DAPloaders.runAuvctdLoader(url, self.campaignName, aName, 'martin', 'ship', 'cruise', self.martin_parms, self.dbAlias, self.stride)
+
+
+    def loadAll(self):
+        '''
+        Execute all the load functions
+        '''
+        try:
+            self.loadDorado()
+        except AttributeError:
+            pass
+        try:
+            self.loadTethys()
+        except AttributeError:
+            pass
+        try:
+            self.loadMartin()
+        except AttributeError:
+            pass
+
+if __name__ == '__main__':
+    '''
+    Test operation of this class
+    '''
+    cl = CANONLoader('default', 'Test Load')
+    cl.stride = 1000
+    cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2010/netcdf/'
+    cl.dorado_files = ['Dorado389_2010_300_00_300_00_decim.nc']
+    cl.loadAll()
+
+
--- loaders/CANON/resyncCANON_databases.sh
+++ loaders/CANON/resyncCANON_databases.sh
@@ -1,7 +1,7 @@
 #python manage.py syncdb --noinput --database=stoqs 
-python manage.py syncdb --noinput --database=stoqs_sept2010
-python manage.py syncdb --noinput --database=stoqs_oct2010
+python manage.py syncdb --noinput --database=stoqs_september2010
+python manage.py syncdb --noinput --database=stoqs_october2010
 python manage.py syncdb --noinput --database=stoqs_april2011
 python manage.py syncdb --noinput --database=stoqs_june2011
-python manage.py syncdb --noinput --database=stoqs_September2011
-python manage.py syncdb --noinput --database=stoqs_October2011
+python manage.py syncdb --noinput --database=stoqs_september2011
+python manage.py syncdb --noinput --database=stoqs_october2011
