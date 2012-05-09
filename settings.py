d1515efcab180c6fabd26a7e4d6382e3f0ace234
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-05-09 01:00:25 -0700

    Change TIME_ZONE from 'Antarctica/Vostok' to 'GMT'.  This is what MBARITracking uses and it works, avoiding one of the variables affecting the time values.

--- settings.py
+++ settings.py
@@ -113,7 +113,7 @@ DATABASE_ROUTERS = ['stoqs.db_router.DatabaseRouter']
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Antarctica/Vostok'
+TIME_ZONE = 'GMT'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
