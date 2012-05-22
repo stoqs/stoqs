ba80a17db74b4b60e68cabe29b82de58c3e8bcf5
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-05-21 23:51:56 -0700

    Added support for database routing by adding dbalias in the appropriate places.  Persisting Sample record if a gulper_id measurement is seen.

--- loaders/CANON/realtime/persistTrex.py
+++ loaders/CANON/realtime/persistTrex.py
@@ -15,6 +15,7 @@ project_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../../../"))  # settings.py is three dirs up
 from django.conf import settings
 from stoqs import models as m
+from django.db.utils import IntegrityError
 
 logger = logging.getLogger('__main__')
 logger.setLevel(logging.DEBUG)
@@ -31,13 +32,13 @@ class Consumer():
     Credentials are read in from privateSettings
     '''
 
-    def __init__(self, vhost = 'trackingvhost', exchange_name = '', exchange_type = '', queue_name = '', routing_key = '', persistFlag = False):
+    def __init__(self, vhost = 'trackingvhost', exchange_name = '', exchange_type = '', queue_name = '', routing_key = '', dbAlias = ''):
         self.vhost = vhost
         self.exchange_name = exchange_name
         self.exchange_type = exchange_type
         self.queue_name = queue_name
         self.routing_key = routing_key
-        self.persistFlag = persistFlag
+        self.dbAlias = dbAlias
 
         (self.connection, self.channel) = self.create_connection_and_channel(vhost)
 
@@ -51,21 +52,16 @@ class Consumer():
         ##amqp_host = 'messaging.shore.mbari.org:5672'
         amqp_host = '%s:%s' % (settings.RABBITMQ_TRACKING_HOST, settings.RABBITMQ_TRACKING_PORT,)
         logger.debug(amqp_host)
-        raw_input('paused')
-        if vhost == 'canonvhost':
-            connection = amqp.Connection( host = amqp_host, 
-                userid = "canon", 
-                password = "canon", 
-                virtual_host = "canonvhost", 
-                insist = False )
-        elif vhost == 'trackingvhost':
+        # It seems somewhat meaningless to require vhost when (for now) the only allowed value is 'trackingvhost', but
+        # the logic is kept here in case there is another virtual host that we may connect to.
+        if vhost == 'trackingvhost':
             connection = amqp.Connection( host = amqp_host,
                 userid = settings.RABBITMQ_TRACKING_USER,
                 password = settings.RABBITMQ_TRACKING_PASSWORD,
                 virtual_host = settings.RABBITMQ_TRACKING_VHOST,
                 insist = False )
         else:
-            print 'vhost must be either canonvhost or trackingvhost.'
+            logger.error('vhost must be trackingvhost')
             sys.exit(1)
 
         # Create our channel
@@ -81,19 +77,19 @@ class Consumer():
         print "persistMessage(): Length of message.body = %i" % len(message.body)
         sm.ParseFromString(message.body)
         i = 0
-        measVars = ['temperature', 'salinity', 'nitrate']
+        measVars = ['temperature', 'salinity', 'nitrate', 'gulper_id']
         for s in sm.sample:
             i += 1
             print 20*'-'
-            ##print "%d. %s" % (i, s)
+            print "%d. %s" % (i, s)
             # Assume that every sample has utime, easting, northing, and depth (not every sample has all of the state variables)
             for mv in measVars:
                 if s.HasField(mv):
-                    ##print "utime = %d" % s.utime
+                    logger.debug("utime = %d", s.utime)
                     dt = datetime.datetime.fromtimestamp(s.utime)
                     print "dt = %s" % dt
-                    ##print "easting = %f" % s.easting
-                    ##print "northing = %f" % s.northing
+                    logger.debug("easting = %f", s.easting)
+                    logger.debug("northing = %f", s.northing)
                     (lon, lat) = self.utmProj(s.easting, s.northing, inverse = True)
                     print "lat = %f" % lat
                     print "lon = %f" % lon
@@ -101,15 +97,21 @@ class Consumer():
                     print "depth = %f" % s.depth
                     value = s.__getattribute__(mv)
                     print "%s = %f" % (mv, value)
+        
+                    if mv == 'gulper_id':
+                        logger.info('>>> gulper_id = %s', value)
+                        self.persistSample(dt, s.depth, lat, lon, mv, value)
+                        raw_input('paused')
+                    else:
+                        try:
+                            self.persistMeasurement(dt, s.depth, lat, lon, mv, value)
+                        except Exception, e:
+                            logger.error("ERROR: *** Could not persist this measurement.  Is something wrong with PostgreSQL?  See details below. ***\n")
+                            logger.error(e)
+                            traceback.print_exc(file = sys.stdout)
+                            ##sys.exit(-1)
+                            print "Continuing on with processing messages..."
 
-                    try:
-                        self.persistMeasurement(dt, s.depth, lat, lon, mv, value)
-                    except Exception, e:
-                        print "ERROR: *** Could not persist this measurement.  Is something wrong with PostgreSQL?  See details below. ***\n"
-                        print e
-                        traceback.print_exc(file = sys.stdout)
-                        sys.exit(-1)
-                        ##print "Continuing on with processing messages..."
 
                 print ''
 
@@ -120,32 +122,52 @@ class Consumer():
             ##print "Mailing message to driftertrack with command:\n%s" % cmd
             ##os.system(cmd);
 
+        # Update Activity attributes with info that stoqs/query needs
+        try:
+            self.updateMaptrack()
+            self.updateSimpleDepthTime()
+        except Exception, e:
+            logger.error("ERROR: *** Could not update MapTrack or SimpleDepthTime.  Is something wrong with PostgreSQL?  See details below. ***\n")
+            logger.error(e)
+            traceback.print_exc(file = sys.stdout)
+            sys.exit(-1)
+
 
     def persistMeasurement(self, dt, depth, lat, lon, var, value):
         '''Call all of the create_ methods to properly persist this measurement in STOQS'''
 
         try:
-            (parm, created) = m.Parameter.objects.get_or_create(name = var)
+            (parm, created) = m.Parameter.objects.using(self.dbAlias).get_or_create(name = var)
         except Exception, e:
             print "ERROR: *** Could not get_or_create name = '%s'.  See details below. ***\n" % var
             print e
             traceback.print_exc(file = sys.stdout)
-            sys.exit(-1)
+            print "Continuing on with processing messages..."
+            ##sys.exit(-1)
 
         meas = self.createMeasurement(dt, depth, lat, lon)
         mp = m.MeasuredParameter(measurement = meas,
                     parameter = parm,
                     datavalue = str(value))
         try:
-            mp.save()
+            mp.save(using=self.dbAlias)
         except IntegrityError, e:
-            print "WARNING: Probably a duplicate measurement that could not be added to the DB.  Skipping it.\n" % var
-            print e
+            logger.error("WARNING: Probably a duplicate measurement that could not be added to the DB.  Skipping it.\n", var)
+            logger.error(e)
         else:
             print "saved %s = %f at %s, %f, %f, %f" % (parm, value, dt, depth, lat, lon)
 
         return 
 
+    def persistSample(self, dt, depth, lat, lon, var, value):
+        '''Call all of the create_ methods to properly persist this sample in STOQS'''
+
+        sample = self.createSample(dt, depth, lat, lon, value)
+
+        if sample:
+            print "saved sample = %s" % (sample,)
+
+        return 
 
     def createActivity(self, platformName, platformType, activityName, activityType):
         '''
@@ -153,15 +175,15 @@ class Consumer():
         Before creating the Activity we also need to get_or_create a Platform and PlatformType.
         '''
 
-        (platformType, created) = m.PlatformType.objects.get_or_create(name = platformType)
+        (platformType, created) = m.PlatformType.objects.using(self.dbAlias).get_or_create(name = platformType)
         self.platformType = platformType
 
-        (platform, created) = m.Platform.objects.get_or_create(name = platformName, platformtype = platformType)
+        (platform, created) = m.Platform.objects.using(self.dbAlias).get_or_create(name = platformName, platformtype = platformType)
         self.platform = platform
 
-        (activityType, created) = m.ActivityType.objects.get_or_create(name = activityType)
+        (activityType, created) = m.ActivityType.objects.using(self.dbAlias).get_or_create(name = activityType)
         self.activityType = activityType
-        (activity, created) = m.Activity.objects.get_or_create(name = activityName,
+        (activity, created) = m.Activity.objects.using(self.dbAlias).get_or_create(name = activityName,
                     platform = self.platform,
                     startdate = datetime.datetime(2011,4,20,0,0,0),     # Hard-coded start & end times
                     enddate = datetime.datetime(2011,4,28,0,0,0))       # For April 2011 CANON activities
@@ -170,7 +192,7 @@ class Consumer():
         if self.activityType is not None:   
             self.activity.activitytype = self.activityType
 
-        self.activity.save()
+        self.activity.save(using=self.dbAlias)
 
     def createMeasurement(self, time, depth, lat, long):
         '''
@@ -184,12 +206,37 @@ class Consumer():
         @param long: The longitude (degrees, assumed WGS84) for the measurement
         @return: An instance of stoqs.models.Measurement
         '''
-        (ip, created) = m.InstantPoint.objects.get_or_create(activity = self.activity, timevalue = time)
+        (ip, created) = m.InstantPoint.objects.using(self.dbAlias).get_or_create(activity = self.activity, timevalue = time)
 
         point = 'POINT(%s %s)' % (repr(long), repr(lat))
-        (measurement, created) = m.Measurement.objects.get_or_create(instantpoint = ip, depth = repr(depth), geom = point)
+        (measurement, created) = m.Measurement.objects.using(self.dbAlias).get_or_create(instantpoint = ip, depth = repr(depth), geom = point)
 
         return measurement
+
+    def createSample(self, time, depth, lat, long, value):
+        '''
+        Create and return a sample object in the database.  The sample object
+        is created by first creating an instance of stoqs.models.Instantpoint using the activity, 
+        then by creating an instance of Sample using the Instantpoint.  A reference to 
+        an instance of a stoqs.models.Sample object is then returned.
+        @param time: A valid datetime instance of a datetime object used to create the Instantpoint
+        @param depth: The depth for the sample
+        @param lat: The latitude (degrees, assumed WGS84) for the sample
+        @param long: The longitude (degrees, assumed WGS84) for the sample
+        @return: An instance of stoqs.models.Sample
+        '''
+        (ip, created) = m.InstantPoint.objects.using(self.dbAlias).get_or_create(activity = self.activity, timevalue = time)
+
+        point = 'POINT(%s %s)' % (repr(long), repr(lat))
+        (sample, created) = m.Sample.objects.using(self.dbAlias).get_or_create(instantpoint = ip, depth = repr(depth), geom = point, name = value)
+
+        return sample
+
+    def updateMaptrack(self):
+        pass
+
+    def updateSimpleDepthTime(self):
+        pass
 
     def signalHandler(self, signum, frame):
         '''Throw exceptoin so as to gracefully close the channel if the process is killed.'''
@@ -223,7 +270,7 @@ class Consumer():
 
         # Let AMQP know to send us messages
         # Instantiate message object setting flag on whhether to use Protocol Buffers or not
-        if self.persistFlag:
+        if self.dbAlias:
             consumer_tag = self.channel.basic_consume( queue = self.queue_name, 
                 no_ack = True,
                 callback = self.persistMessage )
@@ -261,19 +308,19 @@ class Consumer():
         print "RabbitMQ connection closed."
 
 
-def deleteTestMessages(platformName, platformType, activityName, activityType):
+def deleteTestMessages(platformName, platformType, activityName, activityType, dbAlias):
     '''For testing query stoqs for Measurements that match the activity and delete them from the data base.
     '''
 
-    activity = m.Activity.objects.filter(name = activityName, activitytype__name = activityType)
-    qs = m.MeasuredParameter.objects.filter(measurement__instantpoint__activity__name = activityName,
+    activity = m.Activity.objects.using(dbAlias).filter(name = activityName, activitytype__name = activityType)
+    qs = m.MeasuredParameter.objects.using(dbAlias).filter(measurement__instantpoint__activity__name = activityName,
                         measurement__instantpoint__activity__activitytype__name = activityType)
 
     if activity:
         ans = raw_input("Going to delete Activity %s and all %i measurements from it, O.K.? [N/y] " % (activity, qs.count()))
 
         if ans.upper() == 'Y':
-            activity.delete()
+            activity.delete(using=dbAlias)
             print "Activity deleted."
 
 
@@ -282,7 +329,7 @@ if __name__ == '__main__':
 
     parser = OptionParser(usage="""\
 
-Synopsis: %prog --en <exchange_name> --et <exchange_type> --qn<queue_name> [--rk <routing_key> --vh <vhost> --persist --testPersist]
+Synopsis: %prog --en <exchange_name> --et <exchange_type> --qn<queue_name> [--rk <routing_key> --vh <vhost> --persist <dbAlias> --testPersist <dbAlias>]
 
 Starts a consumer for an AMQP exchange.  If the --persist option is specified then persist the
 sensor track information in the messages to a STOQS database that is configured via Django.
@@ -290,13 +337,13 @@ sensor track information in the messages to a STOQS database that is configured 
 Examples: 
 
    To create a new queue on the fanout exchange to display the sensor data:
-     % consumer.py --en SensorMessagesFromTrex --et fanout --qn persist_trex --vh trackingvhost
+     % persistTrex.py --en SensorMessagesFromTrex --et fanout --qn medusa_persist_trex --vh trackingvhost
 
    To persist messages to the Postgres database defined in settings.py:
-     % consumer.py --en SensorMessagesFromTrex --et fanout --qn persist_trex --vh trackingvhost --persist
+     % persistTrex.py --en SensorMessagesFromTrex --et fanout --qn odss-staging_persist_trex --vh trackingvhost --persist stoqs_may2012_r
 
-   To test with a save Google Protobuf message:
-     % consumer.py --testPersist
+   To test with a saved Google Protobuf message:
+     % persistTrex.py --testPersist stoqs_may2012_r
 
 """)
 
@@ -316,11 +363,11 @@ Examples:
         type='string', action='store',
         help="Specify Virtual Host Key")
     parser.add_option('', '--persist',
-        action='store_true', default=False,
-        help="Instead of simply echoing the message to stdout, write them to the database.")
+        type='string', action='store',
+        help="Specify dbAlias from the settings file where the data need to be persisted")
     parser.add_option('', '--testPersist',
-        action='store_true', default=False,
-        help="Run a test to persist a saved message rather than connect to a queue.")
+        type='string', action='store',
+        help="Run a test to persist a saved message to dbAlias rather than connect to a queue.")
 
     opts, args = parser.parse_args()
 
@@ -335,10 +382,11 @@ Examples:
             def __init__(self, body):
                 self.body = body
 
-        file = '/home/stoqsadm/dev/MBARItracking/sensortracks/fredsProtoBuf_1.sbd'
+        ##file = 'test_trex_pb_msg_300025010809770_002294.sbd'
+        file = 'test_gulper_msg_300025010809770_002324.sbd'
         fh = open(file)
-        deleteTestMessages('trex', 'auv', 'test_unassigned', 'test_AUV_mission')
-        c = Consumer()
+        deleteTestMessages('trex', 'auv', 'test_unassigned', 'test_AUV_mission', opts.testPersist)
+        c = Consumer(dbAlias = opts.testPersist)
         c.createActivity('trex', 'auv', 'test_unassigned', 'test_AUV_mission')
         c.persistMessage(Message(fh.read()))
         fh.close()
@@ -350,7 +398,7 @@ Examples:
 
     # Create Consumer object
     c = Consumer(vhost = opts.vh, exchange_name = opts.en, exchange_type = opts.et, queue_name = opts.qn, 
-        routing_key = opts.rk, persistFlag = opts.persist)
+        routing_key = opts.rk, dbAlias = opts.persist)
 
     # Create a dummy activity for this realtime data (paramaters set inside the method)
     c.createActivity('trex', 'auv', 'unassigned', 'AUV_mission')
