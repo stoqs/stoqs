705272421fe1156b0936d436b470224c72d53e27
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-05-25 12:14:39 -0700

    Added Dorado surveys Dorado389_2012_142_01_142_01_decim.nc and Dorado389_2012_142_02_142_02_decim.nc

--- loaders/CANON/loadCANON_may2012.py
+++ loaders/CANON/loadCANON_may2012.py
@@ -38,15 +38,22 @@ except IndexError:
 # ----------------------------------------------------------------------------------
 cl = CANONLoader(dbAlias, 'CANON - May 2011')
 cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2012/netcdf/'
-cl.dorado_files = [ 'Dorado389_2012_143_07_143_07_decim.nc',
+cl.dorado_files = [ 
+                    'Dorado389_2012_142_01_142_01_decim.nc',
+                    'Dorado389_2012_142_02_142_02_decim.nc',
+                    'Dorado389_2012_143_07_143_07_decim.nc',
                     'Dorado389_2012_143_08_143_08_decim.nc',
                   ]
+
 cl.tethys_base = 'http://elvis.shore.mbari.org:8080/thredds/dodsC/lrauv/tethys/2012/'
 cl.tethys_files = [ 
                   ]
 cl.tethys_parms = [ 'sea_water_temperature', 'sea_water_salinity', 'sea_water_density', 'volume_scattering_470_nm', 'volume_scattering_650_nm',
                     'volume_scattering_650_nm', 'mass_concentration_of_oxygen_in_sea_water', 'mole_concentration_of_nitrate_in_sea_water',
                     'mass_concentration_of_chlorophyll_in_sea_water']
+
+cl.martin_parms = []
+
 cl.stride = stride
 cl.loadAll()
 
