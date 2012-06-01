8875e7c0e8f68e900c2ce38fd4e057e3db7e0786
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-06-01 11:26:50 -0700

    Refining for may2012 data loads: added a startDatetime parameter to glider loader so as to specify a start date for the L_662 data.  (Fighting TDS garbled data :-().

--- loaders/CANON/loadCANON_may2012.py
+++ loaders/CANON/loadCANON_may2012.py
@@ -19,6 +19,7 @@ MBARI 22 May 2012
 
 import os
 import sys
+import datetime
 os.environ['DJANGO_SETTINGS_MODULE']='settings'
 project_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # settings.py is one dir up
@@ -46,14 +47,21 @@ cl.dorado_files = [
                     'Dorado389_2012_143_07_143_07_decim.nc',
                     'Dorado389_2012_143_08_143_08_decim.nc',
                     'Dorado389_2012_150_00_150_00_decim.nc',
+                    'Dorado389_2012_151_00_151_00_decim.nc',
                   ]
 
 cl.daphne_base = 'http://aosn.mbari.org/lrauvtds/dodsC/lrauv/daphne/2012/201205/'
 cl.daphne_files = [ 
                     '20120530T160348/shore.nc',
-                    '20120530T215940/shore.nc'
+                    '20120530T215940/shore.nc',
+                    '20120531T010135/shore.nc',
+                    '20120531T011043/shore.nc',
+                    '20120531T050931/shore.nc',
+                    '20120531T062937/shore.nc',
+                    '20120531T174058/shore.nc'
                   ]
-cl.daphne_parms = [ 'platform_battery_charge']
+cl.daphne_parms = [ 'platform_battery_charge', 'sea_water_temperature', 
+                    'mass_concentration_of_oxygen_in_sea_water', 'mass_concentration_of_chlorophyll_in_sea_water']
 
 # Tethys full resolution
 ##cl.tethys_base = 'http://elvis.shore.mbari.org:8080/thredds/dodsC/lrauv/tethys/2012/'
@@ -65,17 +73,23 @@ cl.tethys_base = 'http://aosn.mbari.org/lrauvtds/dodsC/lrauv/tethys/2012/201205/
 
 cl.martin_parms = []
 
-cl.nps_g29_base = 'http://www.cencoos.org/thredds/dodsC/glider/'
+# Garbled TDS response: 1 June 2012
+cl.nps_g29_base = 'http://www.cencoos.org:8080/thredds/dodsC/glider/'
 cl.nps_g29_files = ['OS_Glider_NPS_G29_20120524_TS.nc']
 cl.nps_g29_parms = ['TEMP', 'PSAL', 'OPBS']
 
-cl.l_662_base = 'http://www.cencoos.org/thredds/dodsC/glider/'
+# Zeros at end of time axis: 1 June 2012
+cl.l_662_base = 'http://www.cencoos.org:8080/thredds/dodsC/glider/'
 cl.l_662_files = ['OS_Glider_L_662_20120424_TS.nc']
 cl.l_662_parms = ['TEMP', 'PSAL', 'FLU2']
+cl.l_662_startDatetime = datetime.datetime(2012,5, 15)
 
 ##cl.waveglider = ''
 
 cl.stride = stride
 cl.loadAll()
+##cl.loadDaphne()
+##cl.loadNps_g29()
+##cl.loadL_662()
 
 
