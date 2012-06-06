68dcd685af186c9c6ef5653b4797538cb5ce6c44
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-06-06 15:53:10 -0700

    Added dorado, tethys, and daphne data from the last day.  The LRAUV data are garbled from the TDS, so holding off attempting to load them for now.

--- loaders/CANON/loadCANON_may2012.py
+++ loaders/CANON/loadCANON_may2012.py
@@ -43,13 +43,14 @@ cl = CANONLoader(dbAlias, 'CANON - May 2011')
 
 cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2012/netcdf/'
 cl.dorado_files = [ 
-                    'Dorado389_2012_142_01_142_01_decim.nc',
-                    'Dorado389_2012_142_02_142_02_decim.nc',
-                    'Dorado389_2012_143_07_143_07_decim.nc',
-                    'Dorado389_2012_143_08_143_08_decim.nc',
-                    'Dorado389_2012_150_00_150_00_decim.nc',
-                    'Dorado389_2012_151_00_151_00_decim.nc',
-                    'Dorado389_2012_152_00_152_00_decim.nc',
+##                    'Dorado389_2012_142_01_142_01_decim.nc',
+##                    'Dorado389_2012_142_02_142_02_decim.nc',
+##                    'Dorado389_2012_143_07_143_07_decim.nc',
+##                    'Dorado389_2012_143_08_143_08_decim.nc',
+##                    'Dorado389_2012_150_00_150_00_decim.nc',
+##                    'Dorado389_2012_151_00_151_00_decim.nc',
+##                    'Dorado389_2012_152_00_152_00_decim.nc',
+                    'Dorado389_2012_157_07_157_07_decim.nc',
                   ]
 
 cl.daphne_base = 'http://aosn.mbari.org/lrauvtds/dodsC/lrauv/daphne/2012/'
@@ -61,21 +62,28 @@ cl.daphne_files = [
 ##                    '201205/20120531T050931/shore.nc',
 ##                    '201205/20120531T062937/shore.nc',
 ##                    '201205/20120531T174058/shore.nc'
-                    '201206/20120601T235829/shore.nc',
-                    '201206/20120603T002455/shore.nc',
-                    '201206/20120603T200613/shore.nc',
-                    '201206/20120603T213551/shore.nc',
-                    '201206/20120604T211315/shore.nc',
+##                    '201206/20120601T235829/shore.nc',
+##                    '201206/20120603T002455/shore.nc',
+##                    '201206/20120603T200613/shore.nc',
+##                    '201206/20120603T213551/shore.nc',
+##                    '201206/20120604T211315/shore.nc',
+                    '201206/20120606T050637/shore.nc',
+                    '201206/20120606T094236/shore.nc',
                   ]
 cl.daphne_parms = [ 'platform_battery_charge', 'sea_water_temperature', 
                     'mass_concentration_of_oxygen_in_sea_water', 'mass_concentration_of_chlorophyll_in_sea_water']
 
 ##cl.tethys_base = 'http://elvis.shore.mbari.org:8080/thredds/dodsC/lrauv/tethys/2012/'         # Tethys full resolution
-##cl.tethys_base = 'http://aosn.mbari.org/lrauvtds/dodsC/lrauv/tethys/2012/201205/'             # Tethys realtime
 ##cl.tethys_parms = [ 'sea_water_temperature', 'sea_water_salinity', 'sea_water_density', 'volume_scattering_470_nm', 'volume_scattering_650_nm',
 ##                    'volume_scattering_650_nm', 'mass_concentration_of_oxygen_in_sea_water', 'mole_concentration_of_nitrate_in_sea_water',
 ##                    'mass_concentration_of_chlorophyll_in_sea_water']
 
+cl.tethys_base = 'http://aosn.mbari.org/lrauvtds/dodsC/lrauv/tethys/2012/'                    # Tethys realtime
+cl.tethys_files = [ 
+                    '201206/20120606T171537/shore.nc',
+                  ]
+cl.tethys_parms = [ 'platform_battery_charge', 'sea_water_temperature', 
+                    'mass_concentration_of_oxygen_in_sea_water', 'mass_concentration_of_chlorophyll_in_sea_water']
 
 cl.fulmar_base = []
 cl.fulmar_files = []
@@ -112,9 +120,12 @@ cl.waveglider_parms = [
 cl.stride = stride
 ##cl.loadAll()
 
-##cl.loadDaphne()
 ##cl.loadNps_g29()
 ##cl.loadL_662()
-cl.loadWaveglider()
+##cl.loadWaveglider()
+
+cl.loadDorado()
+##cl.loadTethys()
+##cl.loadDaphne()
 
 
