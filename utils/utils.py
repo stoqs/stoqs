8e7043697f9a1f79186e1a0cc2ae125dbf636b74
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-06-09 18:53:33 -0700

    Added round_to_n() for use in setting cmin & cmax for KML generation

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,16 @@
 # A collection of various utility functions
 
+def round_to_n(x, n):
+    '''
+    reound to n significant digits
+    '''
+    if n < 1:
+        raise ValueError("number of significant digits must be >= 1")
+    # Use %e format to get the n most significant digits, as a string.
+    format = "%." + str(n-1) + "e"
+    as_string = format % x
+    return float(as_string)
+
 def addAttributeToListItems(list_to_modify, name, value):
     '''
     For each item in list_to_modify, add new attribute name with value value.
