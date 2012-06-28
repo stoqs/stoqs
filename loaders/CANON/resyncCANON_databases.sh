9c391d79a1a2885134f265f067cd3ff9d5c883eb
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-06-28 00:08:05 -0700

    Set self.fields

---
+++ loaders/CANON/resyncCANON_databases.sh
@@ -0,0 +1,7 @@
+#python manage.py syncdb --noinput --database=stoqs 
+python manage.py syncdb --noinput --database=stoqs_september2010
+python manage.py syncdb --noinput --database=stoqs_october2010
+python manage.py syncdb --noinput --database=stoqs_april2011
+python manage.py syncdb --noinput --database=stoqs_june2011
+python manage.py syncdb --noinput --database=stoqs_september2011
+python manage.py syncdb --noinput --database=stoqs_october2011
