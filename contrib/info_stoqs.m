88a4c24757f151d9f056192fcb19a2ca3507c47a
Francisco Lopez <flcastej@gmail.com>
2012-07-02 14:13:51 -0400

    I change it. Now not screen ouput, is a function that  ouput the structure of the json response. This structure will be use in other function, as show_stoqs.

--- contrib/info_stoqs.m
+++ contrib/info_stoqs.m
@@ -1,33 +1,37 @@
-function inf = info_stoqs(u)
+function inf = info_stoqs(u,table)
 
-%Get info from a STOQS data basej, platform, date, variables,
-%       inf=info_stoqs('http://192.168.79.138:8000/default')
+%Get info from a table in STOQS data base
+%       inf=info_stoqs('http://192.168.79.138:8000/default','platform')
 %Usage:
 %
 %   t=info_stoqs(u)
 %Input :
 %   u = Url direction of the STOQS data server. Ex: http://192.168.79.138:8000/default
 %Output
-% Show in the screen all the basic information and get the struct variable
+% Get the struct variable
 % inf with all the information.
 % 
 
 %   Brian Schlining & Francisco Lopez
 %   30/June/2012
-
+% Last review 30/June/2012
 
 %Load the information
 
-ur=[u '/platform.json'];
-url = java.net.URL(ur); %Read de URL
+ur=[u '/' table '.json'];
+
+try
+    url = java.net.URL(ur);
+catch me
+    m = MException([mfilename ':BadURL'], '%s is not a valid URL. Cause: %s', u, me.message);
+    throw(m);
+end
+ 
 
 urlStream = url.openStream(); %Open it
 isr = java.io.InputStreamReader(urlStream);
 br = java.io.BufferedReader(isr);
 inf=loadjson(char(readLine(br))); %Read de information, Convert java.string to string with char, so loadjson could convert it to a struct.
 
-%Show the information
-l=length(inf(:));
-fprintf('%s\n','PLATAFORMAS');
-fprintf('%s\n',inf(1:l).name);
+
 
