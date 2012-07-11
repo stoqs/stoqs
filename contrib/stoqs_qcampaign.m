344f065d6c5d348d1133a16323c6175dc4d9a14d
Francisco Lopez <flcastej@gmail.com>
2012-07-11 19:39:17 -0400

    Add stoqs_qcampaign to get info about a campaign in STOQS

---
+++ contrib/stoqs_qcampaign.m
@@ -0,0 +1,29 @@
+function infc = stoqs_qcampaign(u,show)
+
+%Get info from a campaign in STOQS data base
+%       inf=stoqs_qcampaign('http://odss.mbari.org/canon/default');
+%Usage:
+%
+%   inf=stoqs_qcampaign('http://odss.mbari.org/canon/default',1);
+%Input :
+
+%   u = Url direction of the STOQS data server. Ex: http://odss.mbari.org/canon/default
+%   show = Show the info on the screen or not. If show=1 , show the info,
+%               if show=0 doesn't show it.
+
+%Output
+% Get the campaign available on the STOQS data server
+% 
+
+%   Brian Schlining & Francisco Lopez
+%   30/June/2012
+% Last review 30/July/2012
+
+infc=stoqs_info(u,'campaign');
+
+if show==1
+    for i=1:length(infc)  
+       fprintf('%s\n','CAMPAIGN');
+        fprintf('   %s  from  %s to %s\n',infc(i).name,infc(i).startdate,infc(i).enddate);
+    end
+end
