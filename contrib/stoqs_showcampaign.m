8b1b9d3653af0eb27b398eb24d8a7f7d1dfa064c
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-07-23 16:46:14 -0700

    Merged Francisco's commits

--- contrib/stoqs_showcampaign.m
+++ contrib/stoqs_showcampaign.m
@@ -28,12 +28,5 @@ infc=stoqs_qcampaign(urlc,1);
 [infpa,parname]=stoqs_qparameter(urlc,1) ; %Get the parameter information and a vector of platform with de I
 
 
-%We want to show for each campain, what platform are and the variable who
-%each measure.
-%for i=1:length(infc) %Loop the campaign
-%    infa=stoqs_qactivity(u,camp,1);   %Get the activities for a campaign
-%    for j=1:length(infa)
-%        infact=stoqs_qparameter_in_platformactivity(infa,num2str(infa(i).id),1);
-%    end
-%end
+
 
