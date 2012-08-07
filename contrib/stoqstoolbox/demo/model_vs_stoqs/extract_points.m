4f55c03b82129a1f18893214dd4f00aa65dc6251
Francisco Lopez <flcastej@gmail.com>
2012-08-07 17:53:46 -0400

    Now export the latitude and longitude of the node that have in situ measurements

--- contrib/stoqstoolbox/demo/model_vs_stoqs/extract_points.m
+++ contrib/stoqstoolbox/demo/model_vs_stoqs/extract_points.m
@@ -1,4 +1,4 @@
-function [extra]=extract_points(model,d)
+function [extra]=extract_points(model,insit)
 %
 %
 %  Usage
@@ -16,50 +16,38 @@ function [extra]=extract_points(model,d)
 %        Get one model output for every insitudata
 %           .pointdata = Value of the nearest node to the insitu data                        
 %           .indx,indy = Index of the node nearest to the insitu data
-%        Get one mean insitu data for every model node where there are in
-%        situ data
-%           .insitumean = Mean of all the insitu measurement inside the
-%                         same model node.
-%           .node  = Value of the model node nearest to the insitu data
-%           
+%           .modeltime = Model time.
+%           .time = Time of each of the in situ measurement
 %           
 %
-joi(length(model.lat),length(model.lon))=0;
-cjoi=joi;
-if isempty(d.vari)
+
+
+if isempty(insit.vari)
   extra.pointdata(1)=NaN;
   extra.indx(1)=NaN;
   extra.indy(1)=NaN;
+  extra.lat(1)=NaN;
+  extra.lon(1)=NaN;
   extra.modeltime(1)=model.date;
-  extra.time(1)=NaN;
+
 else
 
- for i=1:length(d.long)
-  [inde,dist]=near(d.long(i),model.lon);
+ for i=1:length(insit.long)
+  [inde,dist]=near(insit.long(i),model.lon);
   indy=inde;clear inde;
-  [inde,dist]=near(d.lati(i),model.lat);
+  [inde,dist]=near(insit.lati(i),model.lat);
   indx=inde;clear inde;
   extra.pointdata(i)=model.data_inlevel(indx,indy);
   extra.indx(i)=indx;
   extra.indy(i)=indy;
   extra.modeltime(i)=model.date;
-  joi(indx,indy)=joi(indx,indy)+d.vari(i);
-  cjoi(indx,indy)=cjoi(indx,indy)+1;  
-  extra.time(i)=d.time(i);
+  extra.lat(i)=model.lat(indx);
+  extra.lon(i)=model.lon(indy);
+ 
  end
 
 
 
-%ux=unique(extra.indx);
-%uy=unique(extra.indy);
-%k=1;
-%for i=1:length(ux)
-%    for j=1:length(uy)
-%        extra.insitumean(k)=joi(ux(i),uy(j))/cjoi(ux(i),uy(j));
-%        extra.node(k)=model.data_inlevel(ux(i),uy(j));
-%        k=k+1;
-%    end
-%end
 end
 
     
