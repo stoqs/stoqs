c022d2f8737aa18d54db451cf95c9655a3453687
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-09-18 00:37:32 -0400

    New script that inherits from BaseWriter.  Creates wf_uctd.nc and copies back to a TDS server on the ship.

---
+++ loaders/CANON/toNetCDF/makeWFuctdNetCDF.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# For use on Western Flyer CANON September 2012
+
+rsync -rv stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/wf/uctd .
+
+./wfuctdToNetcdf.py
+
+scp wf_uctd.nc stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/wf
