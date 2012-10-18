05f88825508499ce2736a82d1c76edacfb28c237
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-10-18 17:45:19 -0400

    Made to work on shore with the odss machine

--- loaders/CANON/toNetCDF/makeWGnetcdf.sh
+++ loaders/CANON/toNetCDF/makeWGnetcdf.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # For use on Western Flyer CANON September 2012
+# Set Remote Host (RH) to what's appropriate
+RH=192.168.111.177
+RH=beach.mbari.org
 
-scp stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/waveglider/tracking/waveglider_gpctd_WG.txt .
-scp stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/waveglider/tracking/waveglider_pco2_WG.txt .
+scp stoqsadm@$RH:/ODSS/data/canon/2012_Sep/waveglider/tracking/waveglider_gpctd_WG.txt .
+scp stoqsadm@$RH:/ODSS/data/canon/2012_Sep/waveglider/tracking/waveglider_pco2_WG.txt .
 ./wgToNetcdf.py
-scp waveglider_gpctd_WG.nc stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/waveglider/netcdf
-scp waveglider_pco2_WG.nc stoqsadm@192.168.111.177:/ODSS/data/canon/2012_Sep/waveglider/netcdf
+scp waveglider_gpctd_WG.nc stoqsadm@$RH:/ODSS/data/canon/2012_Sep/waveglider/netcdf
+scp waveglider_pco2_WG.nc stoqsadm@$RH:/ODSS/data/canon/2012_Sep/waveglider/netcdf
 
 # Clean up
 rm waveglider_gpctd_WG.*
