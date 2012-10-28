dc97dc455139cf289474be3888d66e4551747b35
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-10-28 13:52:30 -0700

    Add 2012 campaigns, with September 2012 commented out for now.  Set up for reloadeing database that have the summary count errors.

--- loaders/CANON/loadCANON_all.sh
+++ loaders/CANON/loadCANON_all.sh
@@ -3,8 +3,10 @@
 # and fix the id_seq reset problem.  Must be run from the 
 # loaders/CANON directory.  Argument is the stride.
 
-python loadCANON_september2010.py 1
-python loadCANON_october2010.py 1
-python loadCANON_april2011.py 1
-python loadCANON_june2011.py 1
+python loadCANON_september2010.py 1 stoqs_september2010
+python loadCANON_october2010.py 1 stoqs_october_2010
+python loadCANON_april2011.py 1 stoqs_april2011
+python loadCANON_june2011.py 1 stoqs_june2011
+python loadCANON_may2012.py 1 stoqs_may2012
+python loadCANON_september2012.py 1 stoqs_september2012
 
