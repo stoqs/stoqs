1e09a36895f5645b5a93bbf83d8b0043788c0867
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-11-01 01:02:40 -0400

    Cleaned up help docuemtation

--- contrib/stoqstoolbox/stoqs_down.m
+++ contrib/stoqstoolbox/stoqs_down.m
@@ -1,63 +1,52 @@
 function outp=stoqs_down(varargin)
-%       Usage
-%            Acces to a STOQS server and download a dataset based in a
-%            query.
+% STOQS_DOWN  Download data from a STOQS database directly into a Matlab strucure
 %
-%            Time Queries:
-%                The user could write a begging and end date. If, for example, he doesnt
-%                write  a end date, the program will query to all the data
-%                available from the begging date. In the same way, if the
-%                query only have a end date, the program will query to all
-%                the data available until that date. 
-%                        Ex. d=stoqs_down('http://odss-staging.shore.mbari.org/canon/stoqs_may2012','2012-05-30 00:10:00','2012-05-30 01:10:00','1','2','sea_water_temperature','dorado');
-%                            Will query to all the data available of
-%                            sea_water_temperature
-%                            measured by dorado between  2012-05-30 00:10:00 and 2012-05-30 01:10:00
+% Use as:
+%   data = stoqs_down(campaign,start_time,end_time,min_depth,max_depth,platforms,parametername,parameterstandardname);
 %
-%                            between 1 - 2 meters.
-%            Depth Queries:
-%                The user could write a top and low leve to query. If there
-%                is not any depth, the program will query for all the
-%                depths. If write the minimum depth, it will search for the
-%                minimum to the maximum depth available,and in the same way
-%                with the maximum.
-%                         Ex. d=stoqs_down('http://odss-staging.shore.mbari.org/canon/stoqs_may2012','2012-05-30 00:10:00','2012-05-30 01:10:00','','3','sea_water_temperature','dorado');
-%                             Will query to all the variables measured by
-%                             dorado  up 3 m depth between '2010-10-27 21:00:00'and '2010-10-29 23:00:00'
+% Arguments:
+%   campaign = Base url for a campaign from a stoqs server
 %
-%            Variable Query:
-%                 MUST use the standard name of he variable following the CF-Metadata conventions
-% 			      (http://cf-pcmdi.llnl.gov/documents/cf-standard-names/standard-name-table/19/cf-standard-name-table.html).
-% 			      In the case you want to get all the variable,
-% 			      leave it empty
+%   Arguments that constrain retreival of data (set to '' for no constraint):
+%     start_time            = Start date and time in format yyyy-mm-dd HH:MM:SS
+%     end_time              = End date and time in format yyyy-mm-dd HH:MM:SS
+%     min_depth             = Minimum depth in meters
+%     max_depth             = Maximum depth in meters
+%     platforms             = Name of platform
+%     parametername         = Parameter name
+%     parameterstandardname = Parameter standard_name 
 %
-%           Platform Query:
-%                  Name of the platform to query, If is empty will query for all the platform.
-%                
+% Return:
+%   A structure like:
+%     data = 
+%              platform: {366x1 cell}
+%                  time: [366x1 double]
+%             longitude: [366x1 double]
+%              latitude: [366x1 double]
+%                 depth: [366x1 double]
+%                 value: [366x1 double]
+%         parametername: {366x1 cell}
+%          standardname: {366x1 cell}
 %
+% Example:
+%   
+%   campaign = 'http://odss.mbari.org/canon/stoqs_september2012/';
+%   start_time = '2012-09-09 15:22:19';
+%   end_time = '2012-09-09 17:09:33';
+%   min_depth = 2.61;
+%   max_depth = 104.38;
+%   parametername = '';
+%   parameterstandardname = 'sea_water_temperature';
+%   platforms = '';
+%   
+%   data = stoqs_down(campaign, start_time, end_time, min_depth, max_depth, platforms, parametername, parameterstandardname);
 %
-%       Input
-%          The input in order are:
-% 			URL of the STOQS server
-% 			Start time in the format 'yyyy-mm-dd+HH:MM:SS'
-% 			End time in the format 'yyyy-mm-dd+HH:MM:SS'
-%			Minimum depth in meters
-% 			Maximum depth in meters
-%			Platform name. Set to '' to get data from all platforms.
-% 			Parameter name 
-% 			Parameter standard_name - the CF standard_name
-%       Ouput
-%           outp = Structure with the data structured by 
-%                  platform, time, longitude, latitude, depth and parameter values
+% Notes:
+%   The stoqs web query user interface (Measurement Data Access -> stoqstoolbox) provides the argument 
+%   arguments for stoqs_down() for the selections made using the graphical web query interface.
 %
-%
-%
-%   Francisco Lopez & Mike McCann & Brian Schlining 
-%
-%   Last modified
-%   30/October/2012
-
-
+%   Francisco Lopez, Mike McCann, & Brian Schlining 
+%   Last modified  31 October 2012
 
 
 %CHECK INPUT ERRORS
@@ -109,16 +98,14 @@ end
 
 
 
-
-
 %DO THE JSON QUERY TO STOQS
-
-disp('START THE JSON QUERY');
+query = strrep(query, ' ', '%20');       % Replace spaces - in time parms - to be URL friendly
+disp(['START THE JSON QUERY: ']);
 try
     url = java.net.URL(query);
-    disp('CONNECTION TO THE SERVER SUCCESSFULLY')
+    disp(['CONNECTED TO THE SERVER AND READING DATA FROM: ' query])
 catch me
-    m = MException([mfilename ':BadURL'], '%s is not a valid URL. Cause: %s', u, me.message);
+    m = MException([mfilename ':BadURL'], '%s is not a valid URL. Cause: %s', query, me.message);
     throw(m);
 end
 
@@ -134,8 +121,8 @@ if isempty(a)
     disp(error)
     outp='';
 else    
-    disp('CONVERTING FROM JSON');
-    info=loadjson(a); %Read de information, Convert java.string to string with char, so loadjson could convert it to a struct.
+    disp(['CONVERTING FROM JSON']);
+    info=loadjson(a); %Read the information, Convert java.string to string with char, so loadjson could convert it to a struct.
     er=0;
 end
 
