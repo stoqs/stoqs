8d5347679f5e95820e72a88a865ba572b8816fe3
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-11-15 18:31:58 -0500

    Get MEDIA_ROOT and MEDIA_URL to work for section plots on development server

--- settings.py
+++ settings.py
@@ -132,12 +132,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(project_dir, 'stoqs/static/media')
+MEDIA_ROOT = os.path.join(project_dir, 'stoqs/media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'stoqs/media/'
+MEDIA_URL = '/stoqs/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
