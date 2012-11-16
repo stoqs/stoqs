626dd3da37a466295aaa088352f77de7d76c8081
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-11-15 19:01:18 -0500

    Use full production path for MEDIA_ROOT

--- settings.py
+++ settings.py
@@ -132,7 +132,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(project_dir, 'stoqs/media')
+MEDIA_ROOT = '/var/www/html/stoqs/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
