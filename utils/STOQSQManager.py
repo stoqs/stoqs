bdaf5a33090964a62ec779f0cf0bb6381230eb15
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2012-12-12 15:46:46 -0500

    Factored out the Matplotlib code from STOQSQManager.py into Viz.py and created the ContourPlots class there for more flexibility in performing data visualization

--- utils/STOQSQManager.py
+++ utils/STOQSQManager.py
@@ -21,6 +21,8 @@ from django.db.models.query import RawQuerySet
 from django.http import HttpResponse
 from stoqs import models
 from utils import round_to_n, postgresifySQL
+from MPQuery import MPQuery
+from Viz import ContourPlots
 from coards import to_udunits
 from datetime import datetime
 import logging
@@ -31,7 +33,6 @@ import locale
 import time
 import os
 import tempfile
-from MPQuery import MPQuery
 
 logger = logging.getLogger(__name__)
 
@@ -644,192 +645,11 @@ class STOQSQManager(object):
             return None, None, 'Could not get platform name'
             
         logger.debug('platformName = %s', platformName)
+        logger.debug('Instantiating Viz.ContourPlots............................................')
+        cp = ContourPlots(self.kwargs, self.request, self.qs, self.mpq.qs_mp,
+                              self.getParameterMinMax(), self.getSampleQS(), platformName)
 
-        # Setup Matplotlib for running on the server
-        os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
-        import matplotlib
-        matplotlib.use('Agg')               # Force matplotlib to not use any Xwindows backend
-        import matplotlib.pyplot as plt
-
-        from matplotlib.mlab import griddata
-        from matplotlib import mpl
-        import numpy as np
-        import string
-        import random
-        from stoqs.views.KML import readCLT
-
-        # griddata parameters
-        tgrid_max = 1000            # Reasonable maximum width for time-depth-flot plot is about 1000 pixels
-        dgrid_max = 100             # Height of time-depth-flot plot area is 335 pixels
-        dinc = 0.5                  # Average vertical resolution of AUV Dorado
-        nlevels = 255                # Number of color filled contour levels
-
-        # Use session ID so that different users don't stomp on each other with their section plots
-        # - This does not work for Firefox which just reads the previous image from its cache
-        if self.request.session.has_key('sessionID'):
-            sessionID = self.request.session['sessionID']
-        else:
-            sessionID = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(7))
-            self.request.session['sessionID'] = sessionID
-        # - Use a new imageID for each new image
-        imageID = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
-        sectionPngFile = self.kwargs['parametername'][0] + '_' + platformName + '_' + imageID + '.png'
-        sectionPngFileFullPath = os.path.join(settings.MEDIA_ROOT, 'sections', sectionPngFile)
-        colorbarPngFile = self.kwargs['parametername'][0] + '_' + platformName + '_colorbar_' + imageID + '.png'
-        colorbarPngFileFullPath = os.path.join(settings.MEDIA_ROOT, 'sections', colorbarPngFile)
-        
-        # Estimate horizontal (time) grid spacing by number of points in selection, expecting that simplified depth-time
-        # query has salient points, typically in the vertices of the yo-yos.  
-        tmin = None
-        tmax = None
-        xi = None
-        if self.kwargs.has_key('time'):
-            if self.kwargs['time'][0] is not None and self.kwargs['time'][1] is not None:
-                dstart = datetime.strptime(self.kwargs['time'][0], '%Y-%m-%d %H:%M:%S') 
-                dend = datetime.strptime(self.kwargs['time'][1], '%Y-%m-%d %H:%M:%S') 
-                tmin = time.mktime(dstart.timetuple())
-                tmax = time.mktime(dend.timetuple())
-        ##if not tmin and not tmax:
-        ##    logger.debug('Time range not specified in query, getting it from the database')
-        ##    tmin, tmax = self.getTime()
-
-        if tmin and tmax:
-            sdt_count = self.qs.filter(platform__name = platformName).values_list('simpledepthtime__depth').count()
-            sdt_count = int(sdt_count / 2)                 # 2 points define a line, take half the number of simpledepthtime points
-            logger.debug('Half of sdt_count from query = %d', sdt_count)
-            if sdt_count > tgrid_max:
-                sdt_count = tgrid_max
-
-            xi = np.linspace(tmin, tmax, sdt_count)
-            logger.debug('xi = %s', xi)
-
-        # Make depth spacing dinc m, limit to time-depth-flot resolution (dgrid_max)
-        dmin = None
-        dmax = None
-        yi = None
-        if self.kwargs.has_key('depth'):
-            if self.kwargs['depth'][0] is not None and self.kwargs['depth'][1] is not None:
-                dmin = float(self.kwargs['depth'][0])
-                dmax = float(self.kwargs['depth'][1])
-        ##if not dmin and not dmax:
-        ##    logger.debug('Depth range not specified in query, getting it from the database')
-        ##    depths = self.getDepth()
-        ##    dmin = float(depths[0])
-        ##    dmax = float(depths[1])
-
-        if dmin and dmax:
-            y_count = int((dmax - dmin) / dinc )
-            if y_count > dgrid_max:
-                y_count = dgrid_max
-            yi = np.linspace(dmin, dmax, y_count)
-            logger.debug('yi = %s', yi)
-
-
-        # Collect the scattered datavalues(time, depth) and grid them
-        if xi is not None and yi is not None:
-            # Estimate a scale factor to apply to the x values on drid data so that x & y values are visually equal for the flot plot
-            # which is assumed to be 3x wider than tall.  Approximate horizontal coverage by Dorado is 1 m/s.
-            try:
-                scale_factor = float(tmax -tmin) / (dmax - dmin) / 3.0
-            except ZeroDivisionError, e:
-                logger.warn(e)
-                return None, None, 'Bad depth range'
-                
-            logger.debug('scale_factor = %f', scale_factor)
-            xi = xi / scale_factor
-
-            try:
-                os.remove(sectionPngFileFullPath)
-            except Exception, e:
-                logger.warn(e)
-
-            logger.debug('Gridding data with sdt_count = %d, and y_count = %d', sdt_count, y_count)
-            x = []
-            y = []
-            z = []
-            qs_mp = self.mpq.qs_mp
-            if type(qs_mp) == RawQuerySet:
-                # Most likely because it is a RawQuerySet from a ParameterValues query
-                for mp in qs_mp:
-                    ##logger.debug('mp = %s, %s, %s', mp.timevalue, mp.depth, mp.datavalue)
-                    x.append(time.mktime(mp.timevalue.timetuple()) / scale_factor)
-                    y.append(mp.depth)
-                    z.append(mp.datavalue)
-            else:
-                for mp in qs_mp.values('measurement__instantpoint__timevalue', 'measurement__depth', 'datavalue'):
-                    x.append(time.mktime(mp['measurement__instantpoint__timevalue'].timetuple()) / scale_factor)
-                    y.append(mp['measurement__depth'])
-                    z.append(mp['datavalue'])
-          
-            logger.debug('Number of x, y, z data values retrived from database = %d', len(z)) 
-            try:
-                zi = griddata(x, y, z, xi, yi, interp='nn')
-            except KeyError, e:
-                logger.exception('Got KeyError. Could not grid the data')
-                return None, None, 'Got KeyError. Could not grid the data'
-            except Exception, e:
-                logger.exception('Could not grid the data')
-                return None, None, 'Could not grid the data'
-
-            logger.debug('zi = %s', zi)
-
-            parm_info = self.getParameterMinMax()
-            try:
-                # Make the plot
-                # contour the gridded data, plotting dots at the nonuniform data points.
-                # See http://scipy.org/Cookbook/Matplotlib/Django
-                fig = plt.figure(figsize=(6,3))
-                ax = fig.add_axes((0,0,1,1))
-                ax.set_xlim(tmin / scale_factor, tmax / scale_factor)
-                ax.set_ylim(dmax, dmin)
-                ax.get_xaxis().set_ticks([])
-                clt = readCLT(os.path.join(settings.STATIC_ROOT, 'colormaps', 'jetplus.txt'))
-                cm_jetplus = matplotlib.colors.ListedColormap(np.array(clt))
-                ax.contourf(xi, yi, zi, levels=np.linspace(parm_info[1], parm_info[2], nlevels), cmap=cm_jetplus, extend='both')
-                ax.scatter(x, y, marker='.', s=2, c='k', lw = 0)
-
-                # Add sample locations and names
-                xsamp = []
-                ysamp = []
-                sname = []
-                for s in self.getSampleQS().values('instantpoint__timevalue', 'depth', 'name'):
-                    xsamp.append(time.mktime(s['instantpoint__timevalue'].timetuple()) / scale_factor)
-                    ysamp.append(s['depth'])
-                    sname.append(s['name'])
-                ax.scatter(xsamp, ysamp, marker='o', c='w', s=15, zorder=10)
-                for x,y,sn in zip(xsamp, ysamp, sname):
-                    plt.annotate(sn, xy=(x,y), xytext=(5,-5), textcoords = 'offset points', fontsize=7)
-
-                fig.savefig(sectionPngFileFullPath, dpi=120, transparent=True)
-                plt.close()
-            except Exception,e:
-                logger.exception('Could not plot the data')
-                return None, None, 'Could not plot the data'
-
-            try:
-                # Make colorbar as a separate figure
-                cb_fig = plt.figure(figsize=(5, 0.8))
-                cb_ax = cb_fig.add_axes([0.1, 0.8, 0.8, 0.2])
-                logger.debug('parm_info = %s', parm_info)
-                parm_units = models.Parameter.objects.filter(name=parm_info[0]).values_list('units')[0][0]
-                logger.debug('parm_units = %s', parm_units)
-                norm = mpl.colors.Normalize(vmin=parm_info[1], vmax=parm_info[2], clip=False)
-                cb = mpl.colorbar.ColorbarBase( cb_ax, cmap=cm_jetplus,
-                                                norm=norm,
-                                                ticks=[parm_info[1], parm_info[2]],
-                                                orientation='horizontal')
-                cb.ax.set_xticklabels([str(parm_info[1]), str(parm_info[2])])
-                cb.set_label('%s (%s)' % (parm_info[0], parm_units))
-                logger.debug('ticklabels = %s', [str(parm_info[1]), str(parm_info[2])])
-                cb_fig.savefig(colorbarPngFileFullPath, dpi=120, transparent=True)
-                plt.close()
-            except Exception,e:
-                logger.exception('Could not plot the colormap')
-                return None, None, 'Could not plot the colormap'
-
-            return sectionPngFile, colorbarPngFile, ''
-        else:
-            return None, None, 'No depth-time region specified'
+        return cp.contourDatavaluesForFlot()
 
 
     #
