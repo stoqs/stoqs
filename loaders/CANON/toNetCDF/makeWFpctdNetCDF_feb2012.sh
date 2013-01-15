f9db09dabe75a80d041c103fbef84fb4832030ef
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2013-01-15 01:03:44 -0500

    Fix Sample loader to use parameters from master loader.  pctd data load with this checkin.

--- loaders/CANON/toNetCDF/makeWFpctdNetCDF_feb2012.sh
+++ loaders/CANON/toNetCDF/makeWFpctdNetCDF_feb2012.sh
@@ -4,6 +4,11 @@
 
 rsync -rv mccann@elvis.shore.mbari.org:/net/atlas/ifs/mbariarchive/BOG_Archive/DataArchives/Regions/NortheastPacific/CruiseData/GOC.2012/pCTD.GOC12 .
 
+# Copy these files once to beach
+# scp pCTD.GOC12/*.hdr stoqsadm@beach.mbari.org:/ODSS/data/goc/GOC_february2012/wf/pctd
+# scp pCTD.GOC12/*.btl stoqsadm@beach.mbari.org:/ODSS/data/goc/GOC_february2012/wf/pctd
+# rm *bad* *BAD* files that got copied to beach
+
 ./wfpctdToNetcdf.py pCTD.GOC12 pCTD.GOC12 GOC
 
 scp pCTD.GOC12/*.nc stoqsadm@beach.mbari.org:/ODSS/data/goc/GOC_february2012/wf/pctd
@@ -11,3 +16,4 @@ scp pCTD.GOC12/*.nc stoqsadm@beach.mbari.org:/ODSS/data/goc/GOC_february2012/wf/
 # Clean up
 rm -r pCTD.GOC12
 
+# Then run loader for GOC_february2012
