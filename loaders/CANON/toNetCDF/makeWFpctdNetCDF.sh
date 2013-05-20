d061606d21f29ef84d083527430801dda3ba9c0a
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2013-05-20 10:13:28 -0700

    Add attributes for CF-1.6 compliance on coordinates, rework WF loaders after generalizing for Rachel Carson

--- loaders/CANON/toNetCDF/makeWFpctdNetCDF.sh
+++ loaders/CANON/toNetCDF/makeWFpctdNetCDF.sh
@@ -7,7 +7,7 @@ RH=beach.mbari.org
 
 rsync -rv stoqsadm@$RH:/ODSS/data/canon/2012_Sep/wf/pctd .
 
-./wfpctdToNetcdf.py
+./pctdToNetcdf.py
 
 scp pctd/*.nc stoqsadm@$RH:/ODSS/data/canon/2012_Sep/wf/pctd
 
