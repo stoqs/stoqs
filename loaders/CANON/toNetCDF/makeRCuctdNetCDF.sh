5e489e92b0627d12aa5e36ae126713387505b0ef
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2013-05-21 08:02:43 -0700

    Use the odss server instead of zuma

--- loaders/CANON/toNetCDF/makeRCuctdNetCDF.sh
+++ loaders/CANON/toNetCDF/makeRCuctdNetCDF.sh
@@ -3,13 +3,14 @@
 # For use on Rachel Carson CANON - ECOHAB March 2013
 # Set Remote Host (RH) to what's appropriate
 ##RH=192.168.111.177
-RH=zuma.rc.mbari.org
+##RH=zuma.rc.mbari.org
+RH=odss.mbari.org
 
-rsync -rv odssadm@$RH:/data/canon/2013_Mar/carson/uctd .
+rsync -rv stoqsadm@$RH:/data/canon/2013_Mar/carson/uctd .
 
 ./uctdToNetcdf.py uctd uctd 0 1.5
 
-scp uctd/*.nc odssadm@$RH:/data/canon/2013_Mar/carson/uctd
+scp uctd/*.nc stoqsadm@$RH:/data/canon/2013_Mar/carson/uctd
 
 # Clean up 
 rm -r uctd
