8b4c8271fe7021db9404573e229f32670f8e2036
Duane Edgington duane <Duane Edgington duane@mbari.org>
2013-08-15 13:51:15 -0700

    new loader script

---
+++ loaders/CANON/loadCANON_september2011.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+__author__    = 'Mike McCann'
+__copyright__ = '2011'
+__license__   = 'GPL v3'
+__contact__   = 'mccann at mbari.org'
+
+__doc__ = '''
+
+Master loader for all CANON activities in September 2010
+
+Mike McCann
+MBARI 22 April 2012
+
+@var __date__: Date of last svn commit
+@undocumented: __doc__ parser
+@status: production
+@license: GPL
+'''
+
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+project_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # settings.py is one dir up
+
+from CANON import CANONLoader
+
+# Assign input data sources
+cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
+cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2011/netcdf/'
+cl.dorado_files = [ 'Dorado389_2011_249_00_249_00_decim.nc',
+                    
+                  ]
+
+# Execute the load
+cl.process_command_line()
+
+if cl.args.test:
+    cl.loadDorado(stride=100)
+
+elif cl.args.optimal_stride:
+    cl.loadDorado(stride=2)
+
+else:
+    cl.loadDorado(stride=cl.args.stride)
+
