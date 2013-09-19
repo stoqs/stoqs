25a00ae3228e9defe694c12ca73477419f5a5f34
schramm r <schramm.r@gmail.com>
2013-09-19 13:14:35 -0700

    ??

--- loaders/CANON/m1_loadsep2013.py
+++ loaders/CANON/m1_loadsep2013.py
@@ -30,8 +30,14 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # this makes
 from CANON import CANONLoader
        
 # building input data sources object
-#cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
-cl = CANONLoader('stoqs_september2013', 'CANON - September 2013')
+from socket import gethostname
+hostname=gethostname()
+print hostname
+if hostname=='odss-test.shore.mbari.org':
+    cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
+else:
+    cl = CANONLoader('stoqs_september2013', 'CANON - September 2013')
+
 
 # default location of thredds and dods data:
 cl.tdsBase = 'http://odss.mbari.org/thredds/'
@@ -40,7 +46,7 @@ cl.dodsBase = cl.tdsBase + 'dodsC/'
 # Set start and end dates for mooring, twice per day.  In the morning and afternoon.
 ##t =time.strptime("2013-09-09 0:01", "%Y-%m-%d %H:%M")
 ##startdate=t[:6]
-ts=time.time()-(24*60*60)
+ts=time.time()-(33*60*60)
 st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
 t=time.strptime(st,"%Y-%m-%d %H:%M")
 startdate=t[:6]
@@ -52,8 +58,8 @@ print startdate, enddate
 #  MOORINGS
 ######################################################################
 # Mooring M1 Combined file produced by DPforSSDS processing - for just the duration of the campaign
-cl.m1_base = 'http://dods.mbari.org/opendap/data/ssdsdata/deployments/m1/201202/'
-cl.m1_files = ['OS_M1_20120222hourly_CMSTV.nc']
+cl.m1_base = 'http://dods.mbari.org/opendap/hyrax/data/ssdsdata/deployments/m1/201209/'
+cl.m1_files = ['OS_M1_20130918hourly_CMSTV.nc']
 cl.m1_parms = [ 'eastward_sea_water_velocity_HR', 'northward_sea_water_velocity_HR',
                      'SEA_WATER_SALINITY_HR', 'SEA_WATER_TEMPERATURE_HR', 'SW_FLUX_HR', 'AIR_TEMPERATURE_HR',
                      'EASTWARD_WIND_HR', 'NORTHWARD_WIND_HR', 'WIND_SPEED_HR'
