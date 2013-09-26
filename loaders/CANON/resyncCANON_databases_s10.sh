4726e2a2eb24ebfa9d9a2403468bedebdb1f46a0
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2013-09-26 06:30:09 -0700

    Added Dorado mission and new deployment file for M1

---
+++ loaders/CANON/resyncCANON_databases_s10.sh
@@ -0,0 +1,7 @@
+#python manage.py syncdb --noinput --database=stoqs 
+python manage.py syncdb --noinput --database=stoqs_september2010_s10
+python manage.py syncdb --noinput --database=stoqs_october2010_s10
+python manage.py syncdb --noinput --database=stoqs_april2011_s10
+python manage.py syncdb --noinput --database=stoqs_june2011_s10
+python manage.py syncdb --noinput --database=stoqs_may2012_s10
+python manage.py syncdb --noinput --database=stoqs_september2012_s10
