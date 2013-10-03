6b89d2f62d32cc05c411034cac1b0746604949c6
schramm r <schramm.r@gmail.com>
2013-10-03 12:35:18 -0700

    esp_loadsep2013.py add beamc

--- loaders/CANON/esp_loadsep2013.py
+++ loaders/CANON/esp_loadsep2013.py
@@ -30,19 +30,24 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # this makes
 from CANON import CANONLoader
        
 # building input data sources object
-#cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
-cl = CANONLoader('stoqs_september2013', 'CANON - September 2013')
+from socket import gethostname
+hostname=gethostname()
+print hostname
+if hostname=='odss-test.shore.mbari.org':
+    cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
+else:
+    cl = CANONLoader('stoqs_september2013', 'CANON - September 2013')
 
 # default location of thredds and dods data:
 cl.tdsBase = 'http://odss.mbari.org/thredds/'
 cl.dodsBase = cl.tdsBase + 'dodsC/'
 
 # Set start and end dates for mooring, twice per day.  In the morning and afternoon.
-#t =time.strptime("2013-09-10 0:01", "%Y-%m-%d %H:%M")
+t =time.strptime("2013-09-10 0:01", "%Y-%m-%d %H:%M")
 ##startdate=t[:6]
-ts=time.time()-(13*60*60)
-st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
-t=time.strptime(st,"%Y-%m-%d %H:%M")
+##ts=time.time()-(13*60*60)
+##st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M')
+##t=time.strptime(st,"%Y-%m-%d %H:%M")
 startdate=t[:6]
 t =time.strptime("2013-10-29 0:01", "%Y-%m-%d %H:%M")
 enddate=t[:6]
@@ -53,14 +58,14 @@ print startdate, enddate
 ######################################################################
 cl.bruce_moor_base = cl.dodsBase + 'CANON_september2013/Platforms/Moorings/ESP_Bruce/NetCDF/'
 cl.bruce_moor_files = ['Bruce_ctd.nc']
-cl.bruce_moor_parms = [ 'TEMP','PSAL','chl','xmiss','oxygen',
+cl.bruce_moor_parms = [ 'TEMP','PSAL','chl','xmiss','oxygen','beamc',
                    ]
 cl.bruce_moor_startDatetime = datetime.datetime(*startdate[:])
 cl.bruce_moor_endDatetime = datetime.datetime(*enddate[:])
 
 cl.mack_moor_base = cl.dodsBase + 'CANON_september2013/Platforms/Moorings/ESP_Mack/NetCDF/'
 cl.mack_moor_files = ['Mack_ctd.nc']
-cl.mack_moor_parms = [ 'TEMP','PSAL','chl','xmiss','oxygen',
+cl.mack_moor_parms = [ 'TEMP','PSAL','chl','xmiss','oxygen','beamc',
                    ]
 cl.mack_moor_startDatetime = datetime.datetime(*startdate[:])
 cl.mack_moor_endDatetime = datetime.datetime(*enddate[:])
