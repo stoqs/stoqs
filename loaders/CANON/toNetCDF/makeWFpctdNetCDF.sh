b4db3184be89e07337d2424efeac87cd4553e5a4
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2013-10-28 08:53:43 -0700

    Process 2013_Oct data

--- loaders/CANON/toNetCDF/makeWFpctdNetCDF.sh
+++ loaders/CANON/toNetCDF/makeWFpctdNetCDF.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# For use on Western Flyer CANON September 2012
+# For use on Western Flyer CN13ID October 2012
 # Set Remote Host (RH) to what's appropriate
 ##RH=192.168.111.177
 RH=beach.mbari.org
 
-rsync -rv stoqsadm@$RH:/ODSS/data/canon/2012_Sep/wf/pctd .
+rsync -rv stoqsadm@$RH:/data/canon/2013_Oct/Platforms/Ships/Western_Flyer/pctd .
 
-./pctdToNetcdf.py
+./pctdToNetcdf.py pctd pctd C
 
-scp pctd/*.nc stoqsadm@$RH:/ODSS/data/canon/2012_Sep/wf/pctd
+scp pctd/*.nc stoqsadm@$RH:/data/canon/2013_Oct/Platforms/Ships/Western_Flyer/pctd
 
 # Clean up
 rm -r pctd
