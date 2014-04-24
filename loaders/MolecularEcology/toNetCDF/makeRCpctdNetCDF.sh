a81577d0fd56dd19a1b4a3a134eb4394a651e552
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-04-24 13:19:46 -0700

    Added conversion of Profile CTD data from R/V Rachel Carson during CANON - SIMZ Spring 2014 along with a fix for the loaction of simz2013c23

--- loaders/MolecularEcology/toNetCDF/makeRCpctdNetCDF.sh
+++ loaders/MolecularEcology/toNetCDF/makeRCpctdNetCDF.sh
@@ -9,11 +9,11 @@
 # Mike McCann
 # 15 January 2014
 
-##LOGIN=stoqsadm
-##RH=odss.mbari.org
+LOGIN=stoqsadm
+RH=odss.mbari.org
 
-LOGIN=odssadm
-RH=zuma.rc.mbari.org
+##LOGIN=odssadm
+##RH=zuma.rc.mbari.org
 
 
 ##DIR=/data/canon/2013_Sep/Platforms/Ships/Rachel_Carson/pctd
@@ -23,9 +23,38 @@ RH=zuma.rc.mbari.org
 ##scp $LOCALDIR/*.nc $LOGIN@$RH:$DIR
 ##rm -r $LOCALDIR
 
-DIR=/data/simz/2014_spring/Platforms/Ships/Rachel_Carson/pctd
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# From: Erich Rienecker <erich@mbari.org>
+# Subject: Re: October 2013 SIMZ CTD data
+# Date: October 31, 2013 9:45:13 AM PDT
+# To: Mike McCann <mccann@mbari.org>
+# 
+# Found at:
+# 
+# /Tornado/DMO/MDUC_CORE_CTD_200103/Data/2013_224to301_RC_SIMZ
+
+# Original files copied to odss.mbari.org from /mbari/DMO/MDUC_CORE_CTD_200103/DATA/2013_224to301_RC_SIMZ/:
+# scp /mbari/DMO/MDUC_CORE_CTD_200103/DATA/2013_224to301_RC_SIMZ/PCTD/*.asc stoqsadm@odss.mbari.org:/data/canon/2013_Oct/Platforms/Ships/Rachel_Carson/pctd/
+# scp /mbari/DMO/MDUC_CORE_CTD_200103/DATA/2013_224to301_RC_SIMZ/PCTD/*.hdr stoqsadm@odss.mbari.org:/data/canon/2013_Oct/Platforms/Ships/Rachel_Carson/pctd/
+# scp /mbari/DMO/MDUC_CORE_CTD_200103/DATA/2013_224to301_RC_SIMZ/PCTD/*.btl stoqsadm@odss.mbari.org:/data/canon/2013_Oct/Platforms/Ships/Rachel_Carson/pctd/
+
+DIR=/data/canon/2013_Oct/Platforms/Ships/Rachel_Carson/pctd
 LOCALDIR=`echo $DIR | cut -d/ -f8`  # -f must match last directory
 rsync -rv $LOGIN@$RH:$DIR  .
-./pctdToNetcdf.py -i $LOCALDIR -t "Profile CTD data from R/V Rachel Carson during CANON - SIMZ Spring 2014" 
+
+# Correct c23 latitude and longitude based on Julio's email of 23 April 2014 - make it the same as c19
+sed -i "s/* NMEA Latitude = 36 54.63 N/* NMEA Latitude = 36 57.02 N/" pctd/simz2013c23.hdr
+sed -i "s/* NMEA Longitude = 121 52.77 W/* NMEA Longitude = 121 55.62 W/" pctd/simz2013c23.hdr
+
+../../CANON/toNetCDF/pctdToNetcdf.py -i $LOCALDIR -t "Profile CTD data from R/V Rachel Carson during SIMZ October 2013" 
+
 scp $LOCALDIR/*.nc $LOGIN@$RH:$DIR
 rm -r $LOCALDIR
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+##DIR=/data/simz/2014_spring/Platforms/Ships/Rachel_Carson/pctd
+##LOCALDIR=`echo $DIR | cut -d/ -f8`  # -f must match last directory
+##rsync -rv $LOGIN@$RH:$DIR  .
+##../../CANON/toNetCDF/pctdToNetcdf.py -i $LOCALDIR -t "Profile CTD data from R/V Rachel Carson during CANON - SIMZ Spring 2014" 
+##scp $LOCALDIR/*.nc $LOGIN@$RH:$DIR
+##rm -r $LOCALDIR
