a76f0475ecb92f4a1c55f473373edcf91af29cc9
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-06-23 09:09:27 -0700

    Try sleeping an additional 5 minutes for grdtrack to finish for more than 10 million records

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1068,6 +1068,9 @@ class STOQS_Loader(object):
         if self.totalRecords > 1e6:
             self.logger.info('Sleeping 60 seconds to give time for system call to finsh writing to %s', bdepthFileName)
             time.sleep(60)
+        if self.totalRecords > 1e7:
+            self.logger.info('Sleeping another 300 seconds to give time for system call to finsh writing to %s for more than 10 million records', bdepthFileName)
+            time.sleep(300)
 
         # Create our new Parameter
         self.logger.info('Getting or creating new altitude Parameter')
