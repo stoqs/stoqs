95f7032ad358fcdb0352e7462038b804be7b8e15
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-07-01 22:07:16 -0700

    Preparing for database reloads following schema change

--- loaders/CANON/loadCANON_april2011.py
+++ loaders/CANON/loadCANON_april2011.py
@@ -18,12 +18,27 @@ MBARI 22 April 2012
 
 import os
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))      # So that CANON is found
+
+parentDir = os.path.join(os.path.dirname(__file__), "../")
+sys.path.insert(0, parentDir)  # So that CANON is found
 
 from CANON import CANONLoader
 
 # Assign input data sources
-cl = CANONLoader('stoqs_april2011', 'CANON - April 2011')
+cl = CANONLoader('stoqs_april2011', 'CANON - April 2011',
+                    description = 'Dorado and Tethys surveys in Monterey Bay',
+                    x3dTerrains = {
+                                    'http://dods.mbari.org/terrain/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
+                                        'position': '-2822317.31255 -4438600.53640 3786150.85474',
+                                        'orientation': '0.89575 -0.31076 -0.31791 1.63772',
+                                        'centerOfRotation': '-2711557.9403829873 -4331414.329506527 3801353.4691465236',
+                                        'VerticalExaggeration': '10',
+                                        'speed': '1',
+                                    }
+                    },
+                    grdTerrain = os.path.join(parentDir, 'Monterey25.grd')
+                  )
+
 cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2011/netcdf/'
 cl.dorado_files = [ 'Dorado389_2011_110_12_110_12_decim.nc',
                     'Dorado389_2011_111_00_111_00_decim.nc',
@@ -66,5 +81,10 @@ elif cl.args.optimal_stride:
 else:
     cl.loadDorado(stride=cl.args.stride)
     cl.loadTethys(stride=cl.args.stride)
-    
+   
+# Add any X3D Terrain information specified in the constructor to the database - must be done after a load is executed
+cl.addTerrainResources()
+
+print "All Done."
+ 
 
