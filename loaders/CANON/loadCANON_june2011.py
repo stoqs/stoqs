9370618710c112c701bbde2b81ebe47f41a5b21a
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-07-07 11:41:51 -0700

    Put .out files in the project directories, moderniize june2011 loader

--- loaders/CANON/loadCANON_june2011.py
+++ loaders/CANON/loadCANON_june2011.py
@@ -18,12 +18,27 @@ MBARI 22 April 2012
 
 import os
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))      # So that CANON is found
+
+parentDir = os.path.join(os.path.dirname(__file__), "../")
+sys.path.insert(0, parentDir)  # So that CANON is found
 
 from CANON import CANONLoader
 
 # Assign input data sources
-cl = CANONLoader('stoqs_june2011', 'CANON - June 2011')
+cl = CANONLoader('stoqs_june2011', 'CANON - June 2011',
+                    description = 'Front detection Dorado and Tethys surveys in Monterey Bay',
+                    x3dTerrains = {
+                                    'http://dods.mbari.org/terrain/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
+                                        'position': '-2822317.31255 -4438600.53640 3786150.85474',
+                                        'orientation': '0.89575 -0.31076 -0.31791 1.63772',
+                                        'centerOfRotation': '-2711557.9403829873 -4331414.329506527 3801353.4691465236',
+                                        'VerticalExaggeration': '10',
+                                        'speed': '1',
+                                    }
+                    },
+                    grdTerrain = os.path.join(parentDir, 'Monterey25.grd')
+                  )
+
 cl.dorado_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2011/netcdf/'
 cl.dorado_files = [ 'Dorado389_2011_164_05_164_05_decim.nc',
                     'Dorado389_2011_165_00_165_00_decim.nc',
@@ -64,4 +79,8 @@ elif cl.args.optimal_stride:
 else:
     cl.loadDorado(stride=cl.args.stride)
     cl.loadTethys(stride=cl.args.stride)
-    
+
+# Add any X3D Terrain information specified in the constructor to the database - must be done after a load is executed
+cl.addTerrainResources()
+
+print "All Done."    
