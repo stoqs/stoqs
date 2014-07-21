7cc4a6471a242eb1011c2b7fdcc7403cad33f759
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-07-21 16:53:25 -0700

    Added haversine function for computing great circle distances

--- utils/utils.py
+++ utils/utils.py
@@ -408,6 +408,24 @@ def spiciness(t,s):
         bigT*=t
     return sp
 
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
+
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    return km 
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
