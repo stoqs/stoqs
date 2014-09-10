6961578caa5e952b0b56211bd347adaf8a68e2b6
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-09-09 22:19:47 -0700

    Simplify and make work for m1 September 2014

--- loaders/CANON/m1_loadsep2014.sh
+++ loaders/CANON/m1_loadsep2014.sh
@@ -1,6 +1,3 @@
 #!/bin/bash 
-cd /opt/stoqshg/venv-stoqs/bin
-source activate
-cd /opt/stoqshg/loaders/CANON
-python oa_loadsep2013.py -t > /tmp/oa_loadsep2013.log
-date >> /tmp/oa_load.log
+source /opt/stoqshg/venv-stoqs/bin/activate
+/opt/stoqshg/loaders/CANON/m1_loadsep2014.py --append > /opt/stoqshg/loaders/CANON/m1_loadsep2014.out 2>&1
