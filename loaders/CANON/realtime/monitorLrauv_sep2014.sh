115822da5825f047acff4810351bdb52a1202fb5
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-09-24 14:03:18 -0700

    Add --parms option so that parms can be passed in from the command for different vehicles with different sensor suites

--- loaders/CANON/realtime/monitorLrauv_sep2014.sh
+++ loaders/CANON/realtime/monitorLrauv_sep2014.sh
@@ -3,4 +3,8 @@ STOQSDIR=/opt/stoqshg
 cd $STOQSDIR/venv-stoqs/bin
 source activate
 cd $STOQSDIR/loaders/CANON/realtime
-python monitorLrauv.py -o /mbari/LRAUV/tethys/realtime/sbdlogs/2014/201409 -u 'http://elvis.shore.mbari.org/thredds/catalog/LRAUV/tethys/realtime/sbdlogs/2014/201409' -b 'stoqs_september2014' -c 'CANON-ECOHAB - September 2014' > $STOQSDIR/loaders/CANON/realtime/monitorLrauv_sep2014.out 2>&1
+
+python monitorLrauv.py -o /mbari/LRAUV/tethys/realtime/sbdlogs/2014/201409 -u 'http://elvis.shore.mbari.org/thredds/catalog/LRAUV/tethys/realtime/sbdlogs/2014/201409' -b 'stoqs_september2014' -c 'CANON-ECOHAB - September 2014' --parms sea_water_temperature sea_water_salinity mass_concentration_of_chlorophyll_in_sea_water voltage > $STOQSDIR/loaders/CANON/realtime/monitorLrauv_sep2014_tethys.out 2>&1
+
+python monitorLrauv.py -o /mbari/LRAUV/daphne/realtime/sbdlogs/2014/201409 -u 'http://elvis.shore.mbari.org/thredds/catalog/LRAUV/daphne/realtime/sbdlogs/2014/201409' -b 'stoqs_september2014' -c 'CANON-ECOHAB - September 2014' --parms sea_water_temperature sea_water_salinity mass_concentration_of_chlorophyll_in_sea_water > $STOQSDIR/loaders/CANON/realtime/monitorLrauv_sep2014_daphne.out 2>&1
+
