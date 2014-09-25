60068dcfbc073f0f8d88898d9e9127110aa859d7
msdanellecline <msdanellecline@gmail.com>
2014-09-25 15:57:51 -0700

    added support for loading lrauv realtime .nc files; currently will not support variables with a dot in the name

--- loaders/CANON/realtime/monitorLrauv.py
+++ loaders/CANON/realtime/monitorLrauv.py
@@ -21,6 +21,7 @@ MBARI 12 March 2014
 
 import os
 import sys
+import pdb
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../toNetCDF"))      # lrauvNc4ToNetcdf.py is in sister toNetCDF dir
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../../"))           # settings.py is two dirs up
@@ -34,7 +35,8 @@ from datetime import datetime, timedelta
 import time
 import re
 import pydap
-from stoqs import models as mod
+
+from stoqs import models as mod 
 from thredds_crawler.crawl import Crawl
 from coards import from_udunits
 
@@ -55,13 +57,12 @@ class NcFileMissing(Exception):
     def __str__(self):
         return repr(self.nc4FileUrl)
   
-def getNcStartEnd(urlNcDap):
+def getNcStartEnd(urlNcDap, timeAxisName):
     '''Find the lines in the html with the .nc file, then open it and read the start/end times
     return url to the .nc  and start/end as datetime objects.
     '''
     logger.debug('open_url on urlNcDap = %s', urlNcDap)
     df = pydap.client.open_url(urlNcDap)
-    timeAxisName = 'depth_time'
     timeAxisUnits = df[timeAxisName].units
     if timeAxisUnits == 'seconds since 1970-01-01T00:00:00Z' or timeAxisUnits == 'seconds since 1970/01/01 00:00:00Z':
         timeAxisUnits = 'seconds since 1970-01-01 00:00:00'    # coards is picky
@@ -78,11 +79,11 @@ def processDecimated(pw, url, outDir, lastDatetime, parms):
 
     logger.debug('url = %s', url)
     if outDir.startswith('/tmp'):
-        outFile_i = os.path.join(outDir, url.split('/')[-1].split('.')[0] + '_i.nc')
+	outFile_i = os.path.join(outDir, url.split('/')[-1].split('.')[0] + '_i.nc')
     else:
-        outFile_i = os.path.join(outDir, '/'.join(url.split('/')[-2:]).split('.')[0] + '_i.nc')
+	outFile_i = os.path.join(outDir, '/'.join(url.split('/')[-2:]).split('.')[0] + '_i.nc') 
 
-    startDatetime, endDatetime = getNcStartEnd(url)
+    startDatetime, endDatetime = getNcStartEnd(url, 'depth_time')
     logger.debug('startDatetime, endDatetime = %s, %s', startDatetime, endDatetime)
     logger.debug('lastDatetime = %s', lastDatetime)
     url_i = None
@@ -122,10 +123,10 @@ def process_command_line():
                                          description='Read lRAUV data transferred over hotstpot and .nc file in compatible CF1-6 Discrete Sampling Geometry for for loading into STOQS',
                                          epilog=examples)
                                              
-        parser.add_argument('-u', '--inUrl',action='store', help='url where hotspot logs are - must be the same location as -o directory', default='.',required=True)   
+        parser.add_argument('-u', '--inUrl',action='store', help='url where hotspot logs or other realtime processed data are.  If interpolating, must map to the same location as -o directory', default='.',required=True)   
         parser.add_argument('-b', '--database',action='store', help='name of database to load hotspot data to', default='.',required=True)  
         parser.add_argument('-c', '--campaign',action='store', help='name of campaign', default='.',required=True)    
-        parser.add_argument('-o', '--outDir', action='store', help='output directory to store .nc file - must be the same location as -u URL', default='.',required=True)   
+        parser.add_argument('-o', '--outDir', action='store', help='output directory to store interpolated .nc file - must be the same location as -u URL', default='.',required=False)   
         parser.add_argument('-d', '--description', action='store', help='Brief description of experiment')
         parser.add_argument('-a', '--append', action='store_true', help='Append data to existing Activity')
         parser.add_argument('-p', '--parms', action='store', help='List of space separated parameters to load', nargs='*', default=
@@ -139,7 +140,12 @@ if __name__ == '__main__':
     colors = {  'tethys':       'fed976',
                 'daphne':       'feb24c'}
   
-    args = process_command_line()
+    args = process_command_line() 
+    interpolate = False
+
+    # interpolating implied when specifying output directory to store interpolated files
+    if len(args.outDir) > 1: 
+	interpolate = True
 
     platformName = None; 
 
@@ -164,31 +170,48 @@ if __name__ == '__main__':
    
     # Get directory list from sites
     logger.info("Crawling %s for shore.nc files" % (args.inUrl))
-  
-    c = Crawl(os.path.join(args.inUrl, 'catalog.xml'), select=[".*shore_\d+_\d+.nc4$"], debug=False)
+
+    if interpolate:
+	c = Crawl(os.path.join(args.inUrl, 'catalog.xml'), select=[".*shore_\d+_\d+.nc4$"], debug=False)
+    else:
+	c = Crawl(os.path.join(args.inUrl, 'catalog.xml'), select=[".*shore_\d+_\d+.nc$"], debug=False)
+	
     urls = [s.get("url") for d in c.datasets for s in d.services if s.get("service").lower() == "opendap"]
+  
+    if interpolate:
+    	pw = lrauvNc4ToNetcdf.InterpolatorWriter()
 
-    pw = lrauvNc4ToNetcdf.InterpolatorWriter()
-
+    hasData = False
     # Look in time order - oldest to newest
     for url in sorted(urls):
-        (url_i, startDatetime, endDatetime) = processDecimated(pw, url, args.outDir, lastDatetime, args.parms)
+	if interpolate:
+	    (url_i, startDatetime, endDatetime) = processDecimated(pw, url, args.outDir, lastDatetime, args.parms)
+            if url_i:
+          	logger.info("Received new %s data ending at %s in %s" % (platformName, endDatetime, url_i))
+		# Use Hyrax server to avoid the stupid caching that the TDS does
+            	url_src = url_i.replace('http://elvis.shore.mbari.org/thredds/dodsC/LRAUV', 'http://dods.mbari.org/opendap/data/lrauv') 
+		hasData = True
+		stride = 10
+	else:
+		startDatetime, endDatetime = getNcStartEnd(url,'Time') 
+            	url_src = url.replace('http://elvis.shore.mbari.org/opendap/data/lrauv', 'http://dods.mbari.org/opendap/hyrax/data/lrauv') 
+		hasData = True
+		stride = 1 
+
         lastDatetime = endDatetime
 
-        if url_i:
-            logger.info("Received new %s data ending at %s in %s" % (platformName, endDatetime, url_i))
-            aName = platformName + '_sbdlog_' + startDatetime.strftime('%Y%m%dT%H%M%S')
-
-            # Use Hyrax server to avoid the stupid caching that the TDS does
-            url_i = url_i.replace('http://elvis.shore.mbari.org/thredds/dodsC/LRAUV', 'http://dods.mbari.org/opendap/data/lrauv')
+        if hasData:
+            logger.info("Received new %s data ending at %s in %s" % (platformName, endDatetime, url_src))
+            aName = platformName + '_sbdlog_' + startDatetime.strftime('%Y%m%dT%H%M%S')  
 
             dataStartDatetime = None
+
             if args.append:
                 # Return datetime of last timevalue - if data are loaded from multiple activities return the earliest last datetime value
                 dataStartDatetime = InstantPoint.objects.using(self.dbAlias).filter(activity__name=aName).aggregate(Max('timevalue'))['timevalue__max']
 
-            try:
-                logger.debug("Instantiating Lrauv_Loader for url_i = %s", url_i)
+            try: 
+	        logger.debug("Instantiating Lrauv_Loader for url = %s", url_src) 	 
                 lrauvLoad = DAPloaders.runLrauvLoader(cName = args.campaign,
                                                       cDesc = None,
                                                       aName = aName,
@@ -196,20 +219,20 @@ if __name__ == '__main__':
                                                       pName = platformName,
                                                       pTypeName = 'auv',
                                                       pColor = colors[platformName],
-                                                      url = url_i,
+                                                      url = url_src,
                                                       parmList = args.parms,
                                                       dbAlias = args.database,
-                                                      stride = 10,
+                                                      stride = stride,
                                                       startDatetime = startDatetime,
                                                       dataStartDatetime = dataStartDatetime,
                                                       endDatetime = endDatetime)
 
             except DAPloaders.NoValidData:
                 logger.info("No measurements in this log set. Activity was not created as there was nothing to load.")
+ 
+	    except pydap.exceptions.ServerError as e:
+		logger.warn(e)
 
-            except pydap.exceptions.ServerError as e:
-                logger.warn(e)
-
-            except DAPloaders.ParameterNotFound as e:
-                logger.warn(e)
+	    except DAPloaders.ParameterNotFound as e:
+		logger.warn(e)
 
