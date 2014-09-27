b5f7ea073f0b3422564bfae2895986d2db2a41e1
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-09-26 18:00:24 -0700

    Do not allow parameters with periods. Tolerate lrauv netCDFs with 'CF:featureType' for 'featureType'. Clean up Lrauv_Loader().

---
+++ contrib/analysis/drift_data.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+
+__author__ = "Mike McCann"
+__copyright__ = "Copyright 2014, MBARI"
+__license__ = "GPL"
+__maintainer__ = "Mike McCann"
+__email__ = "mccann at mbari.org"
+__status__ = "Development"
+__doc__ = '''
+
+Script produce products (plots, kml, etc.) to help understand drifting data.
+- Make progressive vector diagram from moored ADCP data (read from STOQS)
+- Plot drogued drifter data (read from Tracking DB)
+- Plot sensor data (read from STOQS)
+
+Output as a .png map, .kml file, or ...
+
+Mike McCann
+MBARI 22 September 2014
+
+@var __date__: Date of last svn commit
+@undocumented: __doc__ parser
+@author: __author__
+@status: __status__
+@license: __license__
+'''
+
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../../"))  # settings.py is one dir up
+
+import csv
+import urllib2
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+from collections import defaultdict
+from stoqs.models import MeasuredParameter, NominalLocation
+from mpl_toolkits.basemap import Basemap
+
+
+class Drift():
+    '''Data and methods to support drift data product preparation
+    '''
+    drifters = defaultdict(lambda: {'es': [], 'lat': [], 'lon': []})        # Keyed by drifter name
+    adcpDrift = defaultdict(lambda: {'es': [], 'lat': [], 'lon': []})       # Keyed by depth
+
+    def loadDrifterData(self):
+        '''Fill up drifters dictionary
+        '''
+        for url in self.args.drifterData:
+            # Careful - trackingdb returns the records in reverse time order
+            for r in csv.DictReader(urllib2.urlopen(url)):
+                # Use logic to skip appending values if one or the other or both start and end are specified
+                if self.startDatetime:
+                    if datetime.utcfromtimestamp(float(r['epochSeconds'])) < self.startDatetime:
+                        continue
+                if self.endDatetime:
+                    if datetime.utcfromtimestamp(float(r['epochSeconds'])) > self.endDatetime:
+                        continue
+
+                self.drifters[r['platformName']]['es'].append(float(r['epochSeconds']))
+                self.drifters[r['platformName']]['lat'].append(float(r['latitude']))
+                self.drifters[r['platformName']]['lon'].append(float(r['longitude']))
+
+
+    def computeADCPDrift(self):
+        '''Read data from database and put computed progressive vectors into adcpDrift dictionary
+        '''
+        if self.args.adcpPlatform:
+            adcpQS = MeasuredParameter.objects.using(self.args.database).filter(
+                                measurement__instantpoint__activity__platform__name=self.args.adcpPlatform)
+
+        if self.startDatetime:
+            adcpQS = adcpQS.filter(measurement__instantpoint__timevalue__gte=self.startDatetime)
+        if self.endDatetime:
+            adcpQS = adcpQS.filter(measurement__instantpoint__timevalue__lte=self.endDatetime)
+
+        if self.args.adcpMinDepth:
+            adcpQS = adcpQS.filter(measurement__depth__gte=self.args.adcpMinDepth)
+        if self.args.adcpMaxDepth:
+            adcpQS = adcpQS.filter(measurement__depth__lte=self.args.adcpMaxDepth)
+
+        utd = adcpQS.filter(parameter__standard_name='eastward_sea_water_velocity').values_list(
+                                'datavalue', 'measurement__instantpoint__timevalue', 'measurement__depth').order_by(
+                                        'measurement__depth', 'measurement__instantpoint__timevalue')
+        vtd = adcpQS.filter(parameter__standard_name='northward_sea_water_velocity').values_list(
+                                'datavalue', 'measurement__instantpoint__timevalue', 'measurement__depth').order_by(
+                                        'measurement__depth', 'measurement__instantpoint__timevalue')
+
+        # Compute positions (progressive vectors) - horizontal displacement in meters
+        x = defaultdict(lambda: [0])
+        y = defaultdict(lambda: [0])
+        for i, ((u, ut, ud), (v, vt, vd)) in enumerate(zip(utd, vtd)):
+            try:
+                udiff = utd[i+1][1] - ut
+                vdiff = vtd[i+1][1] - vt
+            except IndexError:
+                # Extrapolate using last time difference, assuming it's regular and that we are at the last point
+                udiff = utd[i-1][1] - utd[i][1]
+                vdiff = vtd[i-1][1] - vtd[i][1]
+                
+            if udiff != vdiff:
+                raise Exception('udiff != vdiff')
+            else:
+                dt = udiff.seconds + udiff.days * 24 * 3600
+
+            x[ud].append(u * dt / 1000)
+            y[ud].append(u * dt / 1000)
+
+        # Work in UTM space to add x & y offsets to begining position of the mooring
+        geom0 = NominalLocation.objects.using(self.args.database).filter(activity__platform__name=self.args.adcpPlatform).values_list('geom')[0][0]
+        import pdb
+        pdb.set_trace()
+
+    def process(self):
+        '''Read in data and build structures that we can generate products from
+        '''
+        self.loadDrifterData()
+        self.computeADCPDrift()
+
+
+    def getExtent(self):
+        '''For all data members find the min and max latitude and longitude
+        '''
+        lonMin = 180
+        lonMax = -180
+        latMin = 90
+        latMax = -90
+        for k,v in self.drifters.iteritems():
+            import pdb
+            pdb.set_trace()
+            if np.min(v['lon']) < lonMin:
+                lonMin = np.min(v['lon'])
+            if np.max(v['lon']) > lonMax:
+                lonMax = np.max(v['lon'])
+            if np.min(v['lat']) < latMin:
+                latMin = np.min(v['lat'])
+            if np.max(v['lat']) > latMax:
+                latMax = np.max(v['lat'])
+
+        return lonMin, latMin, lonMax, latMax
+
+    def createPNG(self):
+        '''Draw processed data on a map and save it as a .png file
+        '''
+        e = self.getExtent() 
+        import pdb
+        pdb.set_trace()
+        m = Basemap(llcrnrlon=e[0], llcrnrlat=e[1], urcrnrlon=e[2], urcrnrlat=e[3], projection='cyl', resolution=resolution, ax=ax)
+        m.arcgisimage(server='http://services.arcgisonline.com/ArcGIS', service='Ocean_Basemap')
+
+
+    def saveFigure(self, fig, figCount):
+        '''
+        Save this page
+        '''
+        provStr = 'Created with STOQS command ' + '\\\n'.join(wrap(self.commandline, width=160)) + ' on ' + datetime.now().ctime()
+        plt.figtext(0.0, 0.0, provStr, size=7, horizontalalignment='left', verticalalignment='bottom')
+        plt.tight_layout()
+        if self.args.title:
+            fig.text(0.5, 0.975, self.args.title, horizontalalignment='center', verticalalignment='top')
+
+        fileName = self.getFileName(figCount)
+        if self.args.verbose:
+            print '  Saving file', fileName
+        fig.savefig(fileName)
+
+    def createGeoTiff(self):
+        '''Your image must be only the geoplot with no decorations like axis titles, axis labels, etc., and you 
+        will need accurate upper-left and lower-right coordinates in EPSG:4326 projection, also known as WGS 84 projection,...
+
+        The syntax is pretty straightforward, something like the following will convert your image to the correct format:
+
+            gdal_translate <image.png> <image.tiff> -a_ullr -122.25 37.1 -121.57365 36.67558 
+
+        There is also a python wrapper for the GDAL library
+ 
+        https://pypi.python.org/pypi/GDAL/
+        '''
+
+    def process_command_line(self):
+        '''
+        The argparse library is included in Python 2.7 and is an added package for STOQS.
+        '''
+        import argparse
+        from argparse import RawTextHelpFormatter
+
+        examples = 'Examples:' + '\n\n' 
+        examples += "M1 ADCP progressive vector diagram and Stella drifter data:\n"
+        examples += sys.argv[0] + " --database stoqs_september2014 --adcpPlatform M1_Mooring --adcpMinDepth 30 --adcpMaxDepth 40"
+        examples += " --drifterData http://odss.mbari.org/trackingdb/position/stella101/between/20140922T171500/20141010T000000/data.csv"
+        examples += " http://odss.mbari.org/trackingdb/position/stella110/between/20140922T171500/20141010T000000/data.csv"
+        examples += " http://odss.mbari.org/trackingdb/position/stella122/between/20140922T171500/20141010T000000/data.csv"
+        examples += "\n"
+        examples += '\nIf running from cde-package replace ".py" with ".py.cde" in the above list.'
+    
+        parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
+                                         description='Script to produce products to help understand drift caused by currents in the ocean',
+                                         epilog=examples)
+                                             
+        parser.add_argument('-d', '--database', action='store', help='Database alias', default='stoqs_september2014', required=True)
+
+        parser.add_argument('--adcpPlatform', action='store', help='STOQS Platform Name for ADCP data')
+        parser.add_argument('--adcpMinDepth', action='store', help='Minimum depth of ADCP data for progressive vector data', type=float)
+        parser.add_argument('--adcpMaxDepth', action='store', help='Maximum depth of ADCP data for progressive vector data', type=float)
+
+        parser.add_argument('--drifterData', action='store', help='List of MBARItracking database .csv urls for drifter data', nargs='*', default=[])
+    
+        parser.add_argument('--start', action='store', help='Start time in YYYYMMDDTHHMMSS format')
+        parser.add_argument('--end', action='store', help='End time in YYYYMMDDTHHMMSS format')
+
+        parser.add_argument('--kmlFileName', action='store', help='Name of file for KML output')
+        parser.add_argument('--pngFileName', action='store', help='Name of file for PNG image of map')
+        parser.add_argument('--geotiffFileName', action='store', help='Name of file for geotiff image of map')
+
+        parser.add_argument('-v', '--verbose', nargs='?', choices=[1,2,3], type=int, help='Turn on verbose output. Higher number = more output.', const=1)
+    
+        self.args = parser.parse_args()
+        self.commandline = ' '.join(sys.argv)
+
+        self.startDatetime = None
+        if self.args.start:
+            self.startDatetime = datetime.strptime(self.args.start, '%Y%m%dT%H%M%S')
+        self.endDatetime = None
+        if self.args.end:
+            self.endDatetime = datetime.strptime(self.args.end, '%Y%m%dT%H%M%S')
+    
+    
+if __name__ == '__main__':
+
+    d = Drift()
+    d.process_command_line()
+
+    d.process()
+    if d.args.kmlFileName:
+        d.createKML()
+
+    if d.args.pngFileName:
+        d.createPNG()
+
---
+++ loaders/CANON/JM_loadsep2013.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+__author__    = 'Mike McCann,Duane Edgington,Reiko Michisaki'
+__copyright__ = '2013'
+__license__   = 'GPL v3'
+__contact__   = 'duane at mbari.org'
+
+__doc__ = '''
+
+Master loader for all CANON activities in September 2013
+
+Mike McCann; Modified by Duane Edgington and Reiko Michisaki
+MBARI 02 September 2013
+
+@var __date__: Date of last svn commit
+@undocumented: __doc__ parser
+@status: production
+@license: GPL
+'''
+
+import os
+import sys
+import datetime  # needed for glider data
+import time      # for startdate, enddate args
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+project_dir = os.path.dirname(__file__)
+
+# the next line makes it possible to find CANON
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # this makes it possible to find CANON, one directory up
+
+from CANON import CANONLoader
+       
+# building input data sources object
+from socket import gethostname
+hostname=gethostname()
+print hostname
+if hostname=='odss-test.shore.mbari.org':
+    cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
+else:
+    cl = CANONLoader('stoqs_september2013', 'CANON - September 2013')
+
+#25613JMC01.nc                                                                              100% 3172     3.1KB/s   00:00
+#25613JMC02.nc                                                                              100% 6268     6.1KB/s   00:00
+#25613JMC03.nc                                                                              100% 5692     5.6KB/s   00:00
+#25613JMC04.nc                                                                              100% 6340     6.2KB/s   00:00
+#25613JMC05.nc                                                                              100% 4468     4.4KB/s   00:00
+#26013JMC01.nc                                                                              100% 6484     6.3KB/s   00:00
+#26013JMC02.nc                                                                              100% 3460     3.4KB/s   00:00
+#26013JMC03.nc                                                                              100% 6484     6.3KB/s   00:00
+#26013JMC04.nc                                                                              100% 5692     5.6KB/s   00:00
+#uctd/25413JMm01.txt
+#uctd/25613JMm01.txt
+
+# default location of thredds and dods data:
+cl.tdsBase = 'http://odss.mbari.org/thredds/'
+cl.dodsBase = cl.tdsBase + 'dodsC/'
+
+
+######################################################################
+#  John Martin: September 16-20? (259-262) Sep 30 - Oct 3
+######################################################################
+# UCTD
+cl.JMuctd_base = cl.dodsBase + 'CANON_september2013/Platforms/Ships/Martin/uctd/'
+cl.JMuctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'wetstar' ]
+cl.JMuctd_files = [ 
+                        '25913RCm01.nc', '26013RCm01.nc', 
+                      ]
+
+# PCTD
+cl.pctdDir = 'CANON_september2013/Platforms/Ships/Martin/pctd/'
+cl.JMpctd_base = cl.dodsBase + cl.pctdDir
+cl.JMpctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'ecofl', 'oxygen' ]
+cl.JMpctd_files = [ 
+                    '25613JMC01.nc', '25613JMC02.nc', '25613JMC03.nc', '25613JMC04.nc', '25613JMC05.nc',
+                    '26013JMC01.nc', '26013JMC02.nc', '26013JMC03.nc','26013JMC04.nc', 
+                      ]
+# BCTD
+# SubSample data files from /mbari/BOG_Archive/ReportsForSTOQS/GOC12/ copied to local BOG_Data dir
+#cl.bctdDir = 'CANON_september2013/Platforms/Ships/Rachel_Carson/bctd/'
+#cl.subsample_csv_base = cl.dodsBase + cl.bctdDir
+#cl.subsample_csv_base = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BOG_Data')
+#cl.subsample_csv_files = [
+#                            'STOQS_canon13_CHL_1U.csv', 'STOQS_canon13_CHL_5U.csv', 'STOQS_canon13_NH4.csv', 'STOQS_canon13_NO2.csv', 'STOQS_canon13_NO3.csv', 
+#			    'STOQS_canon13_OXY_ML.csv', 'STOQS_canon13_PHAEO_1U.csv', 'STOQS_canon13_PHAEO_5U.csv',
+#                            'STOQS_canon13_PHAEO_GFF.csv', 'STOQS_canon13_PO4.csv', 'STOQS_canon13_SIO4.csv', 'STOQS_canon13_CARBON_GFF.csv',
+#                            'STOQS_canon13_CHL_GFF.csv',
+#                         ]
+
+
+###################################################################################################################
+# Execute the load
+cl.process_command_line()
+
+if cl.args.test:
+#    cl.loadJMuctd(stride=1)
+    cl.loadJMpctd(stride=1)
+
+elif cl.args.optimal_stride:
+   #cl.loadMartin(stride=1)
+#    cl.loadJMuctd(stride=1)
+    cl.loadJMpctd(stride=1)
+
+else:
+#    cl.loadDorado(stride=cl.args.stride)
+#    cl.loadJMuctd(stride=1)
+    cl.loadJMpctd(stride=1)
