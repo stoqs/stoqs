71488452cc690b377c0e3a623f4e548ad8415cf8
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-10-01 09:28:03 -0700

    Made to work on kraken from cron

--- contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
+++ contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Thursday's rhodamine deployment start time
 
+cd /opt/stoqshg
+source venv-stoqs/bin/activate
+cd contrib/analysis
+
 START=20140925T170000
 END=20141010T230000
 
@@ -8,7 +12,7 @@ END=20141010T230000
 # stella(s) for just the times in the water
 # Stride daphne to avoid spurious fixes
 
-./drift_data.py --database stoqs_september2014_kraken --adcpPlatform M1_Mooring --adcpMinDepth 35 --adcpMaxDepth 45 --trackData \
+./drift_data.py --database stoqs_september2014 --adcpPlatform M1_Mooring --adcpMinDepth 35 --adcpMaxDepth 45 --trackData \
     http://odss.mbari.org/trackingdb/position/stella101/between/20140925T180000/20140929T150000/data.csv \
     http://odss.mbari.org/trackingdb/position/R_CARSON/between/20140925T150000/20140925T200000/data.csv \
     http://odss.mbari.org/trackingdb/position/m1/between/$START/$END/data.csv \
