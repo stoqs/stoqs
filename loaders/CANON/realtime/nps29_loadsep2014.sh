7f864d8c7d8812c77d395578fe8ff7f077d88c86
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-10-01 14:47:17 -0700

    Remove -v flag

--- loaders/CANON/realtime/nps29_loadsep2014.sh
+++ loaders/CANON/realtime/nps29_loadsep2014.sh
@@ -2,4 +2,4 @@
 STOQSDIR=/opt/stoqshg
 cd $STOQSDIR
 source venv-stoqs/bin/activate
-loaders/CANON/realtime/nps29_loadsep2014.py --append -v > loaders/CANON/realtime/nps29_loadsep2014.out 2>&1
+loaders/CANON/realtime/nps29_loadsep2014.py --append > loaders/CANON/realtime/nps29_loadsep2014.out 2>&1
