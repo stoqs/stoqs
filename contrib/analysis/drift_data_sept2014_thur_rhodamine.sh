8bbe0510503426f14e1e32c47ca6e6a199cd5f63
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-10-02 15:59:28 -0700

    Replace daphne with makai, use a DATABASE variable

--- contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
+++ contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
@@ -8,10 +8,12 @@ then
     cd /home/stoqsadm/dev/stoqshg
     source venv-stoqs/bin/activate
     cd contrib/analysis
+    DATABASE=stoqs_september2014
     PRODUCTDIR=/data/canon/2014_Sep/Products/STOQS_Plots
     MAPDIR=/data/mapserver/mapfiles/2014Fall/drift
     LOGFILE='/home/stoqsadm/dev/stoqshg/contrib/analysis/drift_data_sept2014_thur_rhodamine.out'
 else
+    DATABASE=stoqs_september2014_kraken
     PRODUCTDIR=.
     MAPDIR=.
     LOGFILE=/dev/tty
@@ -22,13 +24,14 @@ END=20141010T230000
 
 # R_CARSON for the time of rhodamine pumping
 # stella(s) for just the times in the water
-# Stride daphne to avoid spurious fixes
+# Stride daphne and makai to avoid spurious fixes
 
-./drift_data.py --database stoqs_september2014 --adcpPlatform M1_Mooring --adcpMinDepth 35 --adcpMaxDepth 45 --trackData \
+##    http://odss.mbari.org/trackingdb/position/daphne/between/$START/20141001T180000/stride/11/data.csv \
+./drift_data.py --database $DATABASE --adcpPlatform M1_Mooring --adcpMinDepth 35 --adcpMaxDepth 45 --trackData \
     http://odss.mbari.org/trackingdb/position/stella101/between/20140925T180000/20140929T150000/data.csv \
     http://odss.mbari.org/trackingdb/position/R_CARSON/between/20140925T150000/20140925T200000/data.csv \
     http://odss.mbari.org/trackingdb/position/m1/between/$START/$END/data.csv \
-    http://odss.mbari.org/trackingdb/position/daphne/between/$START/20141001T203600/stride/11/data.csv \
+    http://odss.mbari.org/trackingdb/position/makai_ac/between/20141001T104000/$END/data.csv \
     http://odss.mbari.org/trackingdb/position/wgTiny/between/$START/$END/data.csv \
     --extent -122.3 36.3 -121.75 37.0 \
     --start $START --end $END \
