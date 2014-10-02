31248d09fd64acc916bb9e44a11b4a9f2a264e0a
Mike McCann MBARIMike <Mike McCann MBARIMike@gmail.com>
2014-10-02 10:52:28 -0700

    Set and end date for daphne so that its label does not bleed off of the map which screws up the geotiff registration

--- contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
+++ contrib/analysis/drift_data_sept2014_thur_rhodamine.sh
@@ -19,7 +19,7 @@ MAPDIR=/data/mapserver/mapfiles/2014Fall/drift
     http://odss.mbari.org/trackingdb/position/stella101/between/20140925T180000/20140929T150000/data.csv \
     http://odss.mbari.org/trackingdb/position/R_CARSON/between/20140925T150000/20140925T200000/data.csv \
     http://odss.mbari.org/trackingdb/position/m1/between/$START/$END/data.csv \
-    http://odss.mbari.org/trackingdb/position/daphne/between/$START/$END/stride/11/data.csv \
+    http://odss.mbari.org/trackingdb/position/daphne/between/$START/20141001T203600/stride/11/data.csv \
     http://odss.mbari.org/trackingdb/position/wgTiny/between/$START/$END/data.csv \
     --extent -122.3 36.3 -121.75 37.0 \
     --start $START --end $END \
