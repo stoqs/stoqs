e97e3ec69073d75b83804ae5de66fa549e4bf753
Mike McCann <MBARIMike@gmail.com>
2015-04-18 14:51:46 -0700

    Unescape the quotes for psql commands

--- load.sh
+++ load.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Do database operations to create default database and load data for testing
 
-psql -c \"CREATE USER stoqsadm WITH PASSWORD 'CHANGEME';\" -U postgres
-psql -c \"CREATE DATABASE template_postgis WITH TEMPLATE postgis;\" -U postgres
-psql -c \"CREATE DATABASE stoqs owner=stoqsadm template=template_postgis;\" -U postgres
-psql -c \"ALTER DATABASE stoqs SET TIMEZONE='GMT';\" -U postgres
-psql -c -d stoqs \"GRANT ALL ON ALL TABLES IN SCHEMA public TO stoqsadm;\" -U postgres
+psql -c "CREATE USER stoqsadm WITH PASSWORD 'CHANGEME';" -U postgres
+psql -c "CREATE DATABASE template_postgis WITH TEMPLATE postgis;" -U postgres
+psql -c "CREATE DATABASE stoqs owner=stoqsadm template=template_postgis;" -U postgres
+psql -c "ALTER DATABASE stoqs SET TIMEZONE='GMT';" -U postgres
+psql -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO stoqsadm;" -U postgres -d stoqs
 
 # Assume in stoqsgit directory
 export DJANGO_SECRET_KEY='SET_YOUR_OWN_IMPOSSIBLE_TO_GUESS_SECRET_KEY_ENVIRONMENT_VARIABLE'
