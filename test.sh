5777762515251469dd68b72a5805b43b0826e877
Mike McCann <MBARIMike@gmail.com>
2015-04-22 00:05:47 -0700

    Move numpy and matplotlib back to requirements/base.txt, trying to have travis role create test_stoqs

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 # Run tests using the continuous integration configuaration
 
 export DJANGO_SECRET_KEY='SET_YOUR_OWN_IMPOSSIBLE_TO_GUESS_SECRET_KEY_ENVIRONMENT_VARIABLE'
-export DATABASE_URL="postgis://postgres:@localhost:5432/stoqs_test"
+export DATABASE_URL="postgis://postgres:@localhost:5432/test_stoqs"
 cd stoqs
+# test_stoqs database created and dropped by role of the shell account
 ./manage.py test stoqs.tests --settings=config.ci
