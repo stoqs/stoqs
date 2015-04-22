bfbcf8f0679ae8735bf6e6b03c3346426036a23f
Mike McCann <MBARIMike@gmail.com>
2015-04-21 20:58:48 -0700

    Create travis DB role to try and get test.sh to run

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 
 export DJANGO_SECRET_KEY='SET_YOUR_OWN_IMPOSSIBLE_TO_GUESS_SECRET_KEY_ENVIRONMENT_VARIABLE'
 export DATABASE_URL="postgis://stoqsadm:CHANGEME@127.0.0.1:5432/stoqs"
+id
 cd stoqs
 ./manage.py test stoqs.tests --settings=config.ci
