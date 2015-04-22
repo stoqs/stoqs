44c4a575b9e0b19d012f83c8ca24428db52491a0
Mike McCann <MBARIMike@gmail.com>
2015-04-21 23:03:47 -0700

    Try using postgres login for stoqs_test database in test.sh - simplify requirements

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 # Run tests using the continuous integration configuaration
 
 export DJANGO_SECRET_KEY='SET_YOUR_OWN_IMPOSSIBLE_TO_GUESS_SECRET_KEY_ENVIRONMENT_VARIABLE'
-export DATABASE_URL="postgis://stoqsadm:CHANGEME@127.0.0.1:5432/stoqs"
-id
+export DATABASE_URL="postgis://postgres:@localhost:5432/stoqs_test"
 cd stoqs
 ./manage.py test stoqs.tests --settings=config.ci
