faa029509e5e4d495f5e33f85bda32a611f0ec14
Mike McCann <MBARIMike@gmail.com>
2015-04-27 07:58:45 -0700

    Try 'coveralls --base_dir stoqs --verbose'

--- test.sh
+++ test.sh
@@ -16,7 +16,8 @@ stoqs/loaders/loadTestData.py
 # test_stoqs database created and dropped by role of the shell account using Test framework's DB names
 cd stoqs
 unset DATABASE_URL
-coverage run --source="utils,stoqs" ./manage.py test stoqs.tests --settings=config.ci
+coverage run --source=utils,stoqs ./manage.py test stoqs.tests --settings=config.ci
 coverage report
+cd ..
 
 
