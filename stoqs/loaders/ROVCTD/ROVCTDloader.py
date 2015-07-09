d9100d96292e3f81c9c36dc112d9e413c5178ae0
Mike McCann <MBARIMike@gmail.com>
2015-07-08 22:09:52 -0700

    Create ROVCTD directory for specialized loaders such as loadAllTransectDives.sh

--- stoqs/loaders/ROVCTD/ROVCTDloader.py
+++ stoqs/loaders/ROVCTD/ROVCTDloader.py
@@ -28,7 +28,7 @@ import sys
 
 # Insert Django App directory (parent of config) into python path 
 sys.path.insert(0, os.path.abspath(os.path.join(
-                    os.path.dirname(__file__), "../")))
+                    os.path.dirname(__file__), "../../")))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.local'
 os.environ['DJANGO_CONFIGURATION'] = 'Local'
 
