2058d49172bb965cd98281070f1a2dd1b96fa052
Mike McCann <MBARIMike@gmail.com>
2015-07-25 14:51:21 -0700

    Change syncdb to makemigrations/migrate; trying to get Selenium tests working in test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,8 @@ psql -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO stoqsadm;" -U postgres -d s
 # Set environment variables and create initial default database that is used for testing
 export DJANGO_SECRET_KEY='SET_YOUR_OWN_IMPOSSIBLE_TO_GUESS_SECRET_KEY'
 export DATABASE_URL="postgis://stoqsadm:CHANGEME@127.0.0.1:5432/stoqs"
-stoqs/manage.py syncdb --settings=config.settings.local --noinput --database=default
+stoqs/manage.py makemigrations stoqs --settings=config.settings.local --noinput
+stoqs/manage.py migrate --settings=config.settings.local --noinput --database=default
 
 # Assume starting in project home (stoqsgit) directory, get bathymetry, and load data
 cd stoqs
@@ -27,7 +28,7 @@ coverage run -a --include="contrib/analysis/classify.py" contrib/analysis/classi
 
 # Run tests using the continuous integration setting and default Local class configuration
 # test_stoqs database created and dropped by role of the shell account using Test framework's DB names
-./manage.py dumpdata --settings=config.settings.ci stoqs > fixtures/stoqs_test_data.json
+./manage.py dumpdata --settings=config.settings.ci stoqs > stoqs/fixtures/stoqs_test_data.json
 unset DATABASE_URL
 coverage run -a --source=utils,stoqs ./manage.py test stoqs.tests.unit_tests --settings=config.settings.ci
 unit_tests_status=$?
