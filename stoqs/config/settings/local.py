1a9f6258528623b901a9a905a54e1aa069fe688f
Mike McCann <MBARIMike@gmail.com>
2015-08-07 16:30:30 -0700

    VM IP changed during the day

--- stoqs/config/settings/local.py
+++ stoqs/config/settings/local.py
@@ -62,4 +62,4 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Your local stuff: Below this line define 3rd party library settings
 
 # Must be externally accessible if your STOQS server is to be externally accessible
-MAPSERVER_HOST = '172.16.130.203'
+MAPSERVER_HOST = '172.16.130.204'
