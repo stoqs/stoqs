008b6a51c3c8c2e0dea472697de51b0b5a792f56
Danelle Cline <danelle.cline%40att.net@localhost>
2015-09-23 22:43:27 -0700

    Modified to support animated graphs with nighttime shading.

--- stoqs/loaders/CANON/realtime/monitorLrauv.py
+++ stoqs/loaders/CANON/realtime/monitorLrauv.py
@@ -189,19 +189,15 @@ def process_command_line():
                                                                  ' If interpolating, must map to the same location as -o directory',
                             default='http://elvis.shore.mbari.org/thredds/catalog/LRAUV/daphne/realtime/cell-logs/.*Normal*.nc4$',required=False)'''
 
-        #parser.add_argument('-b', '--database',action='store', help='name of database to load hotspot data to', default='default',required=True)
         parser.add_argument('-b', '--database',action='store', help='name of database to load hotspot data to', default='stoqs_lrauv',required=False)
-        #parser.add_argument('-c', '--campaign',action='store', help='name of campaign', default='.',required=True)
         parser.add_argument('-c', '--campaign',action='store', help='name of campaign', default='April 2015 testing',required=False)
         parser.add_argument('-s', '--stride',action='store', help='amount to stride data before loading e.g. 10=every 10th point', default=1)
-        #parser.add_argument('-o', '--outDir', action='store', help='output directory to store interpolated .nc file or contour output - must map to the same location as -u URL', default='', required=False)
         parser.add_argument('-o', '--outDir', action='store', help='output directory to store interpolated .nc file or log contour output '
                                                                    '- must map to the same location as -u URL', default='/tmp/TestMonitorLrauv', required=False)
-        #parser.add_argument('-t', '--contourUrl', action='store', help='base url to store cross referenced contour plot resources', default='.',required=True)
+         parser.add_argument('--zoom', action='store', help='time window in hours to zoom animation',default=6, required=False)
+        parser.add_argument('--overlap', action='store', help='time window in hours to overlap animation',default=5, required=False)
         parser.add_argument('--contourUrl', action='store', help='base url to store cross referenced contour plot resources',
                             default='http://dods.mbari.org/opendap/data/lrauv/stoqs/',required=False)
-        #parser.add_argument('-u', '--contourDir', action='store', help='output directory to store 24 hour contour output',
-                            #default='/tmp/TestMonitorLrauv/',required=True)
         parser.add_argument('--iparm', action='store', help='parameter to interpolate against; must exist in the -p/--parms list',
                             default='bin_mean_mass_concentration_of_chlorophyll_in_sea_water',required=False)
         parser.add_argument('--plotDotParmName', action='store', help='parameter to plot as colored dot in map; must exist in the -p/--parms list',
@@ -386,7 +382,7 @@ if __name__ == '__main__':
 
                 logger.debug('out file %s', outFile)
 
-                c = Contour(startDatetimeUTC, endDatetimeUTC, args.database, [platformName], plot_group, title, outFile, False, args.autoscale, args.plotDotParmName, args.booleanPlotGroup)
+                c = Contour(startDatetimeUTC, endDatetimeUTC, args.database, [platformName], plot_group, title, outFile, args.autoscale, args.plotDotParmName, args.booleanPlotGroup)
                 c.run()
 
                 # Replace netCDF file with png extension and that is the URL of the log
@@ -415,7 +411,7 @@ if __name__ == '__main__':
                 url = args.contourUrl + platformName  + '_log_' + startDateTimeUTC24hr.strftime('%Y%m%dT%H%M%S') + '_' + endDateTimeUTC24hr.strftime('%Y%m%dT%H%M%S') + '.png'
 
                 logger.debug('out file %s url: %s ', outFile, url)
-                c = Contour(startDateTimeUTC24hr, endDateTimeUTC24hr, args.database, [platformName], args.plotgroup, title, outFile, False, args.autoscale, args.booleanPlotGroup)
+                c = Contour(startDateTimeUTC24hr, endDateTimeUTC24hr, args.database, [platformName], args.plotgroup, title, outFile, args.autoscale, args.booleanPlotGroup)
                 c.run()
 
                 if outFile.startswith('/tmp'):
@@ -460,8 +456,13 @@ if __name__ == '__main__':
 
             outFileLatest = args.contourDir + '/' + platformName  + '_24h_latest.png'
             outFileLatestProduct = args.productDir + '/' + platformName  + '_log_last24hr.png'
+            outFileLatestAnim = args.contourDir + '/' + platformName  + '_24h_latest_anim.gif'
+            outFileLatestProductAnim = args.productDir + '/' + platformName  + '_log_last24hr_anim.gif'
 
-            c = Contour(nowStartDateTimeUTC24hr, nowEndDateTimeUTC24hr, args.database, [platformName], args.plotgroup, title, outFileLatest, False, args.autoscale)
+            c = Contour(nowStartDateTimeUTC24hr, nowEndDateTimeUTC24hr, args.database, [platformName], args.plotgroup, title, outFileLatest, args.autoscale)
+            c.run()
+
+            c = Contour(nowStartDateTimeUTC24hr, nowEndDateTimeUTC24hr, args.database, [platformName], args.plotgroup, title, outFileLatestAnim, args.autoscale, True, args.zoom, args.overlap)
             c.run()
 
             if outFileLatest.startswith('/tmp'):
@@ -475,6 +476,11 @@ if __name__ == '__main__':
             else:
                 # copy to the atlas share that will get cataloged in ODSS
                 cmd = r'cp %s %s' %(outFileLatest, outFileLatestProduct)
+                logger.debug('%s', cmd)
+                os.system(cmd)
+
+                # copy to the atlas share that will get cataloged in ODSS
+                cmd = r'cp %s %s' %(outFileLatestAnim, outFileLatestProductAnim)
                 logger.debug('%s', cmd)
                 os.system(cmd)
 
