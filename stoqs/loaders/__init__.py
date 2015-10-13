da86cb4afd12dda85de64f270f250a10f2aedd34
Mike McCann <MBARIMike@gmail.com>
2015-10-13 00:18:19 -0700

    Add nominaldepth to PlatformResource for X3D model

--- stoqs/loaders/__init__.py
+++ stoqs/loaders/__init__.py
@@ -59,7 +59,7 @@ from django.db.backends.utils import CursorWrapper
 MEASUREDINSITU = 'Measured in situ'
 
 # Constants for a Resource and ResourceType names - to be also used in utils/Viz
-X3D_MODEL = 'X3D_model'
+X3D_MODEL = 'X3D_MODEL'
 X3DPLATFORMMODEL = 'x3dplatformmodel'
 
 if settings.DEBUG:
@@ -251,28 +251,35 @@ class LoadScript(object):
         activity = m.Activity.objects.using(self.dbAlias).get(name=aName)
         
         resource, created = m.Resource.objects.using(self.dbAlias).get_or_create(
-                            uristring=x3dmodelurl, name='X3D_MODEL', value='Output from bed2x3d.py - uristring to be included in GeoLocation node', resourcetype=resourceType)
+                            uristring=x3dmodelurl, name=X3D_MODEL, value='Output from bed2x3d.py - uristring to be included in GeoLocation node', resourcetype=resourceType)
         cr, created = m.ActivityResource.objects.using(self.dbAlias).get_or_create(
                             activity=activity, resource=resource)
         self.logger.info('Resource uristring=%s', x3dmodelurl)
 
-    def addPlatformResources(self, x3dmodelurl, pName, value='X3D model'):
-        '''
-        Add Resources to Platform.  Used initially for adding X3D model of a platform.  Can put additional descriptive 
-        information in value option, e.g.: "X3D model derived from SolidWorks model of ESP and processed through aopt"
+    def addPlatformResources(self, x3dmodelurl, pName, value='X3D model', nominaldepth=0.0):
+        '''Add Resources to Platform.  Used initially for adding X3D model of a platform.
+        Can put additional descriptive information in value option, e.g.: "X3D model 
+        derived from SolidWorks model of ESP and processed through aopt"
         '''
 
         resourceType, created = m.ResourceType.objects.using(self.dbAlias).get_or_create(
-                                name=X3DPLATFORMMODEL, description='X3D scene for model of a platform')
+                name=X3DPLATFORMMODEL, description='X3D scene for model of a platform')
 
         self.logger.info('Adding to ResourceType: %s', resourceType)
         self.logger.debug('Looking in database %s for Platform name = %s', self.dbAlias, pName)
         platform = m.Platform.objects.using(self.dbAlias).get(name=pName)
         
-        resource, created = m.Resource.objects.using(self.dbAlias).get_or_create(
-                            uristring=x3dmodelurl, name='X3D_MODEL', value=value, resourcetype=resourceType)
-        cr, created = m.PlatformResource.objects.using(self.dbAlias).get_or_create(
-                            platform=platform, resource=resource)
+        r_url, created = m.Resource.objects.using(self.dbAlias).get_or_create(
+                uristring=x3dmodelurl, name=X3D_MODEL, value=value, resourcetype=resourceType)
+        m.PlatformResource.objects.using(self.dbAlias).get_or_create(
+                platform=platform, resource=r_url)
+        r_nd, created = m.Resource.objects.using(self.dbAlias).get_or_create(
+                uristring=x3dmodelurl, 
+                name='X3D_MODEL_nominaldepth', value=nominaldepth, resourcetype=resourceType)
+        m.PlatformResource.objects.using(self.dbAlias).get_or_create(
+                platform=platform, resource=r_nd)
+
+
         self.logger.info('Resource uristring=%s', x3dmodelurl)
 
 
