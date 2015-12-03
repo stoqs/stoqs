613096eabd8cb0bf2c56f79b62efb1e34ea33b56
Mike McCann <MBARIMike@gmail.com>
2015-12-02 16:55:48 -0800

    Staring to make work for PP.csv and 'beyond' pep-8ing

--- stoqs/loaders/planktonpump.py
+++ stoqs/loaders/planktonpump.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+#
+# From: Julio Harvey <jharvey@mbari.org>
+# Subject: Erin's plankton pump data for STOQS
+# Date: December 1, 2015 at 5:04:43 PM PST
+# To: Mike McCann <mccann@mbari.org>
+# 
+# Hi Mike,
+# 
+# I've attached the plankton pump data in tidy format, thanks in advance 
+# for your help getting it into stoqs.
+# 
+# I heard back from Erin and the temporal story is that plankton pump 
+# sample uptake durations were for 10 minutes each time.  Shall we call 
+# the sampling start time as 1 minute after the last of the four Niskins 
+# were fired at each depth? If you have a better call, I'll defer to your 
+# judgement.
+# 
+# Feel free to raise issues with column headings or anything else as needed.
+# 
+# Thanks again!
+# 
+# Julio
 
 '''
 Script to load Sampling events for Plankton Pump data
@@ -36,9 +58,8 @@ class PlanktonPump():
         '''Read records into a hash keyed on 'Cruise', typically the CTD cast 
         name. The values of the hash are a list of sample_metadata (sm) hashes
         which are then checked for consistency. For SIMZ cruises the convention
-        has been to conduct one net tow per CTD cast. If data differences 
-        indicate that more than one tow has been done then raise an
-        exception.
+        has been to conduct one plankton pump per group of 4 Niskin bottle trips,
+        3 pumps per cast. 
         '''
         cast_hash = defaultdict(lambda: [])
         with open(self.args.subsampleFile) as f:
@@ -47,40 +68,46 @@ class PlanktonPump():
                 sm['name'] = r.get('Name', '')
                 sm['depth'] = r.get('Depth [m]', '')
                 sm['sampletype'] = r.get('Sample Type', '')
-                sm['volume'] = r.get('Sample Volume [mL]', '')
-                if not sm['volume']:
-                    sm['volume'] = r.get('Sample Volume [m^3]', '')
-                sm['filterdiameter'] = r.get('Filter Diameter [mm]', '')
-                try:
-                    sm['filterporesize'] = float(r.get('Filter Pore Size [um]'))
-                except ValueError:
-                    sm['filterporesize'] = float(r.get('Filter Pore Size [um]').split()[0])
+                sm[r.get('Comment Name')] = r.get('Comment Value', '')
+
                 cast_hash[r.get('Cruise')].append(sm)
 
         # Ensure consistency of sample metadata following SIMZ convention
         cast_hash_consistent = OrderedDict()
-        for cast,sm_list in sorted(cast_hash.items()):
+        for cast, sm_list in sorted(cast_hash.items()):
             for sm_hash in sm_list[1:]:
                 self.logger.debug('Checking consistency of record %s', sm_hash)
                 assert_equal(sm_list[0], sm_hash)
 
             cast_hash_consistent[cast] = sm_list[0]
                     
+        import pdb; pdb.set_trace()
         return cast_hash_consistent
+
+    def _pumping_depth(self):
+        '''Compare with cluster of Niskin bottles and return depth of the
+        Plankton Pump
+        '''
+        pass
 
     def _db_join(self, sm_hash):
         '''Join Sample data with data already in the database for the CTD cast.
         Use Cruise/Cast/Activity name as the key to get time, geom, and 
         environmental information.
         '''
-        self.logger.info('Joining subsample information with cast data from the database using subtractMinutes = %d', self.args.subtractMinutes)
+        self.logger.info('Joining subsample information with cast data from the'
+                         ' database using add_minutes = %d', self.args.add_minutes)
         new_hash = OrderedDict()
-        for a_name,v in sm_hash.iteritems():
+        import pdb; pdb.set_trace()
+        for a_name, v in sm_hash.iteritems():
             try:
+                import pdb; pdb.set_trace()
                 a = Activity.objects.using(self.args.database).get(name__contains=a_name)
+                import pdb; pdb.set_trace()
                 v['longitude'] = a.mappoint.x
                 v['latitude'] = a.mappoint.y
-                v['datetime_gmt'] = (a.startdate - timedelta(minutes=self.args.subtractMinutes)).isoformat()
+                v['datetime_gmt'] = (a.startdate - timedelta(minutes=self.args.add_minutes)).isoformat()
+                v['depth'] = self._pumping_depth()
                 new_hash[a_name] = v
             except Activity.DoesNotExist as e:
                 self.logger.warn('Activity matching "%s" does not exist in database %s', a_name, self.args.database)
@@ -94,7 +121,7 @@ class PlanktonPump():
                 self.logger.warn('Creating a load record for the first one, but make sure that this is what you want!')
                 v['longitude'] = acts[0].mappoint.x
                 v['latitude'] = acts[0].mappoint.y
-                v['datetime_gmt'] = (acts[0].startdate - timedelta(minutes=self.args.subtractMinutes)).isoformat()
+                v['datetime_gmt'] = (acts[0].startdate - timedelta(minutes=self.args.add_minutes)).isoformat()
                 new_hash[a_name] = v
                 continue
 
@@ -103,19 +130,12 @@ class PlanktonPump():
     def make_csv(self):
         '''Construct and write parent Sample csv file
         '''
-        try:
-            s = self._collect_samples(self.args.subsampleFile)
-        except AssertionError as e:
-            # TODO: Apply logic to allow multiple net tows at a CTD cast station
-            self.logger.error('Sample metadata differs for net tow within a CTD cast.')
-            self.logger.error('Are there more than one net tows per cast or is this an error in file %s?', self.args.subsampleFile)
-            self.logger.exception(e)
-            sys.exit(-2)
+        s = self._collect_samples(self.args.subsampleFile)
 
         # Get spatial-temporal and campaign information from the database
         s = self._db_join(s)
 
-        with open(self.args.csvFile, 'w') as f:
+        with open(self.args.csv_file, 'w') as f:
             f.write('Cast,')
             f.write(','.join(s.itervalues().next().keys()))
             f.write('\n')
@@ -124,7 +144,7 @@ class PlanktonPump():
                 f.write(','.join([str(dv) for dv in v.values()]))
                 f.write('\n')
 
-    def _get_net_tow_platform(self, cast_platform):
+    def _get_plankton_pump_platform(self, cast_platform):
         '''Use name of profile CTD platform to construct a new Platform for connecting to a PlanktonPump Activity.
         Return existing Platform if it's already been created.
         '''
@@ -142,14 +162,9 @@ class PlanktonPump():
     def _create_activity_instantpoint_platform(self, r, duration_minutes, nettow_number, point):
         '''Create an Activity and an InstantPoint from which to hang the Sample. Return the InstantPoint.
         '''
-        if r.get('sampletype').lower().find('vertical') != -1:
-            mindepth = 0
-        else:
-            mindepth = r.get('depth')
-
         campaign = Campaign.objects.using(self.args.database).filter(activity__name__contains=r.get('Cast'))[0]
         cast_plt = Platform.objects.using(self.args.database).filter(activity__name__contains=r.get('Cast'))[0]
-        platform = self._get_net_tow_platform(cast_plt)
+        platform = self._get_plankton_pump_platform(cast_plt)
         at, created = ActivityType.objects.using(self.args.database).get_or_create(name=VERTICALNETTOW)
 
         timevalue = datetime.strptime(r.get('datetime_gmt'), '%Y-%m-%dT%H:%M:%S')
@@ -161,8 +176,6 @@ class PlanktonPump():
                             platform = platform,
                             startdate = timevalue,
                             enddate = timevalue + timedelta(minutes=duration_minutes),
-                            mindepth = mindepth,
-                            maxdepth = r.get('depth'),
                        )
         # Update loaded_date after get_or_create() so that we can get the old record if script is re-executed
         act.loaded_date = datetime.utcnow()
@@ -235,7 +248,7 @@ class PlanktonPump():
         examples += "  Step 1 - Create .csv file of parent Sample information:\n"
         examples += "    " + sys.argv[0] + " --database stoqs_simz_aug2013_t"
         examples += " --subsampleFile 2013_SIMZ_TowNets_STOQS.csv"
-        examples += " --csvFile 2013_SIMZ_TowNet_ParentSamples.csv\n"
+        examples += " --csv_file 2013_SIMZ_TowNet_ParentSamples.csv\n"
         examples += "\n"
         examples += "  Step 2 - Load parent Sample information:\n"
         examples += "    " + sys.argv[0] + " --database stoqs_simz_aug2013_t"
@@ -248,24 +261,33 @@ class PlanktonPump():
                                              
         parser.add_argument('-d', '--database', action='store', help='Database alias', required=True)
 
-        parser.add_argument('-s', '--subsampleFile', action='store', help='File name containing analysis data from net tows in STOQS subsample format')
-        parser.add_argument('-c', '--csvFile', action='store', help='Output comma separated value file containing parent Sample data')
-        parser.add_argument('-m', '--subtractMinutes', action='store', help='Subtract these number of minutes from start of CTD cast for net tow time', 
-                                    type=int, default=30)
-        parser.add_argument('--laboratory', action='store', help='Laboratory responsible for the Samples')
-        parser.add_argument('--researcher', action='store', help='Researcher responsible for the Samples')
-        parser.add_argument('--purpose', action='store', help='Purpose of the Sample')
+        parser.add_argument('--subsampleFile', action='store', 
+                            help='File name containing analysis data from net tows in STOQS subsample format')
+        parser.add_argument('--csv_file', action='store', 
+                            help='Output comma separated value file containing parent Sample data')
+        parser.add_argument('--add_minutes', action='store', type=int, default=1,
+                            help='Add these number of minutes to last Niskin bottle trip in cluster')
+        parser.add_argument('--duration', action='store', type=int, default=10,
+                            help='Duration in minutes of the pumping')
+        parser.add_argument('--laboratory', action='store', 
+                            help='Laboratory responsible for the Samples')
+        parser.add_argument('--researcher', action='store', 
+                            help='Researcher responsible for the Samples')
+        parser.add_argument('--purpose', action='store', 
+                            help='Purpose of the Sample')
 
-        parser.add_argument('-l', '--loadFile', action='store', help='Load parent Sample data into database')
+        parser.add_argument('-l', '--loadFile', action='store', 
+                            help='Load parent Sample data into database')
 
-        parser.add_argument('-v', '--verbose', nargs='?', choices=[1,2,3], type=int, help='Turn on verbose output. Higher number = more output.', const=1)
+        parser.add_argument('-v', '--verbose', nargs='?', choices=[1,2,3], type=int, 
+                            help='Turn on verbose output. Higher number = more output.', const=1)
     
         self.args = parser.parse_args()
         self.commandline = ' '.join(sys.argv)
 
         if self.args.subsampleFile:
-            if not self.args.csvFile:
-                parser.error('Must include --csvFile argument with --subsampleFile option')
+            if not self.args.csv_file:
+                parser.error('Must include --csv_file argument with --subsampleFile option')
                 
         elif self.args.loadFile:
             pass
@@ -283,7 +305,7 @@ if __name__ == '__main__':
     pp = PlanktonPump()
     pp.process_command_line()
 
-    if pp.args.subsampleFile and pp.args.csvFile:
+    if pp.args.subsampleFile and pp.args.csv_file:
         pp.make_csv()
 
     elif pp.args.loadFile:
