bd3ec727490da5519a3fffb271e7e8ab984332a3
Mike McCann <MBARIMike@gmail.com>
2015-12-08 16:21:58 -0800

    Go back to Django 1.8.7 as this SITE_ID thing is a show-stopper for now

--- stoqs/config/settings/common.py
+++ stoqs/config/settings/common.py
@@ -25,7 +25,7 @@ DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    ##'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
