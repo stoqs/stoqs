a6960201933e3ecf938c5d96957e412e834acc05
Mike McCann <MBARIMike@gmail.com>
2016-03-02 11:39:30 -0800

    Add 'STOQS' test to the title bar, per Nancy's suggestion

---
+++ stoqs/stoqs/views/parameterinfo.py
@@ -0,0 +1,24 @@
+'''
+A views designed to provide information on a Parameter from the UI
+Supports both Sampled Parameter and Measured Parameter.  Provide
+linkage to vocabulary URIs if they exist...
+'''
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.http import HttpResponse
+from stoqs.models import Parameter, ParameterResource
+import logging 
+import simplejson as json
+import threading
+
+_thread_local_vars = threading.local()
+logger=logging.getLogger(__name__)
+
+def parameterinfo(request, pid):
+    logger.info('Got request %r for Parameter with ID of %s', request, pid)
+    db_alias = request.META.get('dbAlias')
+    p = Parameter.objects.using(db_alias).get(pk=pid)
+    pr = ParameterResource.objects.using(db_alias).filter(parameter=p).values('resource__name', 'resource__value')
+    response = HttpResponse(pr, content_type="application/json")
+    return response
