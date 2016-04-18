593ce75e16d487cfb483f7b4f6e8ae0a8d538fca
Mike McCann <MBARIMike@gmail.com>
2016-04-18 16:54:22 -0700

    Added _wait_until_visible_then_click() and test_share_view(). Making sure we have test coverage for the 'Share this view' function.

--- stoqs/stoqs/tests/functional_tests.py
+++ stoqs/stoqs/tests/functional_tests.py
@@ -24,7 +24,9 @@ from django.test import TestCase
 from django.conf import settings
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import selenium.webdriver.support.ui as ui
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 import logging
 
@@ -35,6 +37,10 @@ class BrowserTestCase(TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.set_window_size(1200, 768)
+        self.browser.set_window_position(300, 0)
+        ##dj_toolbar = self.browser.find_element_by_id('djHideToolBarButton')
+        ##self._wait_until_visible_then_click(dj_toolbar)
 
     def tearDown(self):
         self.browser.quit()
@@ -42,7 +48,7 @@ class BrowserTestCase(TestCase):
     def _mapserver_loading_panel_test(self):
         '''Wait for ajax-loader GIF image to go away'''
         seconds = 2
-        wait = ui.WebDriverWait(self.browser, seconds)
+        wait = WebDriverWait(self.browser, seconds)
         try:
             wait.until(lambda display: self.browser.find_element_by_id('map').
                         find_element_by_class_name('olControlLoadingPanel').
@@ -52,6 +58,15 @@ class BrowserTestCase(TestCase):
                     str(seconds) + ' seconds')
         else:
             return ''
+
+    def _wait_until_visible_then_click(self, element, scroll_up=True):
+        # See: http://stackoverflow.com/questions/23857145/selenium-python-element-not-clickable
+        element = WebDriverWait(self.browser, 5, poll_frequency=.2).until(
+                        EC.visibility_of(element))
+        if scroll_up:
+            self.browser.execute_script("window.scrollTo(0, 0)")
+
+        element.click()
 
     def test_campaign_page(self):
         self.browser.get('http://localhost:8000/')
@@ -68,7 +83,7 @@ class BrowserTestCase(TestCase):
             # Click on Platforms to expand
             platforms_anchor = self.browser.find_element_by_id(
                                     'platforms-anchor')
-            platforms_anchor.click()
+            self._wait_until_visible_then_click(platforms_anchor)
         except NoSuchElementException as e:
             print e
             print "Is the development server running?"
@@ -77,18 +92,31 @@ class BrowserTestCase(TestCase):
         # Finds <tr> for 'dorado' then gets the button for clicking
         dorado_button = self.browser.find_element_by_id('dorado'
                             ).find_element_by_tag_name('button')
-        dorado_button.click()
+        self._wait_until_visible_then_click(dorado_button)
 
         # Test that Mapserver returns images
         self.assertEquals('', self._mapserver_loading_panel_test())
 
         # Test Spatial 3D
-        self.browser.implicitly_wait(10)
         spatial_3d_anchor = self.browser.find_element_by_id('spatial-3d-anchor')
-        spatial_3d_anchor.click()
+        self._wait_until_visible_then_click(spatial_3d_anchor)
         showplatforms = self.browser.find_element_by_id('showplatforms')
-        showplatforms.click()
+        self._wait_until_visible_then_click(showplatforms)
         
         dl = self.browser.find_element_by_id('dorado_LOCATION')
         assert dl.tag_name == 'geolocation'
 
+    def test_share_view(self):
+        self.test_dorado_trajectory()
+        share_view = self.browser.find_element_by_id('permalink')
+        share_view.click()
+        self.browser.implicitly_wait(10)
+        permalink = self.browser.find_element_by_id('permalink-box').find_element_by_name('permalink')
+        self._wait_until_visible_then_click(permalink)
+        permalink_url = permalink.get_attribute('value')
+
+        # Restart browser and load permalink
+        self.tearDown()
+        self.setUp()
+        self.browser.get(permalink_url)
+        self.assertEquals('', self._mapserver_loading_panel_test())
