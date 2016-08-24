b2400049780e6873c7c99ee84cb6a291f0e710dd
Mike McCann <MBARIMike@gmail.com>
2016-08-24 10:02:40 -0700

    Add 'cm' to the summary request for the colormap_choice

--- stoqs/stoqs/views/query.py
+++ stoqs/stoqs/views/query.py
@@ -72,6 +72,7 @@ query_parms = {
                    'showgeox3dsample': 'showgeox3dsample',                                 # Flag value from checkbox
                    'showplatforms': 'showplatforms',                    # Flag value from checkbox
                    'showdataas': 'showdataas',              # Value from radio button, either 'contour' or 'scatter'
+                   'cm': 'cm',                              # Value from colormap picker
 
                    'only': 'only',                          # List of options to update - when only a partial response is needed
                    'except': 'except',                      # List of options not to update - when all but listed items are needed
