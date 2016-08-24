16e1beac4278ffaa8f619a4426d0a9886ba3895e
Mike McCann <MBARIMike@gmail.com>
2016-08-24 09:25:48 -0700

    Use ROOT_DIR to get static dir, build jetplus image, print progress

--- stoqs/tools/colormaps.py
+++ stoqs/tools/colormaps.py
@@ -1,21 +1,27 @@
+#!/bin/env python
 '''
-A simple view designed to generate images of colormaps to choose from.
+Generate images of colormaps to populate static/images/colormaps.
 '''
+
+import os
+import sys
+
+# Insert Django App directory (parent of config) into python path 
+sys.path.insert(0, os.path.abspath(os.path.join(
+                            os.path.dirname(__file__), "../")))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.local'
+# django >=1.7
+try:
+    import django
+    django.setup()
+except AttributeError:
+    pass
 
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist, SuspiciousOperation
-from django.http import Http404
-from django.http import HttpResponse
-import logging 
-from django.views.decorators.csrf import csrf_exempt
-from django.core.urlresolvers import reverse
-
+from utils.Viz.KML import readCLT
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import numpy as np
-import os
-import threading
-
-logger=logging.getLogger(__name__)
 
 cmaps = [('Uniform',
                             ['viridis', 'inferno', 'plasma', 'magma']),
@@ -34,7 +40,7 @@ cmaps = [('Uniform',
          ('Miscellaneous',  ['gist_earth', 'terrain', 'ocean', 'gist_stern',
                              'brg', 'CMRmap', 'cubehelix',
                              'gnuplot', 'gnuplot2', 'gist_ncar',
-                             'nipy_spectral', 'jet', 'rainbow',
+                             'nipy_spectral', 'jet', 'jetplus', 'rainbow',
                              'gist_rainbow', 'hsv', 'flag', 'prism'])]
 
 def _plot_color_bar(cmap):
@@ -43,20 +49,31 @@ def _plot_color_bar(cmap):
     gradient = np.linspace(0, 1, 256)
     gradient = np.vstack((gradient, gradient))
 
-    cb_fig = plt.figure(figsize=(2.56, 0.2))
+    cb_fig = plt.figure(figsize=(2.56, 0.15))
     cb_ax = cb_fig.add_axes([0., 0., 1., 1.])
-    cb_ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(cmap))
+    if cmap == 'jetplus':
+        clt = readCLT(os.path.join(str(settings.ROOT_DIR.path('static')), 'colormaps', 'jetplus.txt'))
+        cm_jetplus = colors.ListedColormap(np.array(clt))
+        cb_ax.imshow(gradient, aspect='auto', cmap=cm_jetplus)
+    else:
+        cb_ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(cmap))
+
     cb_ax.set_axis_off()
-    file_name = os.path.join(settings.STATIC_ROOT, 'images', 'colormaps', cmap)
+    file_name = os.path.join(str(settings.ROOT_DIR.path('static')), 'images', 'colormaps', cmap)
     cb_fig.savefig(file_name, dpi=100)
     plt.close()
 
-def generate_colormaps(request):
+def generate_colormaps():
     '''Build images as in http://matplotlib.org/examples/color/colormaps_reference.html
     '''
+    print('Making colormap images:')
     for cmap_category, cmap_list in cmaps:
+        print('\t{}:'.format(cmap_category))
         for cmap in cmap_list:
+            print('\t\t{}'.format(cmap))
             _plot_color_bar(cmap)
 
         
-    
+if __name__ == '__main__':
+    generate_colormaps()
+
