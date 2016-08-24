742e0c2a92d65f96e12c2a342d36e78b94953dc7
Mike McCann <MBARIMike@gmail.com>
2016-08-23 23:15:04 -0700

    Make image for each colormap in static/images/colormaps

--- stoqs/stoqs/views/colormaps.py
+++ stoqs/stoqs/views/colormaps.py
@@ -17,13 +17,13 @@ import threading
 
 logger=logging.getLogger(__name__)
 
-cmaps = [('Perceptually_Uniform_Sequential',
+cmaps = [('Uniform',
                             ['viridis', 'inferno', 'plasma', 'magma']),
-         ('Sequential_1',     ['Blues', 'BuGn', 'BuPu',
+         ('Sequential',     ['Blues', 'BuGn', 'BuPu',
                              'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
                              'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
-                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
-         ('Sequential_2', ['afmhot', 'autumn', 'bone', 'cool',
+                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd',
+                             'afmhot', 'autumn', 'bone', 'cool',
                              'copper', 'gist_heat', 'gray', 'hot',
                              'pink', 'spring', 'summer', 'winter']),
          ('Diverging',      ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
@@ -37,43 +37,26 @@ cmaps = [('Perceptually_Uniform_Sequential',
                              'nipy_spectral', 'jet', 'rainbow',
                              'gist_rainbow', 'hsv', 'flag', 'prism'])]
 
-nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps)
-gradient = np.linspace(0, 1, 256)
-gradient = np.vstack((gradient, gradient))
+def _plot_color_bar(cmap):
+    '''Make an image file for each colormap
+    '''
+    gradient = np.linspace(0, 1, 256)
+    gradient = np.vstack((gradient, gradient))
 
-def _plot_color_gradients(cmap_category, cmap_list):
-    fig, axes = plt.subplots(nrows=nrows)
-    fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
-    axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
-
-    for ax, name in zip(axes, cmap_list):
-        ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
-        pos = list(ax.get_position().bounds)
-        x_text = pos[0] - 0.01
-        y_text = pos[1] + pos[3]/2.
-        fig.text(x_text, y_text, name, va='center', ha='right', fontsize=10)
-
-    # Turn off *all* ticks & spines, not just the ones with colormaps.
-    for ax in axes:
-        ax.set_axis_off()
-
-    fn = os.path.join(settings.MEDIA_ROOT, 'sections', cmap_category)
-    fig.savefig(fn, dpi=120)
+    cb_fig = plt.figure(figsize=(2.56, 0.2))
+    cb_ax = cb_fig.add_axes([0., 0., 1., 1.])
+    cb_ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(cmap))
+    cb_ax.set_axis_off()
+    file_name = os.path.join(settings.STATIC_ROOT, 'images', 'colormaps', cmap)
+    cb_fig.savefig(file_name, dpi=100)
+    plt.close()
 
 def generate_colormaps(request):
     '''Build images as in http://matplotlib.org/examples/color/colormaps_reference.html
     '''
     for cmap_category, cmap_list in cmaps:
-        _plot_color_gradients(cmap_category, cmap_list)
-        fn = os.path.join(settings.MEDIA_ROOT, 'sections', cmap_category) + '.png'
-        with open(fn, 'rb') as f:
-            response = HttpResponse(f.read(), content_type="image/png")
-            return response
+        for cmap in cmap_list:
+            _plot_color_bar(cmap)
 
-
-    ##except Exception:
-    ##    logger.exception('Doh!')
-    ##    raise SuspiciousOperation('Attempt to create permalink without any data, or with invalid data')
         
-    return response
     
