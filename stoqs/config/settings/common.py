3d2afaefcf2aeb10bbfb9f49d5dbe0bbe3b7d2e7
Mike McCann <MBARIMike@gmail.com>
2017-02-13 21:53:58 -0800

    Use default keyword

--- stoqs/config/settings/common.py
+++ stoqs/config/settings/common.py
@@ -81,7 +81,7 @@ MIGRATION_MODULES = {
 # DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 # FIXTURE CONFIGURATION
 # ------------------------------------------------------------------------------
