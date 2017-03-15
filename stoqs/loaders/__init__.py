009ad57732d12dba41b1d792b58a2d7519b138c2
Mike McCann <MBARIMike@gmail.com>
2017-03-15 19:05:52 +0000

    Fixes made by 2to3, mainly making list()s of .keys() and .items()

--- stoqs/loaders/__init__.py
+++ stoqs/loaders/__init__.py
@@ -223,9 +223,9 @@ class LoadScript(object):
         self.logger.debug('Looking in database %s for Campaign name = %s', self.dbAlias, self.campaignName)
         campaign = m.Campaign.objects.using(self.dbAlias).get(name=self.campaignName)
         
-        for url, viewpoint in self.x3dTerrains.items():
+        for url, viewpoint in list(self.x3dTerrains.items()):
             self.logger.debug('url = %s, viewpoint = %s', url, viewpoint)
-            for name, value in viewpoint.items():
+            for name, value in list(viewpoint.items()):
                 resource, _ = m.Resource.objects.using(self.dbAlias).get_or_create(
                               uristring=url, name=name, value=value, resourcetype=resourceType)
                 m.CampaignResource.objects.using(self.dbAlias).get_or_create(
@@ -442,7 +442,7 @@ class STOQS_Loader(object):
             '''
 
             # Go through the keys of the OPeNDAP URL for the dataset and add the parameters as needed to the database
-            for key in parmDict.keys():
+            for key in list(parmDict.keys()):
                 self.logger.debug("key = %s", key)
                 if (key in self.ignored_names) or (key not in self.include_names): # skip adding parameters that are ignored
                     continue
@@ -489,14 +489,14 @@ class STOQS_Loader(object):
                             self.logger.error('Exception %s', e)
                             raise Exception('''Failed to add parameter for %s
                                 %s\nEither add parameter manually, or add to ignored_names''' % (key,
-                                '\n'.join(['%s=%s' % (k1,v1) for k1,v1 in parms.items()])))
+                                '\n'.join(['%s=%s' % (k1,v1) for k1,v1 in list(parms.items())])))
                         
                     except Exception as e:
                         self.logger.error('%s', e)
                         transaction.savepoint_rollback(sid,using=self.dbAlias)
                         raise Exception('''Failed to add parameter for %s
                             %s\nEither add parameter manually, or add to ignored_names''' % (key,
-                            '\n'.join(['%s=%s' % (k1,v1) for k1,v1 in parms.items()])))
+                            '\n'.join(['%s=%s' % (k1,v1) for k1,v1 in list(parms.items())])))
                     self.logger.debug("Added parameter %s from data set to database %s", key, self.dbAlias)
 
         return innerAddParameters(self, parmDict)
@@ -570,10 +570,10 @@ class STOQS_Loader(object):
 
         # The NC_GLOBAL attributes from the OPeNDAP URL.  Save them all.
         self.logger.debug("Getting or Creating ResourceType nc_global...")
-        self.logger.debug("ds.attributes.keys() = %s", self.ds.attributes.keys() )
+        self.logger.debug("ds.attributes.keys() = %s", list(self.ds.attributes.keys()) )
         if 'NC_GLOBAL' in self.ds.attributes:
             resourceType, _ = m.ResourceType.objects.using(self.dbAlias).get_or_create(name = 'nc_global')
-            for rn, value in self.ds.attributes['NC_GLOBAL'].items():
+            for rn, value in list(self.ds.attributes['NC_GLOBAL'].items()):
                 self.logger.debug("Getting or Creating Resource with name = %s, value = %s", rn, value )
                 resource, _ = m.Resource.objects.using(self.dbAlias).get_or_create(
                             name=rn, value=value, resourcetype=resourceType)
@@ -604,7 +604,7 @@ class STOQS_Loader(object):
         for v in self.include_names + ['altitude']:
             self.logger.info('v = %s', v)
             try:
-                for rn, value in self.ds[v].attributes.items():
+                for rn, value in list(self.ds[v].attributes.items()):
                     self.logger.debug("Getting or Creating Resource with name = %s, value = %s", rn, value )
                     resource, _ = m.Resource.objects.using(self.dbAlias).get_or_create(
                                           name=rn, value=value, resourcetype=resourceType)
@@ -797,14 +797,14 @@ class STOQS_Loader(object):
                 if not allNaNFlag[v]:
                     anyValidData = True
             except KeyError:
-                self.logger.debug('Parameter %s not in %s. Skipping.', v, self.ds.keys())
+                self.logger.debug('Parameter %s not in %s. Skipping.', v, list(self.ds.keys()))
                 if v.find('.') != -1:
                     raise Exception('Parameter names must not contain periods - cannot load data. Paramater %s violates CF conventions.' % v)
             except ValueError:
                 pass
 
         self.logger.debug("allNaNFlag = %s", allNaNFlag)
-        for v in allNaNFlag.keys():
+        for v in list(allNaNFlag.keys()):
             if not allNaNFlag[v]:
                 self.varsLoaded.append(v)
         self.logger.info("Variables that have data: self.varsLoaded = %s", self.varsLoaded)
@@ -822,7 +822,7 @@ class STOQS_Loader(object):
             # Loaders (such as those in SampleLoaders.py) may inherit this method and not use OPeNDAP
             return
         try:
-            for var in self.ds.keys():
+            for var in list(self.ds.keys()):
                 if var in self.standard_names: 
                     continue # don't override pre-specified names
                 if 'standard_name' in self.ds[var].attributes:
