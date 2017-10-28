03d13b2f506824ff88ff976910886b8983385c60
Mike McCann <MBARIMike@gmail.com>
2017-10-28 04:45:09 +0000

    Test fails appropriately on missing Hdg_1215 data

--- stoqs/stoqs/tests/loading_tests.py
+++ stoqs/stoqs/tests/loading_tests.py
@@ -11,39 +11,45 @@ MBARI 24 October 2017
 
 import os
 import sys
+parent_dir = os.path.join(os.path.dirname(__file__), "../../loaders")
+sys.path.insert(0, parent_dir)  # So that CCE is found
+
 import time
 import json
 import time
 import logging
 
+from datetime import timedelta
 from django.conf import settings
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from stoqs.models import Activity, Parameter, Resource
+from stoqs.models import MeasuredParameter
+from CCE.loadCCE_2015 import lores_event_times
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('stoqs.tests')
+settings.LOGGING['loggers']['stoqs.tests']['level'] = 'DEBUG'
 
-        
 class MeasuredParameterTestCase(TestCase):
     fixtures = ['stoqs_load_test.json']
     multi_db = False
 
-    # Many of these tests use a shortcut of building a qstring from the AJAX 
-    # request. 
-    
-
     def test_epic_timeseries1(self):
-        base = reverse('stoqs:show-measuredparmeter', kwargs={'dbAlias': 'stoqs_load_test', 'fmt': '.json'})
 
-        # TODO: use a time value from the stoqs/stoqs/tests/load_data.py script
-        qstring = ('parameter__name=D_3&measurement__instantpoint__timevalue__gt=2016-03-08T00:00:00')
+        # Expected number of records for all the timeseries Parameters from 
+        # the MS moorings
+        parm_counts = dict(D_3=29, Hdg_1215=29, NEP_56=2, P_1=29, Pitch_1216=29, 
+                           Roll_1217=29, S_41=29, T_28=29, Trb_980=2)
 
-        req = base + '?' + qstring
-        response = self.client.get(req)
-        import pdb; pdb.set_trace()
-        self.assertEqual(response.status_code, 200, 'Status code should be 200 for %s' % req)
-        data = json.loads(response.content) # Verify we don't get an exception when we load the data.
+        # Use last day of the 2nd event from CCE.loadCCE_2015 - March 2016
+        one_day_from_end = lores_event_times[1][1] - timedelta(days=1)
+        logger.debug(f'one_day_from_end = {one_day_from_end}')
 
-        # Test that we have the correct number of rows
+        for parm in list(parm_counts.keys()):
+            # one_day_from_end = 2016-03-07T00:00:00
+            mp_count = MeasuredParameter.objects.filter(parameter__name=parm,
+                            measurement__instantpoint__timevalue__gt=one_day_from_end).count()
+            logger.debug(f'{parm:10s}({parm_counts[parm]:2d}) {mp_count:-6d}')
+            self.assertNotEquals(mp_count, 0, f'Expected {parm_counts[parm]} values for {parm}')
+
 
 
