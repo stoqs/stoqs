9b5f365f93e7ff9f26d1b7b774c57b0ba7aff8d5
Mike McCann <MBARIMike@gmail.com>
2018-03-29 14:27:49 -0700

    Set POSTGRES_DB=postgres for inital postgis service, copy CAMPAIGNS_MODULE if set

--- docker/stoqs-start.sh
+++ docker/stoqs-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Ensure that stoqs-postgis container is serving databases before continuing
-python ${STOQS_SRVHOME}/database-check.py > /dev/null 2>&1
+POSTGRES_DB=postgres python ${STOQS_SRVHOME}/database-check.py > /dev/null 2>&1
 while [[ $? != 0 ]] ; do
     sleep 5; echo "*** Waiting for postgres container ..."
-    python ${STOQS_SRVHOME}/database-check.py > /dev/null 2>&1
+    POSTGRES_DB=postgres python ${STOQS_SRVHOME}/database-check.py > /dev/null 2>&1
 done
 
 # Allow for psql execution (used for database creation) without a password
@@ -14,12 +14,10 @@ echo ${PGHOST}:\*:\*:postgres:${POSTGRES_PASSWORD} > /root/.pgpass &&\
 # Ensure that shared volume for Mapserver map files is readable by nginx's uwsgi
 chmod 777 /dev/shm
 
-##export DATABASE_URL=postgis://postgres:changeme@stoqs-postgis:5432/stoqs
-##DATABASE_URL=postgis://stoqsadm:CHANGEME@stoqs-postgis:5432/stoqs
 export PYTHONPATH="${STOQS_SRVPROJ}:${PYTHONPATH}"
 
 # Monterey Bay bathymetry data is needed for default (and many other) database loads
-if [ ! -e ${STOQS_SRVPROJ}/loaders/Monterey25.grd ]; then
+if [[ ! -e ${STOQS_SRVPROJ}/loaders/Monterey25.grd ]]; then
     echo "Getting Monterey25.grd..."
     wget -q -N -O ${STOQS_SRVPROJ}/loaders/Monterey25.grd http://stoqs.mbari.org/terrain/Monterey25.grd
 fi
@@ -48,29 +46,17 @@ if [[ $? != 0 ]]; then
     ./test.sh changeme
 fi
 
+if [[ ! -z $CAMPAIGNS_MODULE ]]; then
+    echo "Copying ${STOQS_SRVHOME}/$CAMPAIGNS_MODULE to ${STOQS_SRVPROJ}/campaigns.py..."
+    cp ${STOQS_SRVHOME}/$CAMPAIGNS_MODULE ${STOQS_SRVPROJ}/campaigns.py
+fi
 
 # Fire up stoqs web app
 if [ "$PRODUCTION" == "false" ]; then
-    echo "Starting development server..."
+    echo "Starting development server with DATABASE_URL=${DATABASE_URL}..."
     ${STOQS_SRVPROJ}/manage.py runserver 0.0.0.0:8000 --settings=config.settings.local
 else
-    echo "Starting production server..."
-
+    echo "Starting production server with DATABASE_URL=${DATABASE_URL}..."
     python stoqs/manage.py collectstatic --noinput  # Collect static files
-
-    # Taken from start_uwsgi.sh... Start the stoqs uWSGI application for nginx
-    # TODO: move env variables to .env
-
-    # These are set in docker-compose.yml...
-    export STOQS_HOME=${STOQS_SRVHOME}
-    ##export STATIC_ROOT=/usr/share/nginx/html/static
-    ##export MEDIA_ROOT=/usr/share/nginx/html/media
-    ##export DATABASE_URL="postgis://<dbuser>:<pw>@<host>:<port>/stoqs"
-    ##export MAPSERVER_HOST="<mapserver_ip_address>"
-    ##export STOQS_CAMPAIGNS="<comma_separated>,<databases>,<not_in_campaigns>"
-    ##export SECRET_KEY="<random_sequence_of_impossible_to_guess_characters>"
-    ##export GDAL_DATA=/usr/share/gdal
-
-    # Connect with nginx
     /usr/local/bin/uwsgi --emperor /etc/uwsgi/django-uwsgi.ini
 fi
