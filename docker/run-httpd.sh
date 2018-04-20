309fb8ae140d73ced4814c14c852b1c7424328fb
Mike McCann <MBARIMike@gmail.com>
2018-04-20 21:35:32 +0000

    Not needed now that Dockerfile-mapserver is gone

--- docker/run-httpd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO rather simplistic at the moment.
-# Possible reference: https://github.com/sclorg/httpd-container/blob/master/2.4/root/usr/bin/run-httpd
-
-set -eu
-
-exec httpd -D FOREGROUND $@
