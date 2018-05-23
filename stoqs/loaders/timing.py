d6c58b0480c4a8377dfe4851f6fda0e6806d2f9a
Mike McCann <MBARIMike@gmail.com>
2018-05-23 01:35:14 +0000

    Initial commit: timing module for all load scripts

---
+++ stoqs/loaders/timing.py
@@ -0,0 +1,29 @@
+# See: https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution/1557906#1557906
+# python3
+import atexit
+from time import time, strftime, localtime
+from datetime import datetime, timedelta
+
+def secondsToStr(elapsed=None):
+    if elapsed is None:
+        return strftime("%Y-%m-%d %H:%M:%S", localtime())
+    else:
+        return str(timedelta(seconds=elapsed))
+
+def log(s, elapsed=None):
+    line = "="*40
+    print(line, flush=True)
+    print(secondsToStr(), '-', s, flush=True)
+    if elapsed:
+        print("Elapsed time:", secondsToStr(elapsed), flush=True)
+        print(f"Minutes: {elapsed / 60.0:.1f}", flush=True)
+    print(line, flush=True)
+
+def endlog():
+    end = datetime.now()
+    elapsed = end - start
+    log("End Load",elapsed.total_seconds())
+
+start = datetime.now()
+atexit.register(endlog)
+log("Start Load")
