b670b998d7137acbbbe3962acbd3b3e73994fe5b
Mike McCann <MBARIMike@gmail.com>
2018-06-02 02:43:01 +0000

    Import etree from lxml

--- stoqs/loaders/CANON/loadCANON_postSeason2017.py
+++ stoqs/loaders/CANON/loadCANON_postSeason2017.py
@@ -32,7 +32,7 @@ sys.path.insert(0, parentDir)  # So that CANON is found
 from CANON import CANONLoader
 from loaders import FileNotFound
 from thredds_crawler.crawl import Crawl
-from thredds_crawler.etree import etree
+from lxml import etree
 import timing
 
 cl = CANONLoader('stoqs_ps2017', 'post Season 2017',
