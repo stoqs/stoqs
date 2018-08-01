12d672de0accccea11a092036d615ecfa3e22708
Mike McCann <MBARIMike@gmail.com>
2018-08-01 16:35:32 +0000

    Use loadLRAUV(), fix construction of wf ctd files

--- stoqs/loaders/CANON/loadCANON_september2016.py
+++ stoqs/loaders/CANON/loadCANON_september2016.py
@@ -20,19 +20,11 @@ MBARI 5 January 2016
 import os
 import sys
 import datetime  # needed for glider data
-import time      # for startdate, enddate args
-import csv
-import urllib.request, urllib.error, urllib.parse
-import urllib.parse
-import requests
 
 parentDir = os.path.join(os.path.dirname(__file__), "../")
 sys.path.insert(0, parentDir)  # So that CANON is found
 
 from CANON import CANONLoader
-from loaders import FileNotFound
-from thredds_crawler.crawl import Crawl
-from lxml import etree
 import timing
 
 cl = CANONLoader('stoqs_canon_september2016', 'CANON - September 2016',
@@ -96,86 +88,14 @@ cl.dorado_parms = [ 'temperature', 'oxygen', 'nitrate', 'bbp420', 'bbp700',
 #####################################################################
 #  LRAUV - Avoid the Lake Michigan tethys missions in August
 #####################################################################
-lrauv_startdates = { 'tethys': datetime.datetime(2016, 8, 30),
-                     'makai': startdate }
-lrauv_enddates = { 'tethys': enddate,
-                   'makai': enddate }
-def find_urls(plat, base, search_str):
-    INV_NS = "http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0"
-    url = os.path.join(base, 'catalog.xml')
-    print(("Crawling: %s" % url))
-    skips = Crawl.SKIPS + [".*Courier*", ".*Express*", ".*Normal*, '.*Priority*", ".*.cfg$" ]
-    u = urllib.parse.urlsplit(url)
-    name, ext = os.path.splitext(u.path)
-    if ext == ".html":
-        u = urllib.parse.urlsplit(url.replace(".html", ".xml"))
-    url = u.geturl()
-    urls = []
-    # Get an etree object
-    try:
-        r = requests.get(url)
-        tree = etree.XML(r.text.encode('utf-8'))
-
-        # Crawl the catalogRefs:
-        for ref in tree.findall('.//{%s}catalogRef' % INV_NS):
-
-            try:
-                # get the mission directory name and extract the start and ending dates
-                mission_dir_name = ref.attrib['{http://www.w3.org/1999/xlink}title']
-                dts = mission_dir_name.split('_')
-                dir_start =  datetime.datetime.strptime(dts[0], '%Y%m%d')
-                dir_end =  datetime.datetime.strptime(dts[1], '%Y%m%d')
-
-                # if within a valid range, grab the valid urls
-                if dir_start >= lrauv_startdates[plat] and dir_end <= lrauv_enddates[plat]:
-                    catalog = ref.attrib['{http://www.w3.org/1999/xlink}href']
-                    c = Crawl(os.path.join(base, catalog), select=[search_str], skip=skips)
-                    d = [s.get("url") for d in c.datasets for s in d.services if s.get("service").lower() == "opendap"]
-                    for url in d:
-                        urls.append(url)
-            except Exception as ex:
-                print(("Error reading mission directory name %s" % ex))
-
-    except BaseException:
-        print(("Skipping %s (error parsing the XML)" % url))
-
-    if not urls:
-        raise FileNotFound('No urls matching "{}" found in {}'.format(search_str, os.path.join(base, 'catalog.html')))
-
-    return urls
 
 # Load netCDF files produced (binned, etc.) by Danelle Cline
 # These binned files are created with the makeLRAUVNetCDFs.sh script in the
 # toNetCDF directory. You must first edit and run that script once to produce
 # the binned files before this will work
 
-# Get directory list from thredds server
-platforms = ['tethys', 'makai']
+# Use the default parameters provided by loadLRAUV() calls below
 
-for p in platforms:
-    base =  'http://dods.mbari.org/thredds/catalog/LRAUV/' + p + '/missionlogs/2016/'
-    dods_base = 'http://dods.mbari.org/opendap/data/lrauv/' + p + '/missionlogs/2016/'
-    setattr(cl, p + '_files', [])
-    setattr(cl, p + '_base', dods_base)
-    setattr(cl, p + '_parms' , ['temperature', 'salinity', 'chlorophyll', 'nitrate', 'oxygen','bbp470', 'bbp650','PAR',
-                                'yaw', 'pitch', 'roll', 
-                                ])
-    try:
-        urls_eng = find_urls(p, base, '.*2S_eng.nc$')
-        urls_sci = find_urls(p, base, '.*10S_sci.nc$')
-        urls = urls_sci + urls_eng
-        files = []
-        if len(urls) > 0 :
-            for url in sorted(urls):
-                file = '/'.join(url.split('/')[-3:])
-                files.append(file)
-            setattr(cl, p + '_files', files)
-
-        setattr(cl, p  + '_startDatetime', startdate)
-        setattr(cl, p + '_endDatetime', enddate)
-
-    except FileNotFound:
-        continue
 
 ######################################################################
 #  GLIDERS
@@ -273,18 +193,16 @@ cl.wg_Tiny_endDatetime = enddate
 # UCTD
 cl.wfuctd_base = cl.dodsBase + 'CANON/2016_Sep/Platforms/Ships/Western_Flyer/uctd/'
 cl.wfuctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'wetstar' ]
-cl.wfuctd_files = [
-                    'canon16m{:02d}.nc'.format(i) for i in (list(range(1,15)),
-                    'wfiv16m{:02d}.nc'.format(i)) for i in range(1,9)
-                  ]
+cl.wfuctd_files = ([ 'canon16m{:02d}.nc'.format(i) for i in range(1,15) ] +
+                   [ 'wfiv16m{:02d}.nc'.format(i) for i in range(1,9) ]
+                  )
 
 # PCTD
 cl.wfpctd_base = cl.dodsBase + 'CANON/2016_Sep/Platforms/Ships/Western_Flyer/pctd/'
 cl.wfpctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'ecofl' , 'oxygen']
-cl.wfpctd_files = [
-                    'canon16c{:02d}.nc'.format(i) for i in (list(range(1,69)),
-                    'wfiv16c{:02d}.nc'.format(i)) for i in range(1,34)
-                  ]
+cl.wfpctd_files = ([ 'canon16c{:02d}.nc'.format(i) for i in range(1,69) ] +
+                   [ 'wfiv16c{:02d}.nc'.format(i) for i in range(1,34) ]
+                  )
 
 ######################################################################
 #  RACHEL CARSON: May 2015 -- 
@@ -433,9 +351,9 @@ if cl.args.test:
     cl.load_wg_Tiny(stride=10)
 
     cl.loadDorado(stride=10)
-    #cl.loadDaphne(stride=100)
-    cl.loadTethys(stride=10)
-    cl.loadMakai(stride=10)
+    #cl.loadLRAUV('daphne', startdate, enddate, stride=100)
+    cl.loadLRAUV('tethys', datetime.datetime(2016, 8, 30), enddate, stride=10)
+    cl.loadLRAUV('makai', startdate, enddate, stride=10)
 
     #cl.loadRCuctd(stride=10)
     #cl.loadRCpctd(stride=10)
@@ -484,9 +402,9 @@ else:
     cl.load_oa1()
     cl.load_oa2()
     cl.loadDorado()
-    #cl.loadDaphne()
-    #cl.loadTethys()
-    #cl.loadMakai()
+    cl.loadLRAUV('daphne', startdate, enddate)
+    cl.loadLRAUV('tethys', datetime.datetime(2016, 8, 30), enddate)
+    cl.loadLRAUV('makai', startdate, enddate)
     #cl.loadRCuctd()
     #cl.loadRCpctd() 
     cl.loadWFuctd()   
