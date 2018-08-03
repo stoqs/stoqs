34db12c33e2a5c69d31d33a9ce4a40dfc7e4eb7e
Mike McCann <MBARIMike@gmail.com>
2018-08-03 21:20:03 +0000

    Pull in WF CTD data files from loadsep11.py

--- stoqs/loaders/CANON/loadCANON_september2011.py
+++ stoqs/loaders/CANON/loadCANON_september2011.py
@@ -64,16 +64,32 @@ cl.l_662_endDatetime = datetime.datetime(2011, 9, 30)
 
 
 
-# western flyer underway ctd
-#added to test functionality
-cl.wfuctd_base = cl.dodsBase + 'CANON_september2012/wf/uctd/'
+######################################################################
+#  WESTERN FLYER
+######################################################################
+
+cl.wfuctd_base = cl.dodsBase + 'CANON_september2011/wf/uctd/'
 cl.wfuctd_files = [
-        'c0912m01.nc', 'c0912m02.nc', 'c0912m03.nc', 'c0912m04.nc', 'c0912m05.nc', 'c0912m06.nc',
-        'c0912m07.nc', 'c0912m08.nc', 'c0912m09.nc', 'c0912m10.nc', 'c0912m11.nc', 'c0912m12.nc',
-        'c0912m13.nc', 'c0912m14.nc', 'c0912m15.nc', 'c0912m16.nc', 'c0912m17.nc', 'c0912m18.nc',
-        'c0912m19.nc',
-                      ]
+                    '24211WF01.nc', '27211WF01.nc', '27411WF01.nc', '27511WF01.nc', '27711WF01.nc', 
+                    '27811WF01.nc', '27911wf01.nc', '28011wf01.nc', '28111wf01.nc', '28211wf01.nc',
+                  ]
 cl.wfuctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'wetstar' ]
+
+cl.pctdDir = 'CANON_september2011/wf/pctd/'
+cl.wfpctd_base = cl.dodsBase + cl.pctdDir
+cl.wfpctd_files = [
+    'canon11c01.nc', 'canon11c02.nc', 'canon11c03.nc', 'canon11c04.nc', 'canon11c05.nc', 'canon11c06.nc', 'canon11c07.nc',
+    'canon11c08.nc', 'canon11c09.nc', 'canon11c10.nc', 'canon11c11.nc', 'canon11c12.nc', 'canon11c13_A.nc', 'canon11c13_B.nc', 'canon11c14.nc',
+    'canon11c16.nc', 'canon11c17.nc', 'canon11c19_A.nc', 'canon11c20.nc', 'canon11c22.nc', 'canon11c23.nc', 'canon11c24.nc', 'canon11c25.nc',
+    'canon11c26.nc', 'canon11c27.nc', 'canon11c28.nc', 'canon11c29.nc', 'canon11c30.nc', 'canon11c31.nc', 'canon11c32.nc', 'canon11c33.nc',
+    'canon11c34.nc', 'canon11c35.nc', 'canon11c36.nc', 'canon11c37.nc', 'canon11c38.nc', 'canon11c39.nc', 'canon11c40.nc', 'canon11c41.nc',
+    'canon11c42.nc', 'canon11c43.nc', 'canon11c44.nc', 'canon11c45.nc', 'canon11c46.nc', 'canon11c48.nc', 'canon11c49.nc', 'canon11c50.nc',
+    'canon11c51.nc', 'canon11c52.nc', 'canon11c53.nc', 'canon11c54.nc', 'canon11c55.nc', 'canon11c56.nc', 'canon11c57.nc', 'canon11c58.nc',
+    'canon11c59.nc', 'canon11c60.nc', 'canon11c61.nc', 'canon11c62.nc', 'canon11c63.nc', 'canon11c64.nc', 'canon11c65.nc', 'canon11c66.nc',
+    'canon11c67.nc', 'canon11c68.nc', 'canon11c69.nc', 'canon11c70.nc', 'canon11c71.nc', 'canon11c72.nc', 'canon11c73.nc', 'canon11c74.nc',
+    'canon11c75.nc', 'canon11c76.nc', 'canon11c77.nc', 'canon11c78.nc', 'canon11c79.nc', 'canon11c80.nc', 'canon11c81.nc', 'canon11c82.nc' ]
+cl.wfpctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'ecofl' , 'oxygen']
+
 
 
 # Liquid Robotics Waveglider
