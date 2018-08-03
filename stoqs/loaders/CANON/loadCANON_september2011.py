3f8bc8da4a41107d8ddd67af7871caee9cbe66b7
Mike McCann <MBARIMike@gmail.com>
2018-08-03 22:29:49 +0000

    Structure like other loaders, tested Dorado & WF ctd

--- stoqs/loaders/CANON/loadCANON_september2011.py
+++ stoqs/loaders/CANON/loadCANON_september2011.py
@@ -6,7 +6,7 @@ __contact__   = 'duane at mbari.org'
 
 __doc__ = '''
 
-Master loader for all CANON activities in September 2011 and beyound
+Master loader for all CANON activities in September 2011
 
 Mike McCann and Duane Edgington and Reiko
 MBARI 15 August 2013
@@ -20,17 +20,29 @@ MBARI 15 August 2013
 import os
 import sys
 import datetime  # needed for glider data
-os.environ['DJANGO_SETTINGS_MODULE']='settings'
-project_dir = os.path.dirname(__file__)
 
-# the next line makes it possible to find CANON
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))  # settings.py is one dir up
+parentDir = os.path.join(os.path.dirname(__file__), "../")
+sys.path.insert(0, parentDir)  # So that CANON is found
 
 from CANON import CANONLoader
 import timing
 
-# building input data sources object
-cl = CANONLoader('stoqs_september2011', 'CANON - September 2011')
+cl = CANONLoader('stoqs_september2011', 'CANON - September 2011',
+                    description = 'CANON observing campaign in Monterey Bay',
+                    x3dTerrains = {
+                            'https://stoqs.mbari.org/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
+                                'position': '-2822317.31255 -4438600.53640 3786150.85474',
+                                'orientation': '0.89575 -0.31076 -0.31791 1.63772',
+                                'centerOfRotation': '-2711557.9403829873 -4331414.329506527 3801353.4691465236',
+                                'VerticalExaggeration': '10',
+                                'speed': '.1',
+                            }
+                    },
+                    grdTerrain = os.path.join(parentDir, 'Monterey25.grd')
+                  )
+
+startdate = datetime.datetime(2011, 9, 15)
+enddate = datetime.datetime(2011, 9, 30)
 
 # default location of thredds and dods data:
 cl.tdsBase = 'http://odss.mbari.org/thredds/'
@@ -48,8 +60,12 @@ cl.dorado_files = [  'Dorado389_2011_249_00_249_00_decim.nc',
                      'Dorado389_2011_264_00_264_00_decim.nc',
                      'Dorado389_2011_285_01_285_01_decim.nc',
                      'Dorado389_2011_286_00_286_00_decim.nc',
-                                                             ]
-
+                  ]
+cl.dorado_parms = [ 'temperature', 'oxygen', 'nitrate', 'bbp420', 'bbp700',
+                    'fl700_uncorr', 'salinity', 'biolume',
+                    'sepCountList', 'mepCountList',
+                    'roll', 'pitch', 'yaw',
+                  ]
 
 
 # special location for spray glider data
@@ -59,8 +75,8 @@ cl.l_662_files = ['OS_Glider_L_662_20110915_TS.nc',
                                                     ]
 
 cl.l_662_parms = ['TEMP', 'PSAL', 'FLU2']
-cl.l_662_startDatetime = datetime.datetime(2011, 9, 15)
-cl.l_662_endDatetime = datetime.datetime(2011, 9, 30)
+cl.l_662_startDatetime = startdate
+cl.l_662_endDatetime = enddate
 
 
 
@@ -70,7 +86,7 @@ cl.l_662_endDatetime = datetime.datetime(2011, 9, 30)
 
 cl.wfuctd_base = cl.dodsBase + 'CANON_september2011/wf/uctd/'
 cl.wfuctd_files = [
-                    '24211WF01.nc', '27211WF01.nc', '27411WF01.nc', '27511WF01.nc', '27711WF01.nc', 
+                    '27211WF01.nc', '27411WF01.nc', '27511WF01.nc', '27711WF01.nc', 
                     '27811WF01.nc', '27911wf01.nc', '28011wf01.nc', '28111wf01.nc', '28211wf01.nc',
                   ]
 cl.wfuctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'wetstar' ]
@@ -100,35 +116,21 @@ cl.waveglider_parms = [ 'TEMP', 'PSAL', 'oxygen' ]
 cl.waveglider_startDatetime = datetime.datetime(2012, 8, 31, 18, 47, 0)
 cl.waveglider_endDatetime = datetime.datetime(2012, 9, 25, 16, 0, 0)
 
-# Western Flyer Profile CTD
-# added to test functionality
-cl.pctdDir = 'CANON_september2012/wf/pctd/'
-cl.wfpctd_base = cl.dodsBase + cl.pctdDir
-cl.wfpctd_files = [
-'c0912c01up.nc',
-'c0912c01.nc',
-'c0912c02.nc',
-'c0912c03.nc', 'c0912c04.nc', 'c0912c05.nc', 'c0912c06.nc',
-'c0912c07.nc', 'c0912c08.nc', 'c0912c09.nc', 'c0912c10.nc', 'c0912c11.nc', 'c0912c12.nc',
-'c0912c13.nc', 'c0912c14.nc', 'c0912c15.nc', 'c0912c16.nc', 'c0912c17.nc', 'c0912c18.nc',
-'c0912c19.nc', 'c0912c20.nc', 'c0912c21.nc', 'c0912c22.nc', 'c0912c23.nc', 'c0912c24.nc',
-'c0912c25.nc', 'c0912c26.nc', 'c0912c27.nc', 'c0912c28.nc', 'c0912c29.nc', 'c0912c30.nc',
-'c0912c31.nc', 'c0912c32.nc', 'c0912c33.nc', 'c0912c34.nc', 'c0912c35.nc', 'c0912c36.nc',
-'c0912c37.nc', 'c0912c38.nc', 'c0912c39.nc', 'c0912c40.nc', 'c0912c41.nc', 'c0912c42.nc',
-'c0912c43.nc', 'c0912c44.nc', 'c0912c45.nc', 'c0912c46.nc', 'c0912c47.nc', 'c0912c48.nc',
-'c0912c49.nc', 'c0912c50.nc', 'c0912c51.nc', 'c0912c52.nc', 'c0912c53.nc', 'c0912c54.nc',
-                      ]
-cl.wfpctd_parms = [ 'TEMP', 'PSAL', 'xmiss', 'ecofl' ]
-#cl.wfpctd_parms = [ 'oxygen' ] we were able to load oxygen for 'c0912c03.nc'
 
 
 # Execute the load
 cl.process_command_line()
 
 if cl.args.test:
-    cl.loadDorado(stride=100)
-    cl.loadL_662(stride=100) # done
-#    cl.loadWFuctd(stride=10) # done
+    cl.stride = 100
+elif cl.args.stride:
+    cl.stride = cl.args.stride
+
+cl.loadDorado()
+cl.loadL_662()
+cl.loadWFuctd()
+cl.loadWFpctd()
+
 #    cl.loadWaveglider(stride=100)
     ##cl.loadDaphne(stride=10)
     ##cl.loadTethys(stride=10)
@@ -137,12 +139,7 @@ if cl.args.test:
     ##cl.loadM1ts(stride=1)
     ##cl.loadM1met(stride=1)
 
-elif cl.args.optimal_stride:
-    cl.loadDorado(stride=2)
-    cl.loadL_662(stride=1)
-#    cl.loadWFuctd(stride=1)
+# Add any X3D Terrain information specified in the constructor to the database - must be done after a load is executed
+cl.addTerrainResources()
 
-else:
-    cl.loadDorado(stride=cl.args.stride)
-    cl.loadL_662()
-#    cl.loadWFuctd()
+print( "All Done.")
