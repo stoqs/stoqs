8c3c2859c1e3476b161887184841e02ec4ca6d70
Mike McCann <MBARIMike@gmail.com>
2018-09-20 18:40:06 +0000

    Update Campaign description and use correct .x3d terrain URLs

--- stoqs/loaders/CANON/loadCANON_september2018.py
+++ stoqs/loaders/CANON/loadCANON_september2018.py
@@ -28,15 +28,15 @@ from CANON import CANONLoader
 import timing
 
 cl = CANONLoader('stoqs_canon_september2018', 'CANON - September 2018',
-                 description='September 2018 campaign observations in Monterey Bay',
+                 description='September 2018 ESP Tethys drift campaign observations in Monterey Bay',
                  x3dTerrains={
-                   'http://dods.mbari.org/terrain/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
+                   'https://stoqs.mbari.org/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
                      'position': '-2822317.31255 -4438600.53640 3786150.85474',
                      'orientation': '0.89575 -0.31076 -0.31791 1.63772',
                      'centerOfRotation': '-2711557.9403829873 -4331414.329506527 3801353.4691465236',
                      'VerticalExaggeration': '10',
                    },
-                   'http://stoqs.mbari.org/x3d/Monterey25_1x/Monterey25_1x_src_scene.x3d': {
+                   'https://stoqs.mbari.org/x3d/Monterey25_1x/Monterey25_1x_src_scene.x3d': {
                      'name': 'Monterey25_1x',
                      'position': '-2822317.31255 -4438600.53640 3786150.85474',
                      'orientation': '0.89575 -0.31076 -0.31791 1.63772',
