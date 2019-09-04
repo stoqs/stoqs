66bdf080e7cfc99955f29ee1f9aaaf9bc27afd9b
Mike McCann <MBARIMike@gmail.com>
2019-09-04 21:21:25 +0000

    Set NGINX_SERVER_NAME to localhost if not in environment

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -2,6 +2,10 @@
 if [[ -z "${NGINX_TMPL}" ]]; then
     NGINX_TMPL=nginx.tmpl
 fi
+if [[ -z "${NGINX_SERVER_NAME}" ]]; then
+    NGINX_SERVER_NAME=localhost
+fi
+
 DOLLAR='$' envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
