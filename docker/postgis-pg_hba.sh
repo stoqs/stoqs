61b4e41ace253964d7422dc84211d5ba521dc35f
Mike McCann <MBARIMike@gmail.com>
2019-09-04 16:48:56 +0000

    Remove root user

--- docker/postgis-pg_hba.sh
+++ docker/postgis-pg_hba.sh
@@ -11,7 +11,7 @@ echo -e "\
 # Allow all MBARI systems access - modify for your institution
 host    all everyone,stoqsadm   134.89.0.0/16   md5\n\
 # === STOQS - allow all private addresses that may be docker host\n\
-host    all stoqsadm,postgres,root   10.0.0.0/8      md5\n\
-host    all stoqsadm,postgres,root   172.16.0.0/12   md5\n\
-host    all stoqsadm,postgres,root   192.168.0.0/16  md5\n\
+host    all stoqsadm,postgres   10.0.0.0/8      md5\n\
+host    all stoqsadm,postgres   172.16.0.0/12   md5\n\
+host    all stoqsadm,postgres   192.168.0.0/16  md5\n\
 local   all all                                 trust" > ${PGDATA}/pg_hba.conf
