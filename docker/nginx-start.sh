d5b75267a325357f81ea9a6460e872843356308a
Mike McCann <MBARIMike@gmail.com>
2019-09-05 14:30:25 +0000

    Print env

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
+echo "*** env before"
+env
 if [[ -z "${NGINX_TMPL}" ]]; then
     NGINX_TMPL=nginx.tmpl
 fi
 if [[ -z "${NGINX_SERVER_NAME}" ]]; then
     NGINX_SERVER_NAME=localhost
 fi
+echo "*** env after"
+env
 
 DOLLAR='$' envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
 
