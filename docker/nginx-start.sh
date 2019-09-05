849b933b53480cb6e41cfb386f47bbd1f3393519
Mike McCann <MBARIMike@gmail.com>
2019-09-05 16:45:24 +0000

    Also set NGINX_[CRT,KEY]_NAME to localhost

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -4,9 +4,11 @@ if [[ -z "${NGINX_TMPL}" ]]; then
     NGINX_TMPL=nginx.tmpl
 fi
 if [[ -z "${NGINX_SERVER_NAME}" ]]; then
-    # Likely running in DockerHub Autotest via an MBARIMike push to stoqs, must export this
-    echo "*** NGINX_SERVER_NAME not found in environment - setting it to localhost"
+    # Likely running in DockerHub Autotest via an MBARIMike push to stoqs, must export these
+    echo "*** NGINX_SERVER_NAME not found in environment - setting it and NGINX_[CRT,KEY]_NAME to localhost"
     export NGINX_SERVER_NAME=localhost
+    export NGINX_CRT_NAME=localhost
+    export NGINX_KEY_NAME=localhost
 fi
 DOLLAR='$' envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
 
