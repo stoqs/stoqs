81ae76c207fec8fc2ce883fc52f6d7a6e00769c0
Mike McCann <MBARIMike@gmail.com>
2019-09-05 14:52:13 +0000

    Must export NGINX_SERVER_NAME for envsubst to see

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
-echo "*** env before"
-env
 if [[ -z "${NGINX_TMPL}" ]]; then
+    echo "*** NGINX_TMPL not found in environment - setting it to nginx.tmpl"
     NGINX_TMPL=nginx.tmpl
 fi
 if [[ -z "${NGINX_SERVER_NAME}" ]]; then
-    NGINX_SERVER_NAME=localhost
+    # Likely running in DockerHub Autotest via an MBARIMike push to stoqs, must export this
+    echo "*** NGINX_SERVER_NAME not found in environment - setting it to localhost"
+    export NGINX_SERVER_NAME=localhost
 fi
-echo "*** env after"
-env
-
 DOLLAR='$' envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
