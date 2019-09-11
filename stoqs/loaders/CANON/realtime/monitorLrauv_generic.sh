cf0e3f11dd47f971dc162c08d6bf8139a584c264
Mike McCann <MBARIMike@gmail.com>
2019-09-11 22:25:56 +0000

    More cleanup and process only sbdlogs from all platforms

--- stoqs/loaders/CANON/realtime/monitorLrauv_generic.sh
+++ stoqs/loaders/CANON/realtime/monitorLrauv_generic.sh
@@ -8,24 +8,24 @@ if [ -z "$DATABASE_URL" ]; then
   exit 1
 fi
 cd "$STOQS_HOME/stoqs/loaders/CANON/realtime"
-debug=''
-##debug='--debug'
+
 urlbase='http://dods.mbari.org/thredds/catalog/LRAUV'
 year=2019
-##declare -a searchstr=("realtime/sbdlogs/2019/.*shore.nc4$" "realtime/cell-logs/.*Priority.nc4$" "realtime/cell-logs/.*Normal.nc4$")
-# LRAUV/makai/realtime/cell-logs/20141217T232653/cell-Normal.nc4
-declare -a searchstr=("/realtime/cell-logs/.*Normal.nc4$")
-##declare -a platforms=('ahi' 'aku' 'brezo' 'daphne' 'galene' 'makai' 'opah' 'pontus' 'tethys' 'triton' 'whoidhs')
-declare -a platforms=('whoidhs')
+
+declare -a searchstr=("/realtime/sbdlogs/${year}/.*shore.nc4$")
+# Note that there is no year directory for in cell-logs - crawling for cell-logs takes longer - uncomment to process cell-logs
+##declare -a searchstr=("/realtime/sbdlogs/2019/.*shore.nc4$" "/realtime/cell-logs/.*Priority.nc4$" "/realtime/cell-logs/.*Normal.nc4$")
+declare -a platforms=('ahi' 'aku' 'brezo' 'daphne' 'galene' 'makai' 'opah' 'pontus' 'tethys' 'triton' 'whoidhs')
 
 for platform in "${platforms[@]}"
 do
     for search in "${searchstr[@]}"
     do
         directory=`echo ${search} | sed 's:/[^/]*$::'`
+        echo "Executing monitorLrauv.py for directory = ${directory}"
         python monitorLrauv.py --start '20190901T000000' --end '20190930T000000' \
  	                           -o /mbari/LRAUV/${platform}/${directory}/ -i /mbari/LRAUV/${platform}/${directory} \
-        -u ${urlbase}/${platform}/${search} \
+        -u ${urlbase}/${platform}${search} \
         --iparm depth \
         --parms depth chlorophyll temperature salinity mass_concentration_of_oxygen_in_sea_water \
         --plotparms depth chlorophyll temperature salinity mass_concentration_of_oxygen_in_sea_water  
