b1be880e49fbd588b6849916c7f9e603cd284e82
Mike McCann <MBARIMike@gmail.com>
2019-09-25 21:11:52 +0000

    Classify the script in an attempt to having logging levels work

--- stoqs/loaders/LRAUV/make_load_scripts.py
+++ stoqs/loaders/LRAUV/make_load_scripts.py
@@ -17,6 +17,7 @@ this_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 stoqs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 import calendar
+import logging
 import re
 from datetime import datetime
 from string import Template
@@ -32,88 +33,98 @@ lrauvs = ('ahi', 'aku', 'brezo', 'daphne', 'galene', 'makai', 'opah', 'pontus',
 title_base = 'LRAUV - Routine Operational data'
 description_base='MBARI Long Range Autonomous Vehicle data'
 
-def create_load_scripts(start_year=None, end_year=None):
-    # If called with no arguments process the current year
-    if not start_year:
-        start_year = datetime.today().year
-    if not end_year:
-        end_year = start_year + 1
-    else:
-        end_year += 1
+class LoaderMaker():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
-    monyyyys = []
-    for year in range(start_year, end_year):
-        for month in range(1, 13):
-            month_name = datetime(year, month, 1).strftime("%B")
-            month_name_short = datetime(year, month, 1).strftime("%b")
+    def create_load_scripts(self, start_year=None, end_year=None):
+        # If called with no arguments process the current year
+        if not start_year:
+            start_year = datetime.today().year
+        if not end_year:
+            end_year = start_year + 1
+        else:
+            end_year += 1
 
-            monyyyy = f"{month_name_short.lower()}{year}"
-            monyyyys.append(monyyyy)
-            db_alias = f"stoqs_lrauv_{monyyyy}"
-            title = f"{title_base} - {month_name} {year}"
-            description = f"{description_base} during {month_name} {year}"
-        
-            s_year = str(year)
-            e_year = str(year)
-            s_month = str(month)
-            e_month = str(month)
-            s_day = str(1)
-            e_day = str(calendar.monthrange(year, month)[1])
+        monyyyys = []
+        for year in range(start_year, end_year):
+            for month in range(1, 13):
+                month_name = datetime(year, month, 1).strftime("%B")
+                month_name_short = datetime(year, month, 1).strftime("%b")
 
-            with open(os.path.join(this_dir, script_template)) as s_t:
-                source = Template(s_t.read())
+                monyyyy = f"{month_name_short.lower()}{year}"
+                monyyyys.append(monyyyy)
+                db_alias = f"stoqs_lrauv_{monyyyy}"
+                title = f"{title_base} - {month_name} {year}"
+                description = f"{description_base} during {month_name} {year}"
+            
+                s_year = str(year)
+                e_year = str(year)
+                s_month = str(month)
+                e_month = str(month)
+                s_day = str(1)
+                e_day = str(calendar.monthrange(year, month)[1])
 
-            script = source.substitute({'db_alias': db_alias, 'title': title, 'description': description,
-                                        's_year': s_year, 's_month': s_month, 's_day': s_day,
-                                        'e_year': e_year, 'e_month': e_month, 'e_day': e_day,
-                                        'lrauvs': repr(lrauvs)})
+                with open(os.path.join(this_dir, script_template)) as s_t:
+                    source = Template(s_t.read())
 
-            load_file_name = f"load_lrauv_{monyyyy}.py"
-            with open(os.path.join(this_dir, load_file_name), 'w') as lf:
-                lf.write(script)
+                script = source.substitute({'db_alias': db_alias, 'title': title, 'description': description,
+                                            's_year': s_year, 's_month': s_month, 's_day': s_day,
+                                            'e_year': e_year, 'e_month': e_month, 'e_day': e_day,
+                                            'lrauvs': repr(lrauvs)})
 
-            os.chmod(os.path.join(this_dir, load_file_name), 0o755)
-            print(f"Done making: {os.path.join(this_dir, load_file_name)}")
+                load_file_name = f"load_lrauv_{monyyyy}.py"
+                with open(os.path.join(this_dir, load_file_name), 'w') as lf:
+                    lf.write(script)
 
-    return monyyyys
+                os.chmod(os.path.join(this_dir, load_file_name), 0o755)
+                print(f"Done making: {os.path.join(this_dir, load_file_name)}")
+                self.logger.info(f"Done making: {os.path.join(this_dir, load_file_name)}")
 
-def update_lrauv_campaigns(monyyyys):
-    '''Respect existing items in existing mbari_lrauv_campaigns.py file
-    and only update with any new campaign_items.
-    '''
+        return monyyyys
 
-    # Read items (if any) from any existing mbari_lrauv_campaigns.py file
-    #     ('stoqs_lrauv_jan2018', 'LRAUV/load_lrauv_jan2018.py'),\n
-    CAMPAIGN_ITEM = re.compile(r".*\('stoqs_lrauv_(.*)', 'LRAUV\/load_lrauv_(.*).py'\),")
-    monyyyy_in_file = []
-    with open(os.path.join(stoqs_dir, campaign_file_name)) as cf:
-        for line in cf:
-            c_match = re.match(CAMPAIGN_ITEM, line)
-            if c_match:
-                monyyyy_in_file.append(c_match.group(1))
+    def update_lrauv_campaigns(self, monyyyys):
+        '''Respect existing items in existing mbari_lrauv_campaigns.py file
+        and only update with any new campaign_items.
+        '''
 
-    # Rewrite the file with existing and new campaigns
-    campaign_items_str = ''
-    for monyyyy in monyyyy_in_file:
-        load_file_name = f"load_lrauv_{monyyyy}.py"
-        campaign_items_str += f"    ('stoqs_lrauv_{monyyyy}', '{os.path.join(os.path.basename(this_dir), load_file_name)}'),\n"
+        # Read items (if any) from any existing mbari_lrauv_campaigns.py file
+        #     ('stoqs_lrauv_jan2018', 'LRAUV/load_lrauv_jan2018.py'),\n
+        CAMPAIGN_ITEM = re.compile(r".*\('stoqs_lrauv_(.*)', 'LRAUV\/load_lrauv_(.*).py'\),")
+        monyyyy_in_file = []
+        with open(os.path.join(stoqs_dir, campaign_file_name)) as cf:
+            for line in cf:
+                c_match = re.match(CAMPAIGN_ITEM, line)
+                if c_match:
+                    monyyyy_in_file.append(c_match.group(1))
 
-    for monyyyy in monyyyys:
-        if monyyyy not in monyyyy_in_file:
+        # Rewrite the file with existing and new campaigns
+        campaign_items_str = ''
+        for monyyyy in monyyyy_in_file:
             load_file_name = f"load_lrauv_{monyyyy}.py"
             campaign_items_str += f"    ('stoqs_lrauv_{monyyyy}', '{os.path.join(os.path.basename(this_dir), load_file_name)}'),\n"
 
-    # Write out campaign file
-    with open(os.path.join(stoqs_dir, campaign_template)) as c_t:
-        source = Template(c_t.read())
-    script = source.substitute({'campaign_tuples': campaign_items_str})
-    with open(os.path.join(stoqs_dir, campaign_file_name), 'w') as cf:
-        cf.write(script)
+        for monyyyy in monyyyys:
+            if monyyyy not in monyyyy_in_file:
+                load_file_name = f"load_lrauv_{monyyyy}.py"
+                campaign_items_str += f"    ('stoqs_lrauv_{monyyyy}', '{os.path.join(os.path.basename(this_dir), load_file_name)}'),\n"
 
-    print(f"Done writing {os.path.join(stoqs_dir, campaign_file_name)}")
+        # Write out campaign file
+        with open(os.path.join(stoqs_dir, campaign_template)) as c_t:
+            source = Template(c_t.read())
+        script = source.substitute({'campaign_tuples': campaign_items_str})
+        with open(os.path.join(stoqs_dir, campaign_file_name), 'w') as cf:
+            cf.write(script)
+
+        print(f"Done writing {os.path.join(stoqs_dir, campaign_file_name)}")
+        self.logger.info(f"Done writing {os.path.join(stoqs_dir, campaign_file_name)}")
 
 
 if __name__ == '__main__':
-    items = create_load_scripts()
-    update_lrauv_campaigns(items)
+    lm = LoaderMaker()
+    lm.logger.setLevel(logging.DEBUG)
+
+    this_year = datetime.today().year
+    items = lm.create_load_scripts(this_year, this_year)
+    lm.update_lrauv_campaigns(items)
 
