f9e9160425487da3ed9030f12a520fc1bda11401
Mike McCann <MBARIMike@gmail.com>
2020-05-29 23:43:00 +0000

    Use maximum.accumulate() to find multiple repeated/decreasing values in a row, drop from each coordinate individually

--- stoqs/loaders/CANON/toNetCDF/lrauvNc4ToNetcdf.py
+++ stoqs/loaders/CANON/toNetCDF/lrauvNc4ToNetcdf.py
@@ -997,12 +997,14 @@ class InterpolatorWriter(BaseWriter):
         logger.info(f'Read file {in_file}')
 
         coord_ts = self.createCoord(coord)
-        repeated_values = np.where(np.diff(coord_ts['time'].index.astype('int')) <= 0)[0]
-        if len(repeated_values) > 0:
-            logger.warning(f"Original time axis has repeated values at indices: {repeated_values}")
-            logger.warning(f"Dropping those indices from coords: {coord}")
-            for c in coord:
-                coord_ts[c].drop(coord_ts[c].index[repeated_values], inplace=True)
+
+        # Remove repeated or decreasing values from each coordinate - accumulate maxiumum to catch multiple decreased values
+        # See: https://stackoverflow.com/questions/28563711/make-a-numpy-array-monotonic-without-a-python-loop
+        for crd in coord:
+            repeated_values = np.where(np.diff(np.maximum.accumulate(coord_ts[crd].index.astype('int'))) <= 0)[0]
+            if len(repeated_values) > 0:
+                logger.warning(f"Dropping from {crd} repeated/decreasing values at indices: {repeated_values}")
+                coord_ts[crd].drop(coord_ts[crd].index[repeated_values], inplace=True)
 
         # Get time parameter and align everything to this
         logger.info(f'Creating t variable of the time indexes')
