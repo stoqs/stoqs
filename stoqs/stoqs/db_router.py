9a18eb5a64894760dfed186ce741724b3484e38d
Mike McCann <mccann@mbari.org>
2023-04-03 09:41:32 -0700

    Fix error messages for various ways databases may not exist: 1) missing from setting.DATABASES, 2) in setting.DATABASES, but doesn't exist

--- stoqs/stoqs/db_router.py
+++ stoqs/stoqs/db_router.py
@@ -39,7 +39,7 @@ class RouterMiddleware(object):
 
         if 'dbAlias' in kwargs:
             if kwargs['dbAlias'] not in settings.DATABASES.keys():
-                return HttpResponseBadRequest('Bad request: Database "' + kwargs['dbAlias'] + '" Does Not Exist')
+                return HttpResponseBadRequest('Bad request: Database "' + kwargs['dbAlias'] + '" Does Not Exist in settings.DATABASES')
 
             # Add a thread local variable, and remove the dbAlias, since it's handled by the middleware.
             _thread_local_vars.dbAlias = kwargs.pop('dbAlias')
