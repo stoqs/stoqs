c9e8ef1a6d2f4db2cd259f9d132b26d1db9fbd1c
Mike McCann <mccann@mbari.org>
2023-07-13 07:46:20 -0700

    Another attempt to get Actions to work

--- docker/stoqs-start-run_server.sh
+++ docker/stoqs-start-run_server.sh
@@ -1,11 +1,71 @@
 #!/bin/bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
+STOQS_SRVHOME=/srv
+STOQS_SRVPROJ=/srv/stoqs
 
+# Ensure that stoqs-postgis container is serving databases before continuing
+POSTGRES_DB=postgres python ${STOQS_SRVHOME}/docker/database-check.py > /dev/null 2>&1
+while [[ $? != 0 ]] ; do
+    sleep 5; echo "*** Waiting for postgis container ..."
+    POSTGRES_DB=postgres python ${STOQS_SRVHOME}/docker/database-check.py > /dev/null 2>&1
+done
+
+# Allow for psql execution (used for database creation) without a password
+echo ${PGHOST}:\*:\*:postgres:${POSTGRES_PASSWORD} > /root/.pgpass &&\
+    chmod 600 /root/.pgpass
+
+export PYTHONPATH="${STOQS_SRVPROJ}:${PYTHONPATH}"
+
+# Monterey Bay bathymetry data is needed for default (and many other) database loads
+if [[ ! -e ${STOQS_SRVPROJ}/loaders/Monterey25.grd ]]; then
+    echo "Getting Monterey25.grd..."
+    wget -q -N -O ${STOQS_SRVPROJ}/loaders/Monterey25.grd http://stoqs.mbari.org/terrain/Monterey25.grd
+fi
+
+# Volume shared with nginx for writing Matplotlib-generated images
+if [ "$PRODUCTION" == "true" ]; then
+    echo "Checking for presence of ${MEDIA_ROOT}/sections..."
+    if [[ ! -e ${MEDIA_ROOT}/sections ]]; then
+        echo "Creating directories for image generation and serving by nginx..."
+        mkdir -p ${MEDIA_ROOT}/sections ${MEDIA_ROOT}/parameterparameter
+        chmod 733 ${MEDIA_ROOT}/sections ${MEDIA_ROOT}/parameterparameter
+    fi
+fi
+
+echo "Checking for presence of directory for mapfiles: ${MAPFILE_DIR}"
+if [[ ! -e ${MAPFILE_DIR} ]]; then
+    echo "mkdir ${MAPFILE_DIR}"
+    mkdir ${MAPFILE_DIR}
+fi
+# Ensure that shared volume for Mapserver map files is readable by nginx's uwsgi
+chmod 777 ${MAPFILE_DIR}
+
+# If default stoqs database doesn't exist then load it - also running the unit and functional tests
+echo "Checking for presence of stoqs database..."
+POSTGRES_DB=stoqs python ${STOQS_SRVHOME}/docker/database-check.py
+if [[ $? != 0 ]]; then
+    echo "Creating default stoqs database and running tests..."
+    ./test.sh changeme load noextraload
+fi
+
+if [[ ! -z $CAMPAIGNS_MODULE ]]; then
+    echo "Checking for presence of ${STOQS_SRVHOME}/stoqs/campaigns.py..."
+    if [[ -e ${STOQS_SRVHOME}/stoqs/campaigns.py ]]; then
+        echo "Warning: File ${STOQS_SRVHOME}/stoqs/campaigns.py exists!"
+        echo "This may be a symbolic link to stoqs/mbari_campaigns.py for use in a development system."
+        echo "File stoqs/campaigns.py must first be removed before running a Docker-based STOQS server."
+        echo "Removing it for you..."
+        /bin/rm ${STOQS_SRVHOME}/stoqs/campaigns.py
+    fi
+    echo "Copying ${STOQS_SRVHOME}/$CAMPAIGNS_MODULE to ${STOQS_SRVPROJ}/campaigns.py..."
+    cp ${STOQS_SRVHOME}/$CAMPAIGNS_MODULE ${STOQS_SRVPROJ}/campaigns.py
+fi
+
+# Fire up stoqs web app for running functional tests
 export MAPSERVER_SCHEME=http
-export ALLOWED_HOSTS=stoqs
 echo "Starting development server with DATABASE_URL=${DATABASE_URL}..."
-# https://stackoverflow.com/a/72206748/1281657
-python stoqs/manage.py runserver_plus --keep-meta-shutdown 0.0.0.0:8001 --settings=config.settings.local
+nohup ${STOQS_SRVPROJ}/manage.py runserver 0.0.0.0:8001 --settings=config.settings.local
+
+export DATABASE_URL=postgis://stoqsadm:CHANGEME@stoqs-postgis:5432/stoqs
+export DATABASE_SUPERUSER_URL=postgis://postgres:changeme@stoqs-postgis:5432/stoqs
+DATABASE_URL=$DATABASE_SUPERUSER_URL stoqs/manage.py test stoqs.tests.functional_tests --settings=config.settings.ci
