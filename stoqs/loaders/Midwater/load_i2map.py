9c1eec39bca9c3d183f5aebb39d04ab1efe19d84
Mike McCann <MBARIMike@gmail.com>
2023-10-27 15:02:49 -0700

    Remove zNear and zFar clipping planes

--- stoqs/loaders/Midwater/load_i2map.py
+++ stoqs/loaders/Midwater/load_i2map.py
@@ -26,8 +26,6 @@ cl = CANONLoader('stoqs_all_i2map', 'Midwater All i2MAP Missions',
                      'position': '-2822317.31255 -4438600.53640 3786150.85474',
                      'orientation': '0.89575 -0.31076 -0.31791 1.63772',
                      'centerOfRotation': '-2711557.9403829873 -4331414.329506527 3801353.4691465236',
-                     'zNear': '10000.0',
-                     'zFar': '30000000.0',
                      'VerticalExaggeration': '10',
                    },
                    'https://stoqs.mbari.org/x3d/Monterey25_10x/Monterey25_e10.glb': {
@@ -35,8 +33,6 @@ cl = CANONLoader('stoqs_all_i2map', 'Midwater All i2MAP Missions',
                      'position': '-2709340.15630 3838759.47032 -4315928.63232',
                      'orientation': '-0.92531 0.16085 0.34340 1.48161',
                      'centerOfRotation': '-2698376.20956 3816324.15548 -4328209.99402',
-                     'zNear': '10000.0',
-                     'zFar': '30000000.0',
                      'VerticalExaggeration': '10',
                    },
                    'https://stoqs.mbari.org/x3d/Monterey25_10x/Monterey25_e10_lat_rev.glb': {
@@ -44,8 +40,6 @@ cl = CANONLoader('stoqs_all_i2map', 'Midwater All i2MAP Missions',
                      'position': '-2709340.15630 3838759.47032 -4315928.63232',
                      'orientation': '-0.92531 0.16085 0.34340 1.48161',
                      'centerOfRotation': '-2698376.20956 3816324.15548 -4328209.99402',
-                     'zNear': '10000.0',
-                     'zFar': '30000000.0',
                      'VerticalExaggeration': '10',
                    },
                    'https://stoqs.mbari.org/x3d/Monterey25_10x/Monterey25_e10_sphere.glb': {
@@ -53,16 +47,12 @@ cl = CANONLoader('stoqs_all_i2map', 'Midwater All i2MAP Missions',
                      'position': '-2709340.15630 3838759.47032 -4315928.63232',
                      'orientation': '-0.92531 0.16085 0.34340 1.48161',
                      'centerOfRotation': '-2698376.20956 3816324.15548 -4328209.99402',
-                     'zNear': '10000.0',
-                     'zFar': '30000000.0',
                      'VerticalExaggeration': '10',
                    },
                    'https://stoqs.mbari.org/x3d/Globe_1m_bath_10x/Globe_1m_bath_10x_scene.x3d': {
                      'position': '14051448.48336 -15407886.51486 6184041.22775',
                      'orientation': '0.83940 0.33030 0.43164 1.44880',
                      'centerOfRotation': '0 0 0',
-                     'zNear': '10000.0',
-                     'zFar': '30000000.0',
                      'VerticalExaggeration': '10',
                    },
                  },
@@ -89,8 +79,15 @@ if cl.args.startdate:
 if cl.args.enddate:
     enddate = datetime.strptime(cl.args.enddate, '%Y%m%d')
 
-#cl.load_i2MAP(datetime(2022, 3, 1), datetime(2022, 4, 1), build_attrs=True)
 cl.load_i2MAP(startdate, enddate, build_attrs=True)
+
+# For testing...
+# cl.load_i2MAP(datetime(2022, 3, 1), datetime(2022, 4, 1), build_attrs=True)
+# cl.i2map_base = 'http://dods.mbari.org/opendap/data/auvctd/surveys/2019/netcdf/'
+# cl.i2map_files = ['i2map_2019.157.02_1S.nc']
+# #cl.i2map_parms = ['seabird25p_temperature']
+# cl.i2map_parms = ['navigation_mWaterSpeed']
+# cl.load_i2MAP(startdate, enddate, build_attrs=False)
 
 # Add any X3D Terrain information specified in the constructor to the database - must be done after a load is executed
 cl.addTerrainResources()
