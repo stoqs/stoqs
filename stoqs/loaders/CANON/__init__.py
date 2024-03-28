0caf8338cd22c541f1bc9f3bc7e37965c5644cdc
Mike McCann <MBARIMike@gmail.com>
2024-03-28 10:41:23 -0700

    Add LRAUV backscatter and ubat variables

--- stoqs/loaders/CANON/__init__.py
+++ stoqs/loaders/CANON/__init__.py
@@ -316,6 +316,8 @@ class CANONLoader(LoadScript):
     def loadLRAUV(self, pname, startdate=None, enddate=None, 
                   parameters=['temperature', 'salinity', 'chlorophyll', 'nitrate', 'oxygen', 'PAR',
                     'volumescatcoeff117deg470nm', 'volumescatcoeff117deg650nm',
+                    'backscatteringcoeff470nm', 'backscatteringcoeff650nm',
+                    'average_bioluminescence', 'ubat_flow_rate',
                     'yaw', 'pitch', 'roll', 'control_inputs_rudder_angle', 'control_inputs_mass_position',
                     'control_inputs_buoyancy_position', 'control_inputs_propeller_rotation_rate',
                     'health_platform_battery_charge', 'health_platform_average_voltage',
