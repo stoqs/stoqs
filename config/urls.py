9b5c4f588df83d2f28f2405363372697c2538dcc
Mike McCann <MBARIMike@gmail.com>
2025-07-15 13:33:47 -0700

    Fix path to jetplus.txt using STATICFILES_DIRS.
    This is probably not the best Django practice, but with this change
    the development server now starts witout error.

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
-from django.urls import path
+from django.urls import path, re_path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
