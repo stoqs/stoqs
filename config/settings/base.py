9bbc36c757f67405dc24cd78fe407f5b003f809a
Mike McCann <MBARIMike@gmail.com>
2025-08-22 21:24:30 +0000

    Set deault engine to postgis.

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
